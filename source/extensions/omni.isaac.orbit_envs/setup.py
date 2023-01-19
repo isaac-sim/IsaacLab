ed14985e81e6ade6d7ebde2e72e04818d194508a
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-01-19 19:41:22 +0100

    Add skrl to RL extra dependencies

--- source/extensions/omni.isaac.orbit_envs/setup.py
+++ source/extensions/omni.isaac.orbit_envs/setup.py
@@ -32,6 +32,7 @@ INSTALL_REQUIRES = [
 # Extra dependencies for RL agents
 EXTRAS_REQUIRE = {
     "sb3": ["stable-baselines3>=1.5.0", "tensorboard"],
+    "skrl": ["skrl>=0.10.0"],
     "rl_games": ["rl-games==1.5.2"],
     "rsl_rl": ["rsl_rl@git+https://github.com/leggedrobotics/rsl_rl.git"],
     "robomimic": ["robomimic@git+https://github.com/ARISE-Initiative/robomimic.git"],
