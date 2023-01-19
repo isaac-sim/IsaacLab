51a8694730cb8e7f6cd4c29bfcae045208bd28df
Antonio Serrano Muñoz <toni.semu@gmail.com>
2023-01-19 17:04:25 +0100

    Add environment wrapper for the skrl RL library

---
+++ source/extensions/omni.isaac.orbit_envs/omni/isaac/orbit_envs/utils/wrappers/skrl.py
@@ -0,0 +1,42 @@
+"""Wrapper to configure an :class:`IsaacEnv` instance to skrl vectorized environment
+
+The following example shows how to wrap an environment for skrl:
+
+.. code-block:: python
+
+    from omni.isaac.orbit_envs.utils.wrappers.skrl import SkrlVecEnvWrapper
+
+    env = SkrlVecEnvWrapper(env)
+
+"""
+
+
+# skrl
+from skrl.envs.torch.wrappers import wrap_env
+
+from omni.isaac.orbit_envs.isaac_env import IsaacEnv
+
+__all__ = ["SkrlVecEnvWrapper"]
+
+
+"""
+Vectorized environment wrapper.
+"""
+
+
+def SkrlVecEnvWrapper(env: IsaacEnv):
+    """Wraps around IsaacSim environment for skrl.
+
+    This function wraps around the IsaacSim environment. Since the IsaacEnv 
+    environment wrapping functionality is defined within the skrl library itself, 
+    this implementation is maintained for compatibility with the structure of the extension. 
+    Internally it calls the :meth:`wrap_env()` from the skrl API.
+
+    Reference:
+        https://skrl.readthedocs.io/en/develop/modules/skrl.envs.wrapping.html
+    """
+    # check that input is valid
+    if not isinstance(env.unwrapped, IsaacEnv):
+        raise ValueError(f"The environment must be inherited from IsaacEnv. Environment type: {type(env)}")
+    # wrap and return the environment    
+    return wrap_env(env, wrapper="isaac-orbit")
