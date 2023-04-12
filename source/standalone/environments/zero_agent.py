3393258018036024daafa8cb26c4cd488cf94d58
Mayank Mittal <mittalma@leggedrobotics.com>
2023-04-12 10:58:44 +0200

    adapts zero agent for hydra

--- source/standalone/environments/zero_agent.py
+++ source/standalone/environments/zero_agent.py
@@ -8,6 +8,7 @@
 """Launch Isaac Sim Simulator first."""
 
 
+import sys
 import argparse
 
 from omni.isaac.kit import SimulationApp
@@ -15,10 +16,11 @@ from omni.isaac.kit import SimulationApp
 # add argparse arguments
 parser = argparse.ArgumentParser("Welcome to Orbit: Omniverse Robotics Environments!")
 parser.add_argument("--headless", action="store_true", default=False, help="Force display off at all times.")
-parser.add_argument("--cpu", action="store_true", default=False, help="Use CPU pipeline.")
-parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
-args_cli = parser.parse_args()
+args_cli, remaining = parser.parse_known_args()
+# clear out sys.argv
+sys.argv = [sys.argv[0]] + remaining
+sys.argc = len(sys.argv)
 
 # launch the simulator
 config = {"headless": args_cli.headless}
@@ -30,15 +32,27 @@ simulation_app = SimulationApp(config)
 import gym
 import torch
 
+import hydra
+from omegaconf import OmegaConf
+
 import omni.isaac.contrib_envs  # noqa: F401
 import omni.isaac.orbit_envs  # noqa: F401
-from omni.isaac.orbit_envs.utils import parse_env_cfg
+from omni.isaac.orbit_envs.isaac_env_cfg import IsaacEnvCfg
+from omni.isaac.orbit_envs.utils.parse_cfg import register_cfg_to_hydra
 
 
-def main():
+@hydra.main(config_path=None, config_name=args_cli.task, version_base="1.3")
+def main(env_cfg: IsaacEnvCfg):
     """Zero actions agent with Isaac Orbit environment."""
-    # parse configuration
-    env_cfg = parse_env_cfg(args_cli.task, use_gpu=not args_cli.cpu, num_envs=args_cli.num_envs)
+    # get underlying object
+    env_cfg = OmegaConf.to_object(env_cfg)
+    # modify the environment configuration
+    if env_cfg.sim.device == "cpu":
+        env_cfg.sim.use_gpu_pipeline = False
+        env_cfg.sim.physx.use_gpu = False
+    elif "cuda" in env_cfg.sim.device:
+        env_cfg.sim.use_gpu_pipeline = True
+        env_cfg.sim.physx.use_gpu = True
     # create environment
     env = gym.make(args_cli.task, cfg=env_cfg, headless=args_cli.headless)
 
@@ -56,8 +70,10 @@ def main():
 
     # close the simulator
     env.close()
-    simulation_app.close()
 
 
 if __name__ == "__main__":
+    # configure hydra configuration
+    register_cfg_to_hydra(args_cli.task)
+    # run main function
     main()
