944606a7b2cd6b0e022cad113fdcd09ed3399484
Mayank Mittal <mittalma@leggedrobotics.com>
2023-04-14 08:27:39 +0200

    adds module version to __init__.py

--- source/extensions/omni.isaac.orbit/omni/isaac/orbit/__init__.py
+++ source/extensions/omni.isaac.orbit/omni/isaac/orbit/__init__.py
@@ -9,13 +9,14 @@ Python module with robotic environments.
 
 
 import os
+import toml
 
 # Conveniences to other module directories via relative paths
 ORBIT_EXT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
 """Path to the extension source directory."""
 
-ORBIT_DATA_DIR = os.path.join(ORBIT_EXT_DIR, "data")
-"""Path to the extension data directory."""
+ORBIT_METADATA = toml.load(os.path.join(ORBIT_EXT_DIR, "config", "extension.toml"))
+"""Extension metadata dictionary parsed from the extension.toml file."""
 
-__author__ = "Mayank Mittal"
-__email__ = "mittalma@ethz.ch"
+# Configure the module-level variables
+__version__ = ORBIT_METADATA["package"]["version"]
