0c6a23bf9e10f3f74cb1648920039b9c775f1236
Mayank Mittal <12863862+Mayankm96@users.noreply.github.com>
2023-06-08 14:57:32 +0200

    Fixes setuptools<=66.x to use gym==0.21.0 (#91)
    Setuptools 67.0 onwards does not support malformed strings. As mentioned in their release notes, users must conform to PEP specifications strictly (i.e. will result in build errors if not complied). 
    
    The MR fixes the `setuptools` package version to be <=66, so that the users can still continue using the gym==0.21.0. Though long-term we should switch to using Gymnasium. Additionally, it removes the malformed strings in Orbit packages to follow the new `setuptools`.

--- source/extensions/omni.isaac.orbit/setup.py
+++ source/extensions/omni.isaac.orbit/setup.py
@@ -37,7 +37,7 @@ setup(
     keywords=EXTENSION_TOML_DATA["package"]["keywords"],
     license="BSD-3-Clause",
     include_package_data=True,
-    python_requires=">=3.7.*",
+    python_requires=">=3.7",
     install_requires=INSTALL_REQUIRES,
     packages=["omni.isaac.orbit"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3.7"],
