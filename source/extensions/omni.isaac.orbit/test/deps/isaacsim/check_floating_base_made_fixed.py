7c363cfb6b11114b261f725c1bd35ce3a37c7b1c
Mayank Mittal <mittalma@leggedrobotics.com>
2024-04-25 11:41:49 +0200

    adds working sample

--- source/extensions/omni.isaac.orbit/test/deps/isaacsim/check_floating_base_made_fixed.py
+++ source/extensions/omni.isaac.orbit/test/deps/isaacsim/check_floating_base_made_fixed.py
@@ -32,13 +32,13 @@ import carb
 import omni.isaac.core.utils.nucleus as nucleus_utils
 import omni.isaac.core.utils.prims as prim_utils
 import omni.isaac.core.utils.stage as stage_utils
-from pxr import Sdf, UsdPhysics
-import omni.physx
 import omni.kit.commands
+import omni.physx
 from omni.isaac.core.articulations import ArticulationView
 from omni.isaac.core.utils.carb import set_carb_setting
 from omni.isaac.core.utils.viewports import set_camera_view
 from omni.isaac.core.world import World
+from pxr import PhysxSchema, UsdPhysics
 
 # check nucleus connection
 if nucleus_utils.get_assets_root_path() is None:
@@ -64,31 +64,6 @@ Main
 
 def main():
     """Spawns the ANYmal robot and makes it fixed."""
-    # -- Robot
-    # resolve asset
-    usd_path = f"{ISAAC_ORBIT_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd"
-    root_prim_path = "/World/Robot/base"
-    # add asset
-    print("Loading robot from: ", usd_path)
-    prim_utils.create_prim(
-        "/World/Robot",
-        usd_path=usd_path,
-        translation=(0.0, 0.0, 0.6),
-    )
-    # create fixed joint
-    if args_cli.fix_base:
-        stage = stage_utils.get_current_stage()
-        root_prim = stage.GetPrimAtPath("/World/Robot/base")
-
-        # create fixed joint
-        omni.kit.commands.execute(
-            "CreateJointCommand", stage=stage, joint_type="Fixed", from_prim=None, to_prim=root_prim
-        )
-        # joint_prim = UsdPhysics.FixedJoint.Define(stage, "/World/Robot/rootJoint")
-        # joint_prim.GetJointEnabledAttr().Set(True)
-        # # joint_prim.CreateBody0Rel().SetTargets([])
-        # joint_prim.CreateBody1Rel().SetTargets([root_prim_path])
-
     # Load kit helper
     world = World(physics_dt=0.005, rendering_dt=0.005, backend="torch", device="cpu")
     # Set main camera
@@ -105,8 +80,66 @@ def main():
     prim_utils.create_prim("/World/Light/GreySphere", "SphereLight", translation=(4.5, 3.5, 10.0))
     # Lights-2
     prim_utils.create_prim("/World/Light/WhiteSphere", "SphereLight", translation=(-4.5, 3.5, 10.0))
+    # -- Robot
+    # resolve asset
+    usd_path = f"{ISAAC_ORBIT_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd"
+    root_prim_path = "/World/Robot/base"
+    # add asset
+    print("Loading robot from: ", usd_path)
+    prim_utils.create_prim(
+        "/World/Robot",
+        usd_path=usd_path,
+        translation=(0.0, 0.0, 0.6),
+    )
+    # create fixed joint
+    if args_cli.fix_base:
+        # get all necessary information
+        stage = stage_utils.get_current_stage()
+        root_prim = stage.GetPrimAtPath(root_prim_path)
+        parent_prim = root_prim.GetParent()
 
-   
+        # here we assume that the root prim is a rigid body
+        # there is no clear way to deal with situation where the root prim is not a rigid body but has articulation api
+        # in that case, it is unclear how to get the link to the first link in the tree
+        if not root_prim.HasAPI(UsdPhysics.RigidBodyAPI):
+            raise RuntimeError("The root prim does not have the RigidBodyAPI applied.")
+
+        # create fixed joint
+        omni.kit.commands.execute(
+            "CreateJointCommand",
+            stage=stage,
+            joint_type="Fixed",
+            from_prim=None,
+            to_prim=root_prim,
+        )
+
+        # move the root to the parent if this is a rigid body
+        # having a fixed joint on a rigid body makes physx treat it as a part of the maximal coordinate tree
+        # if we put to joint on the parent, physx parser treats it as a fixed base articulation
+        # get parent prim
+        parent_prim = root_prim.GetParent()
+        # apply api to parent
+        UsdPhysics.ArticulationRootAPI.Apply(parent_prim)
+        PhysxSchema.PhysxArticulationAPI.Apply(parent_prim)
+
+        # copy the attributes
+        # -- usd attributes
+        root_usd_articulation_api = UsdPhysics.ArticulationRootAPI(root_prim)
+        for attr_name in root_usd_articulation_api.GetSchemaAttributeNames():
+            attr = root_prim.GetAttribute(attr_name)
+            parent_prim.GetAttribute(attr_name).Set(attr.Get())
+        # -- physx attributes
+        root_physx_articulation_api = PhysxSchema.PhysxArticulationAPI(root_prim)
+        for attr_name in root_physx_articulation_api.GetSchemaAttributeNames():
+            attr = root_prim.GetAttribute(attr_name)
+            parent_prim.GetAttribute(attr_name).Set(attr.Get())
+
+        # remove api from root
+        root_prim.RemoveAPI(UsdPhysics.ArticulationRootAPI)
+        root_prim.RemoveAPI(PhysxSchema.PhysxArticulationAPI)
+
+        # rename root path to parent path
+        root_prim_path = parent_prim.GetPath().pathString
 
     # Setup robot
     robot_view = ArticulationView(root_prim_path, name="ANYMAL")
