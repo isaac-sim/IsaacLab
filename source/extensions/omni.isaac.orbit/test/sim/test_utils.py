7e45efff7c3d9311029699d86c477d3d29b7fc28
Lorenz Wellhausen <lorenz.wellhausen@swiss-mile.com>
2024-05-10 18:12:25 +0200

    Added unit test for select_usd_variants()

--- source/extensions/omni.isaac.orbit/test/sim/test_utils.py
+++ source/extensions/omni.isaac.orbit/test/sim/test_utils.py
@@ -22,6 +22,8 @@ import omni.isaac.core.utils.stage as stage_utils
 import omni.isaac.orbit.sim as sim_utils
 from omni.isaac.orbit.utils.assets import ISAAC_NUCLEUS_DIR, ISAAC_ORBIT_NUCLEUS_DIR
 
+from pxr import Usd, UsdGeom, Sdf
+
 
 class TestUtilities(unittest.TestCase):
     """Test fixture for the sim utility functions."""
@@ -106,6 +108,23 @@ class TestUtilities(unittest.TestCase):
         self.assertIsNotNone(sim_utils.find_global_fixed_joint_prim("/World/Franka"))
         self.assertIsNone(sim_utils.find_global_fixed_joint_prim("/World/Franka", check_enabled_only=True))
 
+    def test_select_usd_variants(self):
+        """ Test select_usd_variants() function."""
+        stage = stage_utils.get_current_stage()
+        prim: Usd.Prim = UsdGeom.Xform.Define(stage, Sdf.Path("/World")).GetPrim()
+        stage.SetDefaultPrim(prim)
+
+        # Create the variant set and add your variants to it.
+        variants = ["red", "blue", "green"]
+        variant_set = prim.GetVariantSets().AddVariantSet("colors")
+        for variant in variants:
+            variant_set.AddVariant(variant)
+
+        # Set the variant selection
+        sim_utils.utils.select_usd_variants("/World", {"colors": "red"}, stage)
+
+        # Check if the variant selection is correct
+        self.assertEqual(variant_set.GetVariantSelection(), "red")
 
 if __name__ == "__main__":
     run_tests()
