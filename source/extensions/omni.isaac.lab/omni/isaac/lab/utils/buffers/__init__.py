a661ab8572a5e068c8d1248e04882266c9df939b
Mayank Mittal <mittalma@leggedrobotics.com>
2024-06-24 18:14:03 +0200

    adds test for circular buffer

--- source/extensions/omni.isaac.lab/omni/isaac/lab/utils/buffers/__init__.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/utils/buffers/__init__.py
@@ -5,6 +5,6 @@
 
 """Sub-module containing different buffers."""
 
-from .circular_buffer import BatchedCircularBuffer
+from .circular_buffer import CircularBuffer
 from .delay_buffer import DelayBuffer
 from .timestamped_buffer import TimestampedBuffer
