01bab68611c754323b65f28b79d03943836a4b80
Mayank Mittal <12863862+Mayankm96@users.noreply.github.com>
2024-06-25 16:24:47 +0200

    Adds an example showcasing different camera types (#556)
    This MR fixes some of the docstrings related to the tile-rendering
    camera. Additionally, it adds a demo script showcasing the images
    obtained through the different camera implementations in IsaacLab.
    
    ## Type of change
    
    - New feature (non-breaking change which adds functionality)
    - This change requires a documentation update
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have run all the tests with `./isaaclab.sh --test` and they pass
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [x] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there

--- source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/camera/__init__.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/camera/__init__.py
@@ -6,7 +6,8 @@
 """Sub-module for camera wrapper around USD camera prim."""
 
 from .camera import Camera
-from .camera_cfg import CameraCfg, TiledCameraCfg
+from .camera_cfg import CameraCfg
 from .camera_data import CameraData
 from .tiled_camera import TiledCamera
+from .tiled_camera_cfg import TiledCameraCfg
 from .utils import *  # noqa: F401, F403
