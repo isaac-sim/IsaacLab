88156aa18b734ff304307718f52e9010ead78b91
Pascal <roth.pascal@outlook.de>
2024-06-27 11:05:07 +0200

    nicer separation between date and time

--- docker/container.sh
+++ docker/container.sh
@@ -359,7 +359,7 @@ case $mode in
     job)
         source $SCRIPT_DIR/.env.base
         # Get current date and time
-        current_datetime=$(date +"_%Y%m%d%H%M%S")
+        current_datetime=$(date +"_%Y%m%d_%H%M%S")
         # Append current date and time to CLUSTER_ORBIT_DIR
         CLUSTER_ORBIT_DIR="${CLUSTER_ORBIT_DIR}_${current_datetime}"
         # Check if singularity image exists on the remote host
