fdd8c6c54dc0f91b4dc6ebd777d1153db5eff5e3
Mayank Mittal <mittalma@leggedrobotics.com>
2024-07-02 06:07:06 +0200

    moves render check outside once

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_env.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_env.py
@@ -262,6 +262,10 @@ class ManagerBasedEnv:
         """
         # process actions
         self.action_manager.process_action(action)
+        # check if we need to do rendering within the physics loop
+        # note: checked here once to avoid multiple checks within the loop
+        is_rendering = self.sim.has_gui() or self.sim.has_rtx_sensors()
+
         # perform physics stepping
         for _ in range(self.cfg.decimation):
             self._sim_step_counter += 1
@@ -273,10 +277,8 @@ class ManagerBasedEnv:
             self.sim.step(render=False)
             # render between steps only if the GUI or an RTX sensor needs it
             # note: we assume the render interval to be the shortest accepted rendering interval.
-            #    If a camera needs rendering at a faster frequency, this will lead to errors
-            if self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
-                self.sim.has_gui() or self.sim.has_rtx_sensors()
-            ):
+            #    If a camera needs rendering at a faster frequency, this will lead to unexpected behavior.
+            if self._sim_step_counter % self.cfg.sim.render_interval == 0 and is_rendering:
                 self.sim.render()
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
