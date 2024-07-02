9b2ebfa4a51e3f147083274bc3ad9c307b530906
Mayank Mittal <mittalma@leggedrobotics.com>
2024-07-02 17:16:54 +0200

    fixes docs

--- source/extensions/omni.isaac.lab/omni/isaac/lab/sim/spawners/meshes/__init__.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/sim/spawners/meshes/__init__.py
@@ -9,8 +9,13 @@ NVIDIA Omniverse deals with meshes as `USDGeomMesh`_ prims. This sub-module prov
 configurations to spawn different types of meshes. Based on the configuration, the spawned prim can be:
 
 * a visual mesh (no physics)
-* a static collider (no rigid body)
+* a static collider (no rigid or deformable body)
 * a deformable body (with deformable properties)
+
+.. note::
+    While rigid body properties can be set on a mesh, it is recommended to use the
+    :mod:`omni.isaac.lab.sim.spawners.shapes` module to spawn rigid bodies. This is because USD shapes
+    are more optimized for physics simulations.
 
 .. _USDGeomMesh: https://openusd.org/release/api/class_usd_geom_mesh.html
 """
