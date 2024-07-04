3a42b123db47810d908525e11cde1473039ef83a
Mayank Mittal <mittalma@leggedrobotics.com>
2024-07-04 13:21:21 +0200

    makes repro for isaac team

--- source/extensions/omni.isaac.lab/test/deps/isaacsim/check_ref_count.py
+++ source/extensions/omni.isaac.lab/test/deps/isaacsim/check_ref_count.py
@@ -22,6 +22,7 @@ simulation_app = SimulationApp({"headless": True})
 
 """Rest everything follows."""
 
+import torch  # noqa: F401
 import ctypes
 import gc
 
@@ -55,6 +56,35 @@ ISAACLAB_NUCLEUS_DIR = f"{ISAAC_NUCLEUS_DIR}/IsaacLab"
 
 
 """
+Classes
+"""
+
+
+class AnymalArticulation:
+    """Anymal articulation class."""
+
+    def __init__(self):
+        """Initialize the Anymal articulation class."""
+        # resolve asset
+        usd_path = f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd"
+        # add asset
+        print("Loading robot from: ", usd_path)
+        prim_utils.create_prim("/World/Robot", usd_path=usd_path, translation=(0.0, 0.0, 0.6))
+
+        # Resolve robot prim paths
+        root_prim_path = "/World/Robot/base"
+        # Setup robot
+        self.view = ArticulationView(root_prim_path, name="ANYMAL")
+
+    def __del__(self):
+        """Delete the Anymal articulation class."""
+
+    def initialize(self):
+        """Initialize the Anymal view."""
+        self.view.initialize()
+
+
+"""
 Main
 """
 
@@ -69,47 +99,42 @@ def main():
     # this is needed to visualize the scene when flatcache is enabled
     set_carb_setting(sim._settings, "/persistent/omnihydra/useSceneGraphInstancing", True)
 
-    # -- Robot
-    # resolve asset
-    usd_path = f"{ISAACLAB_NUCLEUS_DIR}/Robots/ANYbotics/ANYmal-C/anymal_c.usd"
-    # add asset
-    print("Loading robot from: ", usd_path)
-    prim_utils.create_prim("/World/Robot", usd_path=usd_path, translation=(0.0, 0.0, 0.6))
+    # Create a dummy tensor for testing
+    # Uncommenting this line will yield a reference count of 1 for the robot (as desired)
+    # dummy_tensor = torch.zeros(1, device="cuda:0")
 
-    # Resolve robot prim paths
-    root_prim_path = "/World/Robot/base"
-    # Setup robot
-    robot_view = ArticulationView(root_prim_path, name="ANYMAL")
+    # Robot
+    robot = AnymalArticulation()
 
-    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot_view)).value)
-    print("Referrers of the robot view: ", gc.get_referrers(robot_view))
+    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot)).value)
+    print("Referrers of the robot view: ", gc.get_referrers(robot))
     print("---" * 10)
 
     # Play the simulator
     sim.reset()
 
-    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot_view)).value)
-    print("Referrers of the robot view: ", gc.get_referrers(robot_view))
+    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot)).value)
+    print("Referrers of the robot view: ", gc.get_referrers(robot))
     print("---" * 10)
 
-    robot_view.initialize()
+    robot.initialize()
 
-    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot_view)).value)
-    print("Referrers of the robot view: ", gc.get_referrers(robot_view))
+    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot)).value)
+    print("Referrers of the robot view: ", gc.get_referrers(robot))
     print("---" * 10)
 
     # Stop the simulator
     sim.stop()
 
-    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot_view)).value)
-    print("Referrers of the robot view: ", gc.get_referrers(robot_view))
+    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot)).value)
+    print("Referrers of the robot view: ", gc.get_referrers(robot))
     print("---" * 10)
 
     # Clean up
     sim.clear()
 
-    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot_view)).value)
-    print("Referrers of the robot view: ", gc.get_referrers(robot_view))
+    print("Reference count of the robot view: ", ctypes.c_long.from_address(id(robot)).value)
+    print("Referrers of the robot view: ", gc.get_referrers(robot))
     print("---" * 10)
 
 
