62afa6191b609fe064d2c0af9a42f17fde382504
Mayank Mittal <mittalma@leggedrobotics.com>
2024-07-30 10:08:36 +0200

    adds proper note

--- source/extensions/omni.isaac.lab/omni/isaac/lab/terrains/terrain_generator.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/terrains/terrain_generator.py
@@ -71,7 +71,8 @@ class TerrainGenerator:
     .. attention::
 
         The terrain generation has its own seed parameter. This is set using the :attr:`TerrainGeneratorCfg.seed`
-        parameter. If the seed is not set, the terrain generation is not reproducible.
+        parameter. If the seed is not set and the caching is disabled, the terrain generation will not be
+        reproducible.
 
     """
 
