6128a3fca1fd43578161340ae31d9c2becec2cbb
David Hoeller <dhoeller@nvidia.com>
2024-08-07 13:42:22 +0200

    Update train.py
    Signed-off-by: David Hoeller <dhoeller@nvidia.com>

--- source/standalone/workflows/rl_games/train.py
+++ source/standalone/workflows/rl_games/train.py
@@ -54,6 +54,7 @@ from rl_games.common import env_configurations, vecenv
 from rl_games.common.algo_observer import IsaacAlgoObserver
 from rl_games.torch_runner import Runner
 
+from omni.isaac.lab.utils.assets import retrieve_file_path
 from omni.isaac.lab.utils.dict import print_dict
 from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
 
@@ -61,7 +62,6 @@ import omni.isaac.lab_tasks  # noqa: F401
 from omni.isaac.lab_tasks.utils import load_cfg_from_registry, parse_env_cfg
 from omni.isaac.lab_tasks.utils.wrappers.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
 
-from omni.isaac.lab.utils.assets import retrieve_file_path
 
 def main():
     """Train with RL-Games agent."""
