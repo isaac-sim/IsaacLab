34cc91b559d4372bf20ae71253fec06c20b620ee
David Hoeller <dhoeller@nvidia.com>
2024-08-12 18:14:10 +0200

    Update train.py
    Signed-off-by: David Hoeller <dhoeller@nvidia.com>

--- source/standalone/workflows/rl_games/train.py
+++ source/standalone/workflows/rl_games/train.py
@@ -23,12 +23,12 @@ parser.add_argument("--num_envs", type=int, default=None, help="Number of enviro
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
 parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
 
-parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint.")
-parser.add_argument("--sigma", type=str, default=None, help="The policy's initial standard deviation.")
-
 parser.add_argument(
     "--distributed", action="store_true", default=False, help="Run training with multiple GPUs or nodes."
 )
+
+parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint.")
+parser.add_argument("--sigma", type=str, default=None, help="The policy's initial standard deviation.")
 parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
 
 # append AppLauncher cli args
