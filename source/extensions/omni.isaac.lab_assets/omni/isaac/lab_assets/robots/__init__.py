34dfb37b16beadadf791514500aa2993bb99cd8d
Pascal <roth.pascal@outlook.de>
2024-08-16 22:27:09 +0200

    formatter

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/robots/__init__.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/robots/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 from .allegro import *
 from .ant import *
 from .anymal import *
