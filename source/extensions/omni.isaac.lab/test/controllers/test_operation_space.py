a8b6ac2744ced1564065f012c44e0d15e0dd54a1
Özhan Özen <oezhan.oezen@gmail.com>
2024-09-04 17:53:16 +0200

    draft added for test_franka_pose_abs_fixed

--- source/extensions/omni.isaac.lab/test/controllers/test_operation_space.py
+++ source/extensions/omni.isaac.lab/test/controllers/test_operation_space.py
@@ -5,6 +5,7 @@
 
 """Launch Isaac Sim Simulator first."""
 
+from click import make_pass_decorator
 from omni.isaac.lab.app import AppLauncher, run_tests
 
 # launch omniverse app
@@ -22,12 +23,13 @@ from omni.isaac.cloner import GridCloner
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.assets import Articulation
 from omni.isaac.lab.controllers import OperationSpaceController, OperationSpaceControllerCfg
-from omni.isaac.lab.utils.math import compute_pose_error, subtract_frame_transforms
+from omni.isaac.lab.utils.math import compute_pose_error, subtract_frame_transforms, quat_rotate_inverse
 
 ##
 # Pre-defined configs
 ##
-from omni.isaac.lab_assets import FRANKA_PANDA_HIGH_PD_CFG, UR10_CFG  # isort:skip
+from omni.isaac.lab_assets import FRANKA_PANDA_CFG, FRANKA_PANDA_HIGH_PD_CFG  # isort:skip
+
 
 class TestOperationSpaceController(unittest.TestCase):
     """Test fixture for checking that differential IK controller tracks commands properly."""
@@ -61,14 +63,6 @@ class TestOperationSpaceController(unittest.TestCase):
             replicate_physics=True,
         )
 
-        # Define goals for the arm [xyz, quat_wxyz]
-        ee_goals_set = [
-            [0.5, 0.5, 0.7, 0.707, 0, 0.707, 0],
-            [0.5, -0.4, 0.6, 0.707, 0.707, 0.0, 0.0],
-            [0.5, 0, 0.5, 0.0, 1.0, 0.0, 0.0],
-        ]
-        self.ee_pose_b_des_set = torch.tensor(ee_goals_set, device=self.sim.device)
-
     def tearDown(self):
         """Stops simulator after each test."""
         # stop simulation
@@ -81,41 +75,163 @@ class TestOperationSpaceController(unittest.TestCase):
     Test fixtures.
     """
 
-    def test_franka_position_abs(self):
-        """Test absolute position control."""
+    def test_franka_pose_abs_fixed(self):
+        """Test absolute pose control with fixed impedance."""
         robot_cfg = FRANKA_PANDA_HIGH_PD_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+        robot_cfg.spawn.rigid_props.disable_gravity = True
         robot = Articulation(cfg=robot_cfg)
 
-        ctrl_cfg = OperationSpaceControllerCfg()
-        controller = OperationSpaceController(ctrl_cfg,....)
+        # Create IK controller
+        opc_cfg = OperationSpaceControllerCfg(command_types=["pose_abs"], impedance_mode="fixed", stiffness=100.0, damping_ratio=1.0)
+        opc = OperationSpaceController(opc_cfg, num_envs=self.num_envs, device=self.sim.device)
 
-    def test_franka_pose_abs(self):
-        """Test absolute pose control."""
+        # Define goals for the arm [xyz, quat_wxyz]
+        ee_goals_set = [
+            [0.5, 0.5, 0.7, 0.707, 0, 0.707, 0],
+            [0.5, -0.4, 0.6, 0.707, 0.707, 0.0, 0.0],
+            [0.5, 0, 0.5, 0.0, 1.0, 0.0, 0.0],
+        ]
+        ee_pose_b_des_set = torch.tensor(ee_goals_set, device=self.sim.device)
 
-    def test_franka_force_abs(self):
-        """test absolute force control."""
+        self._run_op_space_controller(robot, opc, "panda_hand", ["panda_joint.*"], ee_pose_b_des_set)
 
-    def test_franka_fixed(self):
-        """Tests operational space controller for franka using fixed impedance."""
+    # def test_franka_position_abs(self):
+    #     """Test absolute position control."""
+
+    # def test_franka_force_abs(self):
+    #     """test absolute force control."""
+
+    # def test_franka_fixed(self):
+    #     """Tests operational space controller for franka using fixed impedance."""
         
-    def test_franka_variable_kp(self):
-        """Tests operational space controller for franka using variable stiffness impedance."""
+    # def test_franka_variable_kp(self):
+    #     """Tests operational space controller for franka using variable stiffness impedance."""
 
-    def test_franka_variable(self):
-        """Tests operational space controller for franka using variable stiffness and damping ratio impedance."""
+    # def test_franka_variable(self):
+    #     """Tests operational space controller for franka using variable stiffness and damping ratio impedance."""
 
-
-    def test_franka_gravity_compensation(self):
-        """Tests operational space control with gravity compensation."""
+    # def test_franka_gravity_compensation(self):
+    #     """Tests operational space control with gravity compensation."""
     
-    def test_franka_inertial_compensation(self):
-        """Tests operational space control with inertial compensation."""
+    # def test_franka_inertial_compensation(self):
+    #     """Tests operational space control with inertial compensation."""
     
-    def test_franka_control_hybrid_motion_force(self):
-        """Tests operational space control for hybrid motion and force control."""
+    # def test_franka_control_hybrid_motion_force(self):
+    #     """Tests operational space control for hybrid motion and force control."""
 
     """
     Helper functions
     """
 
-    def _run_op_space_controller(self):
+    def _run_op_space_controller(
+        self,
+        robot: Articulation,
+        opc: OperationSpaceController,
+        ee_frame_name: str,
+        arm_joint_names: list[str],
+        targets: torch.tensor,
+    ):
+        # Define simulation stepping
+        sim_dt = self.sim.get_physics_dt()
+        # Play the simulator
+        self.sim.reset()
+
+        # Obtain the frame index of the end-effector
+        ee_frame_idx = robot.find_bodies(ee_frame_name)[0][0]
+        ee_jacobi_idx = ee_frame_idx - 1
+        # Obtain joint indices
+        arm_joint_ids = robot.find_joints(arm_joint_names)[0]
+        # Update existing buffers
+        # Note: We need to update buffers before the first step for the controller.
+        robot.update(dt=sim_dt)
+
+        # Track the given command
+        current_goal_idx = 0
+        # Current goal for the arm
+        ee_pose_b_des = torch.zeros(self.num_envs, opc.action_dim, device=self.sim.device)
+        ee_pose_b_des[:] = targets[current_goal_idx]
+        # Compute current pose of the end-effector
+        ee_pose_w = robot.data.body_state_w[:, ee_frame_idx, 0:7]
+        root_pose_w = robot.data.root_state_w[:, 0:7]
+        ee_pos_b, ee_quat_b = subtract_frame_transforms(
+            root_pose_w[:, 0:3], root_pose_w[:, 3:7], ee_pose_w[:, 0:3], ee_pose_w[:, 3:7]
+        )
+
+        zero_joint_efforts = torch.zeros(self.num_envs, robot.num_joints, device=self.sim.device)
+        joint_efforts = torch.zeros(self.num_envs, len(arm_joint_ids), device=self.sim.device)
+
+        # Now we are ready!
+        for count in range(1500):
+            # reset every 250 steps
+            if count % 250 == 0:
+                # check that we converged to the goal
+                if count > 0:
+                    pos_error, rot_error = compute_pose_error(
+                        ee_pos_b, ee_quat_b, ee_pose_b_des[:, 0:3], ee_pose_b_des[:, 3:7]
+                    )
+                    pos_error_norm = torch.norm(pos_error, dim=-1)
+                    rot_error_norm = torch.norm(rot_error, dim=-1)
+                    # desired error (zer)
+                    des_error = torch.zeros_like(pos_error_norm)
+                    # check convergence
+                    torch.testing.assert_close(pos_error_norm, des_error, rtol=0.0, atol=1e-3)
+                    torch.testing.assert_close(rot_error_norm, des_error, rtol=0.0, atol=1e-3)
+                # reset joint state
+                default_joint_pos = robot.data.default_joint_pos.clone()
+                default_joint_vel = robot.data.default_joint_vel.clone()
+                robot.write_joint_state_to_sim(default_joint_pos, default_joint_vel)
+                robot.set_joint_effort_target(zero_joint_efforts)
+                robot.write_data_to_sim()
+                robot.reset()
+                # reset target pose command
+                ee_pose_b_des[:] = targets[current_goal_idx]
+                opc.reset()
+                opc.set_command(ee_pose_b_des)
+                # update goal for next iteration
+                current_goal_idx = (current_goal_idx + 1) % len(targets)
+            else:
+                # at reset, the jacobians are not updated to the latest state
+                # so we MUST skip the first step
+                # obtain quantities from simulation
+                jacobian = robot.root_physx_view.get_jacobians()[:, ee_jacobi_idx, :, arm_joint_ids]
+                mass_matrix = robot.root_physx_view.get_mass_matrices()[:, arm_joint_ids, arm_joint_ids]
+
+                ee_pose_w = robot.data.body_state_w[:, ee_frame_idx, 0:7]
+                root_pose_w = robot.data.root_state_w[:, 0:7]
+                # compute frame in root frame
+                ee_pos_b, ee_quat_b = subtract_frame_transforms(
+                    root_pose_w[:, 0:3], root_pose_w[:, 3:7], ee_pose_w[:, 0:3], ee_pose_w[:, 3:7]
+                )
+                ee_pose_b = torch.cat([ee_pos_b, ee_quat_b], dim=-1)
+
+                # Extract end-effector velocity in the world frame
+                ee_vel_w = robot.data.body_vel_w[:, ee_frame_idx, :]  # Shape (num_robots, 6), includes linear and angular velocity
+                # Extract root velocity in the world frame
+                root_vel_w = robot.data.root_vel_w  # Shape (num_robots, 6), includes linear and angular velocity
+                # Compute the relative velocity in the world frame
+                relative_vel_w = ee_vel_w - root_vel_w
+                # Transform velocities from world frame to root frame
+                ee_lin_vel_b = quat_rotate_inverse(robot.data.root_quat_w, relative_vel_w[:, 0:3])
+                ee_ang_vel_b = quat_rotate_inverse(robot.data.root_quat_w, relative_vel_w[:, 3:6])
+                ee_vel_b = torch.cat([ee_lin_vel_b, ee_ang_vel_b], dim=-1)  # Shape (num_robots, 6)
+
+                # compute the joint commands
+                joint_efforts = opc.compute(jacobian=jacobian, ee_pose=ee_pose_b, ee_vel=ee_vel_b, mass_matrix=mass_matrix)
+                robot.set_joint_effort_target(joint_efforts, arm_joint_ids)
+                robot.write_data_to_sim()
+
+            pos_error, rot_error = compute_pose_error(
+                ee_pos_b, ee_quat_b, ee_pose_b_des[:, 0:3], ee_pose_b_des[:, 3:7]
+            )
+            pos_error_norm = torch.norm(pos_error, dim=-1)
+            rot_error_norm = torch.norm(rot_error, dim=-1)
+            print("pos_error_norm", pos_error_norm[0], "rot_error_norm", rot_error_norm[0])
+
+            # perform step
+            self.sim.step(render=False)
+            # update buffers
+            robot.update(sim_dt)
+
+
+if __name__ == "__main__":
+    run_tests()
