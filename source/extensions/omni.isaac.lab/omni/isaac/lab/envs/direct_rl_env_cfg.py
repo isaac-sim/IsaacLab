daae87304ebb51cd9ea34114f825698f4ac2e4f9
Mayank Mittal <mittalma@leggedrobotics.com>
2024-09-09 17:39:01 +0200

    run formatter

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/direct_rl_env_cfg.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/direct_rl_env_cfg.py
@@ -43,7 +43,7 @@ class DirectRLEnvCfg:
     # general settings
     seed: int | None = None
     """The seed for the random number generator. Defaults to None, in which case the seed is not set.
-    
+
     Note:
       The seed is set at the beginning of the environment initialization. This ensures that the environment
       creation is deterministic and behaves similarly across different runs.
