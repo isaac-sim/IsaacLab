cc246c8e7627bfe596e9807510e57643501c726e
Luca <luca.j@online.de>
2024-09-20 16:52:53 +0200

    running ur5 sim env with gripper

--- source/standalone/ur5rl/run_ur5_rl_env.py
+++ source/standalone/ur5rl/run_ur5_rl_env.py
@@ -32,13 +32,39 @@ simulation_app = app_launcher.app
 
 import torch
 
-from ur5_rl_env import HawUr5Cfg, HawUr5Env
+from ur5_rl_env import HawUr5EnvCfg, HawUr5Env
+
+
+def gripper_steer(action: float) -> torch.Tensor:
+    """Steer the individual gripper joints.
+       This function translates a single action
+       between -1 and 1 to the gripper joint position targets.
+       value to the gripper joint position targets.
+
+    Args:
+        action (float): Action to steer the gripper.
+
+    Returns:
+        torch.Tensor: Gripper joint position targets.
+    """
+    # create joint position targets
+    gripper_joint_pos = torch.tensor(
+        [
+            36 * action,  # "left_outer_knuckle_joint",
+            -36 * action,  # "left_inner_finger_joint",
+            -36 * action,  # "left_inner_knuckle_joint",
+            -36 * action,  # "right_inner_knuckle_joint",
+            36 * action,  # "right_outer_knuckle_joint",
+            36 * action,  # "right_inner_finger_joint",
+        ]
+    )
+    return gripper_joint_pos
 
 
 def main():
     """Main function."""
     # create environment configuration
-    env_cfg = HawUr5Cfg()
+    env_cfg = HawUr5EnvCfg()
     env_cfg.scene.num_envs = args_cli.num_envs
     # setup RL environment
     env = HawUr5Env(cfg=env_cfg)
@@ -53,12 +79,34 @@ def main():
                 env.reset()
                 print("-" * 80)
                 print("[INFO]: Resetting environment...")
-            # sample random actions
-            joint_efforts = torch.zeros(1)
-            # step the environment
-            obs, rew, terminated, truncated, info = env.step(joint_efforts)
+
+            # Sample test action for the gripper
+            gripper_action = -1 + count % 2
+            # 2Pi/360 helper var
+            deg2rad = 0.017453292519943295
+            # create a tensor for joint position targets with 12 values (6 for joints, 6 for gripper)
+            joint_pos_targets = torch.tensor(
+                [
+                    [
+                        0 * deg2rad,
+                        -110 * deg2rad,
+                        110 * deg2rad,
+                        -180 * deg2rad,
+                        -90 * deg2rad,
+                        0 * deg2rad,
+                        gripper_action,
+                    ]
+                ]
+                * env_cfg.scene.num_envs
+                # [[0, -1, -1, 1, 0, 0, gripper_action]]
+                # * env_cfg.scene.num_envs
+            )
+
+            # Step the environment
+            obs, rew, terminated, truncated, info = env.step(joint_pos_targets)
+
             # update counter
-            count += 1
+            count += 0.01
 
     # close the environment
     env.close()
--- source/standalone/ur5rl/ur5_rl_env.py
+++ source/standalone/ur5rl/ur5_rl_env.py
@@ -18,9 +18,9 @@ from omni.isaac.lab.utils.math import sample_uniform
 
 
 @configclass
-class HawUr5Cfg(DirectRLEnvCfg):
+class HawUr5EnvCfg(DirectRLEnvCfg):
     # env
-    num_actions = 3
+    num_actions = 12
     num_observations = 4
     num_states = 5
     reward_scale_example = 1.0
@@ -29,6 +29,8 @@ class HawUr5Cfg(DirectRLEnvCfg):
 
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
+
+    # Gripper parameters
 
     # robot
     robot_cfg: ArticulationCfg = ArticulationCfg(
@@ -46,28 +48,63 @@ class HawUr5Cfg(DirectRLEnvCfg):
             ),
         },
     )
-    cart_dof_name = "slider_to_cart"
-    pole_dof_name = "cart_to_pole"
+
+    arm_dof_name = [
+        "shoulder_pan_joint",  # 0
+        "shoulder_lift_joint",  # -110
+        "elbow_joint",  # 110
+        "wrist_1_joint",  # -180
+        "wrist_2_joint",  # -90
+        "wrist_3_joint",  # 0
+    ]
+    gripper_dof_name = [
+        "left_outer_knuckle_joint",
+        "left_inner_finger_joint",
+        "left_inner_knuckle_joint",
+        "right_inner_knuckle_joint",
+        "right_outer_knuckle_joint",
+        "right_inner_finger_joint",
+    ]
+
+    haw_ur5_dof_name = arm_dof_name + gripper_dof_name
+
+    action_dim = len(arm_dof_name) + len(gripper_dof_name)
 
     # scene
     scene: InteractiveSceneCfg = InteractiveSceneCfg(
-        num_envs=10, env_spacing=4.0, replicate_physics=True
+        num_envs=10, env_spacing=2.0, replicate_physics=True
     )
+
+    # reset conditions
+    # ...
+
+    # reward scales
+    # ...
 
 
 class HawUr5Env(DirectRLEnv):
-    cfg: HawUr5Cfg
+    cfg: HawUr5EnvCfg
 
-    def __init__(self, cfg: HawUr5Cfg, render_mode: str | None = None, **kwargs):
+    def __init__(self, cfg: HawUr5EnvCfg, render_mode: str | None = None, **kwargs):
         super().__init__(cfg, render_mode, **kwargs)
 
+        self._arm_dof_idx, _ = self.robot.find_joints(self.cfg.arm_dof_name)
+        self._gripper_dof_idx, _ = self.robot.find_joints(self.cfg.gripper_dof_name)
+        self.haw_ur5_dof_idx, _ = self.robot.find_joints(self.cfg.haw_ur5_dof_name)
         self.action_scale = self.cfg.action_scale
+
+        self.joint_pos = self.robot.data.joint_pos
+        self.joint_vel = self.robot.data.joint_vel
+
+        self.action_dim = len(self._arm_dof_idx) + len(self._gripper_dof_idx)
 
     def _setup_scene(self):
         # add Articulation
         self.robot = Articulation(self.cfg.robot_cfg)
+
         # add ground plane
         spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
+
         # clone, filter, and replicate
         self.scene.clone_environments(copy_from_source=False)
         self.scene.filter_collisions(global_prim_paths=[])
@@ -80,14 +117,47 @@ class HawUr5Env(DirectRLEnv):
         light_cfg.func("/World/Light", light_cfg)
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
-        self.actions = self.cfg.action_scale * actions.clone()
+        # Separate the main joint actions (first 6) and the gripper action (last one)
+        main_joint_actions = actions[:, :6]  # Shape: (num_envs, 6)
+        gripper_actions = actions[:, 6]  # Shape: (num_envs)
+        main_joint_actions = self.cfg.action_scale * main_joint_actions.clone()
+
+        # Convert each gripper action to the corresponding 6 gripper joint positions (min max 36 joint limit)
+        gripper_joint_positions = torch.stack(
+            [
+                36 * gripper_actions,  # "left_outer_knuckle_joint"
+                -36 * gripper_actions,  # "left_inner_finger_joint"
+                -36 * gripper_actions,  # "left_inner_knuckle_joint"
+                -36 * gripper_actions,  # "right_inner_knuckle_joint"
+                36 * gripper_actions,  # "right_outer_knuckle_joint"
+                36 * gripper_actions,  # "right_inner_finger_joint"
+            ],
+            dim=1,
+        )  # Shape: (num_envs, 6)
+
+        # Concatenate the main joint actions with the gripper joint positions
+        full_joint_actions = torch.cat(
+            (main_joint_actions, gripper_joint_positions), dim=1
+        )  # Shape: (num_envs, 12)
+
+        # Apply action scaling and assign to self.actions
+        self.actions = full_joint_actions
 
     def _apply_action(self) -> None:
-        # self.cartpole.set_joint_effort_target(self.actions, joint_ids=self._cart_dof_idx)
-        pass
+        self.robot.set_joint_position_target(
+            self.actions, joint_ids=self.haw_ur5_dof_idx
+        )
 
     def _get_observations(self) -> dict:
-        obs = torch.ones(1, device=self.device)
+        obs = torch.cat(
+            (
+                self.joint_pos[self._arm_dof_idx].unsqueeze(dim=1),
+                self.joint_vel[self._arm_dof_idx].unsqueeze(dim=1),
+                self.joint_pos[self._gripper_dof_idx].unsqueeze(dim=1),
+                self.joint_vel[self._gripper_dof_idx].unsqueeze(dim=1),
+            ),
+            dim=-1,
+        )
         observations = {"policy": obs}
         return observations
 
@@ -101,4 +171,21 @@ class HawUr5Env(DirectRLEnv):
         return out_of_bounds, time_out
 
     def _reset_idx(self, env_ids: Sequence[int] | None):
-        super()._reset_idx(env_ids)
+        if env_ids is None:
+            env_ids = self.robot._ALL_INDICES  # type: ignore
+
+        # General resetting tasks (timers etc.)
+        super()._reset_idx(env_ids)  # type: ignore
+
+        joint_pos = self.robot.data.default_joint_pos[env_ids]
+        # TODO Add random noise to joint positions for domain randomization
+
+        joint_vel = self.robot.data.default_joint_vel[env_ids]
+
+        default_root_state = self.robot.data.default_root_state[env_ids]
+
+        self.joint_pos[env_ids] = joint_pos
+        self.joint_vel[env_ids] = joint_vel
+        # self.robot.write_root_pose_to_sim(default_root_state[:, :7], env_ids)
+        self.robot.write_root_velocity_to_sim(default_root_state[:, 7:], env_ids)
+        self.robot.write_joint_state_to_sim(joint_pos, joint_vel, None, env_ids)
