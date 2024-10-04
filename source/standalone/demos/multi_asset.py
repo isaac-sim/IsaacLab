ad1526743a6f3254354e2716eab8846322d42658
Mayank Mittal <mittalma@leggedrobotics.com>
2024-10-04 20:23:00 +0200

    adds multi asset py

--- source/standalone/demos/multi_asset.py
+++ source/standalone/demos/multi_asset.py
@@ -8,7 +8,7 @@
 .. code-block:: bash
 
     # Usage
-    ./orbit.sh -p source/standalone/demos/multi_object.py --num_envs 2048
+    ./isaaclab.sh -p source/standalone/demos/multi_object.py --num_envs 2048
 
 """
 
@@ -36,12 +36,9 @@ simulation_app = app_launcher.app
 """Rest everything follows."""
 
 import random
-import re
-from dataclasses import MISSING
 
-import omni.isaac.core.utils.prims as prim_utils
 import omni.usd
-from pxr import Gf, Sdf, Usd
+from pxr import Sdf, Gf
 
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.assets import AssetBaseCfg, RigidObjectCfg
@@ -64,87 +61,13 @@ def randomize_color_pxr(prim_path_expr: str):
     with Sdf.ChangeBlock():
         for prim_path in prim_paths:
             # spawn single instance
-            env_spec = Sdf.CreatePrimInLayer(stage.GetRootLayer(), prim_path)
+            prim_spec = Sdf.CreatePrimInLayer(stage.GetRootLayer(), prim_path)
 
             # DO YOUR OWN OTHER KIND OF RANDOMIZATION HERE!
             # Note: Just need to acquire the right attribute about the property you want to set
             # Here is an example on setting color randomly
-            color_spec = env_spec.GetAttributeAtPath(prim_path + "/geometry/material/Shader.inputs:diffuseColor")
+            color_spec = prim_spec.GetAttributeAtPath(prim_path + "/geometry/material/Shader.inputs:diffuseColor")
             color_spec.default = Gf.Vec3f(random.random(), random.random(), random.random())
-
-
-##
-# Multi-Object Spawning
-##
-
-
-def spawn_multi_object_randomly(
-    prim_path: str,
-    cfg: MultiAssetCfg,
-    translation: tuple[float, float, float] | None = None,
-    orientation: tuple[float, float, float, float] | None = None,
-) -> Usd.Prim:
-    # resolve: {SPAWN_NS}/AssetName
-    # note: this assumes that the spawn namespace already exists in the stage
-    root_path, asset_path = prim_path.rsplit("/", 1)
-    # check if input is a regex expression
-    # note: a valid prim path can only contain alphanumeric characters, underscores, and forward slashes
-    is_regex_expression = re.match(r"^[a-zA-Z0-9/_]+$", root_path) is None
-
-    # resolve matching prims for source prim path expression
-    if is_regex_expression and root_path != "":
-        source_prim_paths = sim_utils.find_matching_prim_paths(root_path)
-        # if no matching prims are found, raise an error
-        if len(source_prim_paths) == 0:
-            raise RuntimeError(
-                f"Unable to find source prim path: '{root_path}'. Please create the prim before spawning."
-            )
-    else:
-        source_prim_paths = [root_path]
-
-    # spawn everything first in a "Dataset" prim
-    prim_utils.create_prim("/World/Dataset", "Scope")
-    proto_prim_paths = list()
-    for index, asset_cfg in enumerate(cfg.assets_cfg):
-        # spawn single instance
-        proto_prim_path = f"/World/Dataset/Object_{index:02d}"
-        asset_cfg.func(proto_prim_path, asset_cfg, translation=translation, orientation=orientation)
-        # append to proto prim paths
-        proto_prim_paths.append(proto_prim_path)
-
-    # resolve prim paths for spawning and cloning
-    prim_paths = [f"{source_prim_path}/{asset_path}" for source_prim_path in source_prim_paths]
-
-    # acquire stage
-    stage = omni.usd.get_context().get_stage()
-
-    # manually clone prims if the source prim path is a regex expression
-    with Sdf.ChangeBlock():
-        for prim_path in prim_paths:
-            # spawn single instance
-            env_spec = Sdf.CreatePrimInLayer(stage.GetRootLayer(), prim_path)
-            # randomly select an asset configuration
-            proto_path = random.choice(proto_prim_paths)
-            # inherit the proto prim
-            # env_spec.inheritPathList.Prepend(Sdf.Path(proto_path))
-            Sdf.CopySpec(env_spec.layer, Sdf.Path(proto_path), env_spec.layer, Sdf.Path(prim_path))
-
-    # delete the dataset prim after spawning
-    prim_utils.delete_prim("/World/Dataset")
-
-    # return the prim
-    return prim_utils.get_prim_at_path(prim_paths[0])
-
-
-@configclass
-class MultiAssetCfg(sim_utils.SpawnerCfg):
-    """Configuration parameters for loading multiple assets randomly."""
-
-    func = spawn_multi_object_randomly
-    """Function to spawn the asset."""
-
-    assets_cfg: list[sim_utils.SpawnerCfg] = MISSING
-    """List of asset configurations to spawn."""
 
 
 ##
@@ -167,7 +90,7 @@ class MultiObjectSceneCfg(InteractiveSceneCfg):
     # articulation
     object: RigidObjectCfg = RigidObjectCfg(
         prim_path="/World/envs/env_.*/Objects",
-        spawn=MultiAssetCfg(
+        spawn=sim_utils.MultiAssetSpawnerCfg(
             assets_cfg=[
                 sim_utils.ConeCfg(
                     radius=0.3,
@@ -191,7 +114,8 @@ class MultiObjectSceneCfg(InteractiveSceneCfg):
                     collision_props=sim_utils.CollisionPropertiesCfg(),
                     visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0), metallic=0.2),
                 ),
-            ]
+            ],
+            random_choice=False,
         ),
         init_state=RigidObjectCfg.InitialStateCfg(pos=(0.0, 0.0, 0.3)),
     )
