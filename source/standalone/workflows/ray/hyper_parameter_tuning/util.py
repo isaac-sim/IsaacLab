dc9fb3f77307959f9f7c9977b4277f89f1c362b8
Gary Lvov <glvov@theaiinstitute.com>
2024-10-08 11:05:40 -0400

    start argparser

--- source/standalone/workflows/ray/hyper_parameter_tuning/util.py
+++ source/standalone/workflows/ray/hyper_parameter_tuning/util.py
@@ -3,14 +3,51 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# import argparse
+import argparse
 
-# from ray import tune
-# from ray.train import RunConfig
+from ray import tune
+from ray.train import RunConfig
 
 
-# def parse_tune_args() -> argparse.Namespace:
-#     arg_parser = argparse.ArgumentParser("Submit distributed hyperparameter tuning jobs.")
+class IsaacLabTrainable(tune.Trainable):
+    def setup(self, config):
+        pass
+
+    def step(self):
+        pass
+
+#     parser.add_argument(
+#     "--autotune_max_percentage_util",
+#     nargs="+",
+#     type=float,
+#     default=[100.0, 80.0, 80.0, 80.0],
+#     required=False,
+#     help=(
+#         "The system utilization percentage thresholds to reach before an autotune is finished. "
+#         "If any one of these limits are hit, the autotune stops."
+#         "Thresholds are, in order, maximum CPU percentage utilization,"
+#         "maximum RAM percentage utilization, maximum GPU compute percent utilization, "
+#         "amd maximum GPU memory utilization."
+#     ),
+# )
+
+
+def parse_tune_args() -> argparse.Namespace:
+    arg_parser = argparse.ArgumentParser("Submit distributed hyperparameter tuning jobs.")
+    arg_parser.add_argument("executable",
+                            type=str,
+                            default="/workspace/isaaclab/_isaac_sim/python.sh",
+                            help=("what executable to run the train script with "))
+    arg_parser.add_arg
+    arg_parser.add_argument(
+        "--args",
+        nargs="+",
+        type=str,
+        default=[],
+        required=False,
+        help=("Arguments to pass to the training script."
+                "For example, you could pass the task here."))
+    
 
 
 # if __name__ == "__main__":
