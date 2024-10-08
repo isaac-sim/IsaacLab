c7d0756ff6a539cf35a57fdb09994c799e2b1965
Fangzhou Yu <fyu@theaiinstitute.com>
2024-10-07 22:13:58 -0400

    add torchrl workflow

---
+++ source/standalone/workflows/torchrl/cli_args.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2024 Boston Dynamics AI Institute LLC. All rights reserved.
+
+from __future__ import annotations
+
+import argparse
+import random
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from omni.isaac.lab_tasks.utils.wrappers.torchrl import OnPolicyPPORunnerCfg
+
+
+def add_torchrl_args(parser: argparse.ArgumentParser):
+    """Add TorchRL arguments to the parser.
+
+    Adds the following fields to argparse:
+        - "--experiment_name" : Name of the experiment folder where logs will be stored (default: None).
+        - "--run_name" : Run name suffix to the log directory (default: None).
+        - "--resume" : Whether to resume from a checkpoint (default: None).
+        - "--load_run" : Name of the run folder to resume from (default: None).
+        - "--checkpoint" : Checkpoint file to resume from (default: None).
+        - "--logger" : Logger module to use (default: None).
+        - "--log_project_name" : Name of the logging project when using wandb or neptune (default: None).
+    Args:
+        parser: The parser to add the arguments to.
+    """
+    # create a new argument group
+    arg_group = parser.add_argument_group("torchrl", description="Arguments for RSL-RL agent.")
+    # -- experiment arguments
+    arg_group.add_argument(
+        "--experiment_name",
+        type=str,
+        default=None,
+        help="Name of the experiment folder where logs will be stored.",
+    )
+    arg_group.add_argument(
+        "--run_name",
+        type=str,
+        default=None,
+        help="Run name suffix to the log directory.",
+    )
+    # -- load arguments
+    arg_group.add_argument(
+        "--resume",
+        type=bool,
+        default=None,
+        help="Whether to resume from a checkpoint.",
+    )
+    arg_group.add_argument(
+        "--load_run",
+        type=str,
+        default=None,
+        help="Name of the run folder to resume from.",
+    )
+    arg_group.add_argument(
+        "--checkpoint",
+        type=str,
+        default=None,
+        help="Checkpoint file to resume from.",
+    )
+    # -- logger arguments
+    arg_group.add_argument(
+        "--logger",
+        type=str,
+        default=None,
+        choices={"wandb", "tensorboard", "neptune"},
+        help="Logger module to use.",
+    )
+    arg_group.add_argument(
+        "--log_project_name",
+        type=str,
+        default=None,
+        help="Name of the logging project when using wandb or neptune.",
+    )
+
+
+def parse_torchrl_cfg(task_name: str, args_cli: argparse.Namespace) -> OnPolicyPPORunnerCfg:
+    """Parse configuration for RSL-RL agent based on inputs.
+
+    Args:
+        task_name: The name of the environment.
+        args_cli: The command line arguments.
+
+    Returns:
+        The parsed configuration for RSL-RL agent based on inputs.
+    """
+    from omni.isaac.lab_tasks.utils.parse_cfg import load_cfg_from_registry
+
+    # load the default configuration
+    torchrl_cfg: OnPolicyPPORunnerCfg = load_cfg_from_registry(task_name, "torchrl_cfg_entry_point")
+
+    # override the default configuration with CLI arguments
+    torchrl_cfg.device = "cpu" if args_cli.cpu else f"cuda:{args_cli.physics_gpu}"
+
+    # override the default configuration with CLI arguments
+    if args_cli.seed is not None:
+        torchrl_cfg.seed = args_cli.seed
+    if args_cli.resume is not None:
+        torchrl_cfg.resume = args_cli.resume
+    if args_cli.load_run is not None:
+        torchrl_cfg.load_run = args_cli.load_run
+    if args_cli.checkpoint is not None:
+        torchrl_cfg.load_checkpoint = args_cli.checkpoint
+    if args_cli.run_name is not None:
+        torchrl_cfg.run_name = args_cli.run_name
+    if args_cli.logger is not None:
+        torchrl_cfg.logger = args_cli.logger
+    # set the project name for wandb and neptune
+    if torchrl_cfg.logger == "wandb" and args_cli.log_project_name:
+        torchrl_cfg.wandb_project = args_cli.log_project_name
+
+    return torchrl_cfg
+
+def update_torchrl_cfg(agent_cfg: OnPolicyPPORunnerCfg, args_cli: argparse.Namespace):
+    """Update configuration for torchrl agent based on inputs.
+
+    Args:
+        agent_cfg: The configuration for torchrl agent.
+        args_cli: The command line arguments.
+
+    Returns:
+        The updated configuration for torchrl agent based on inputs.
+    """
+    # override the default configuration with CLI arguments
+    if hasattr(args_cli, "seed") and args_cli.seed is not None:
+        # randomly sample a seed if seed = -1
+        if args_cli.seed == -1:
+            args_cli.seed = random.randint(0, 10000)
+        agent_cfg.seed = args_cli.seed
+    if args_cli.resume is not None:
+        agent_cfg.resume = args_cli.resume
+    if args_cli.load_run is not None:
+        agent_cfg.load_run = args_cli.load_run
+    if args_cli.checkpoint is not None:
+        agent_cfg.load_checkpoint = args_cli.checkpoint
+    if args_cli.run_name is not None:
+        agent_cfg.run_name = args_cli.run_name
+    if args_cli.logger is not None:
+        agent_cfg.logger = args_cli.logger
+    # set the project name for wandb and neptune
+    if agent_cfg.logger in {"wandb"} and args_cli.log_project_name:
+        agent_cfg.wandb_project = args_cli.log_project_name
+
+    return agent_cfg
+
---
+++ source/standalone/workflows/torchrl/train.py
@@ -0,0 +1,139 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Script to train RL agent with torchrl."""
+
+"""Launch Isaac Sim Simulator first."""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+import sys
+
+from omni.isaac.lab.app import AppLauncher
+
+# local imports
+import cli_args  # isort: skip
+
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Train an RL agent with RSL-RL.")
+parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
+parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
+parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
+parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
+parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
+# append RSL-RL cli arguments
+cli_args.add_torchrl_args(parser)
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+args_cli, hydra_args = parser.parse_known_args()
+
+# always enable cameras to record video
+if args_cli.video:
+    args_cli.enable_cameras = True
+
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import gymnasium as gym
+import os
+import torch
+from datetime import datetime
+
+from omni.isaac.lab_tasks.utils.wrappers.torchrl import TorchRLEnvWrapper, OnPolicyPPORunnerCfg, OnPolicyPPORunner
+
+from omni.isaac.lab.envs import (
+    ManagerBasedRLEnvCfg
+)
+from omni.isaac.lab.utils.dict import print_dict
+from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
+from omni.isaac.lab_tasks.utils import get_checkpoint_path
+from omni.isaac.lab_tasks.utils.hydra import hydra_task_config
+
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = False
+
+@hydra_task_config(args_cli.task, "torchrl_cfg_entry_point")
+def main(env_cfg: ManagerBasedRLEnvCfg, agent_cfg: OnPolicyPPORunnerCfg):
+    """Train with RSL-RL agent."""
+    # override configurations with non-hydra CLI arguments
+    agent_cfg = cli_args.update_torchrl_cfg(agent_cfg, args_cli)
+    env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
+    agent_cfg.max_iterations = (
+        args_cli.max_iterations if args_cli.max_iterations is not None else agent_cfg.max_iterations
+    )
+
+    # set the environment seed
+    # note: certain randomizations occur in the environment initialization so we set the seed here
+    env_cfg.seed = agent_cfg.seed
+    env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
+
+    # specify directory for logging experiments
+    log_root_path = os.path.join("logs", "torchrl", agent_cfg.experiment_name)
+    log_root_path = os.path.abspath(log_root_path)
+    print(f"[INFO] Logging experiment in directory: {log_root_path}")
+    # specify directory for logging runs: {time-stamp}_{run_name}
+    log_dir = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if agent_cfg.run_name:
+        log_dir += f"_{agent_cfg.run_name}"
+    log_dir = os.path.join(log_root_path, log_dir)
+
+    # create isaac environment
+    env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
+    # wrap for video recording
+    if args_cli.video:
+        video_kwargs = {
+            "video_folder": os.path.join(log_dir, "videos", "train"),
+            "step_trigger": lambda step: step % args_cli.video_interval == 0,
+            "video_length": args_cli.video_length,
+            "disable_logger": True,
+        }
+        print("[INFO] Recording videos during training.")
+        print_dict(video_kwargs, nesting=4)
+        env = gym.wrappers.RecordVideo(env, **video_kwargs)
+
+    # wrap environment for TorchRL
+    env = TorchRLEnvWrapper(env)
+
+    # create runner from rsl-rl
+    runner = OnPolicyPPORunner(env, agent_cfg, log_dir=log_dir, device=agent_cfg.device)
+    # save resume path before creating a new log_dir
+    if agent_cfg.resume:
+        # get path to previous checkpoint
+        resume_path = get_checkpoint_path(log_root_path, agent_cfg.load_run, agent_cfg.load_checkpoint)
+        print(f"[INFO]: Loading model checkpoint from: {resume_path}")
+        # load previously trained model
+        runner.load(resume_path, eval_mode=False)
+
+    env.unwrapped.seed(agent_cfg.seed)
+
+    # dump the configuration into log-directory
+    dump_yaml(os.path.join(log_dir, "params", "env.yaml"), env_cfg)
+    dump_pickle(os.path.join(log_dir, "params", "env.pkl"), env_cfg)
+
+    # run training
+    runner.learn(init_at_random_ep_len=True)
+
+    # close the simulator
+    env.close()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
+
