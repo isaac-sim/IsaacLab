29008145359bb9c0b6a5dcd3a572df3f90236535
David Hoeller <dhoeller@nvidia.com>
2024-10-17 13:54:23 +0200

    Update docs/conf.py
    Signed-off-by: David Hoeller <dhoeller@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -247,7 +247,7 @@ templates_path = [
 ]
 
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^(pull/1247/merge)$"
+smv_branch_whitelist = r"^(refs/remotes/pull/1247/merge)$"
 smv_remote_whitelist = r'^.*$'
 # Whitelist pattern for tags (set to None to ignore all tags)
 smv_tag_whitelist = "tmp"
