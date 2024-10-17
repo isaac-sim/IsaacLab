2353242b2400507b5af34945238122f5dc14bc0c
David Hoeller <dhoeller@nvidia.com>
2024-10-17 11:27:18 +0200

    Update make.bat
    Signed-off-by: David Hoeller <dhoeller@nvidia.com>

--- docs/make.bat
+++ docs/make.bat
@@ -13,9 +13,9 @@ set BUILDDIR=_build
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
-	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.The 'sphinx-multiversion' command was not found. Make sure you have Sphinx
 	echo.installed, then set the SPHINXBUILD environment variable to point
-	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.to the full path of the 'sphinx-multiversion' executable. Alternatively you
 	echo.may add the Sphinx directory to PATH.
 	echo.
 	echo.If you don't have Sphinx installed, grab it from
