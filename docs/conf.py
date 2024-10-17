4f7f8ad839ca45e8f5a1103a84f8001e951112af
David Hoeller <dhoeller@nvidia.com>
2024-10-17 14:11:11 +0200

    Update docs/conf.py
    Signed-off-by: David Hoeller <dhoeller@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -246,11 +246,12 @@ templates_path = [
     "_templates",
 ]
 
+# Whitelist pattern for remotes
+smv_remote_whitelist = r"^.*$"
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^(HEAD)$"
-smv_remote_whitelist = r'^.*$'
+smv_branch_whitelist = os.getenv("SMV_BRANCH_WHITELIST", r"^(main)$")
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = "tmp"
+smv_tag_whitelist = os.getenv("SMV_TAG_WHITELIST", r"^v[1-9]\d*\.\d+\.\d+$")
 print(f"smv_branch_whitelist {smv_branch_whitelist}, smv_tag_whitelist {smv_tag_whitelist}")
 html_sidebars = {
     "**": ["navbar-logo.html", "versioning.html", "icon-links.html", "search-field.html", "sbt-sidebar-nav.html"]
