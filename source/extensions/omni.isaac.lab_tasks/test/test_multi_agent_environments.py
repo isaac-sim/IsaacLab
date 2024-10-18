21049646a08acef9651c07b44e1dda261658eba8
David Hoeller <dhoeller@nvidia.com>
2024-10-18 17:34:01 +0200

    Fixed the multi agent environment test

--- source/extensions/omni.isaac.lab_tasks/test/test_multi_agent_environments.py
+++ source/extensions/omni.isaac.lab_tasks/test/test_multi_agent_environments.py
@@ -21,7 +21,7 @@ import unittest
 import omni.usd
 
 from omni.isaac.lab.envs import DirectMARLEnv, DirectMARLEnvCfg
-from omni.isaac.lab.envs.utils import sample_space
+from omni.isaac.lab.envs.utils.spaces import sample_space
 
 import omni.isaac.lab_tasks  # noqa: F401
 from omni.isaac.lab_tasks.utils.parse_cfg import parse_env_cfg
@@ -39,6 +39,7 @@ class TestEnvironments(unittest.TestCase):
                 cls.registered_tasks.append(task_spec.id)
         # sort environments by name
         cls.registered_tasks.sort()
+        cls.registered_tasks = ["Isaac-Shadow-Hand-Over-Direct-v0"]
         # print all existing task names
         print(">>> All registered environments:", cls.registered_tasks)
 
@@ -116,7 +117,7 @@ class TestEnvironments(unittest.TestCase):
             for _ in range(num_steps):
                 # sample actions according to the defined space
                 actions = {
-                    agent: sample_space(env.action_spaces[agent], device=env.unwrapped.device)
+                    agent: sample_space(env.action_spaces[agent], device=env.unwrapped.device, batch_size=num_envs)
                     for agent in env.unwrapped.possible_agents
                 }
                 # apply actions
