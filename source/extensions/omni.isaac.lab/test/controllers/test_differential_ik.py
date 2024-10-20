add fix for jacobian computation w.r.t to the root frame of robot and added test against it

source/extensions/omni.isaac.lab/test/controllers/test_differential_ik.py
@@ -22,7 +22,14 @@ from omni.isaac.cloner import GridCloner
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.assets import Articulation
 from omni.isaac.lab.controllers import DifferentialIKController, DifferentialIKControllerCfg
-from omni.isaac.lab.utils.math import compute_pose_error, subtract_frame_transforms
+
+from omni.isaac.lab.utils.math import (  # isort:skip
+    compute_pose_error,
+    matrix_from_quat,
+    quat_inv,
+    random_yaw_orientation,
+    subtract_frame_transforms,
+)
 
 ##
 # Pre-defined configs
@@ -169,6 +176,10 @@ class TestDifferentialIKController(unittest.TestCase):
                 robot.write_joint_state_to_sim(joint_pos, joint_vel)
                 robot.set_joint_position_target(joint_pos)
                 robot.write_data_to_sim()
+                # randomize root state yaw, ik should work regardless base rotation
+                root_state = robot.data.root_state_w.clone()
+                root_state[:, 3:7] = random_yaw_orientation(self.num_envs, self.sim.device)
+                robot.write_root_state_to_sim(root_state)
                 robot.reset()
                 # reset actions
                 ee_pose_b_des[:] = self.ee_pose_b_des_set[current_goal_idx]
@@ -185,6 +196,10 @@ class TestDifferentialIKController(unittest.TestCase):
                 jacobian = robot.root_physx_view.get_jacobians()[:, ee_jacobi_idx, :, arm_joint_ids]
                 ee_pose_w = robot.data.body_state_w[:, ee_frame_idx, 0:7]
                 root_pose_w = robot.data.root_state_w[:, 0:7]
+                base_rot = root_pose_w[:, 3:7]
+                base_rot_matrix = matrix_from_quat(quat_inv(base_rot))
+                jacobian[:, :3, :] = torch.bmm(base_rot_matrix, jacobian[:, :3, :])
+                jacobian[:, 3:, :] = torch.bmm(base_rot_matrix, jacobian[:, 3:, :])
                 joint_pos = robot.data.joint_pos[:, arm_joint_ids]
                 # compute frame in root frame
                 ee_pos_b, ee_quat_b = subtract_frame_transforms(