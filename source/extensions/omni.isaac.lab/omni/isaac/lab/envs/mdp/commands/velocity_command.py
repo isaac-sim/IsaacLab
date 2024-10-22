010bd5155e59976d14388239e47b91e7a258a068
Mayank Mittal <mittalma@leggedrobotics.com>
2024-10-22 10:08:04 +0200

    moves check up

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/commands/velocity_command.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/commands/velocity_command.py
@@ -51,9 +51,24 @@ class UniformVelocityCommand(CommandTerm):
         Args:
             cfg: The configuration of the command generator.
             env: The environment.
+
+        Raises:
+            ValueError: If the heading command is active but the heading range is not provided.
         """
         # initialize the base class
         super().__init__(cfg, env)
+
+        # check configuration
+        if self.cfg.heading_command and self.cfg.ranges.heading is None:
+            raise ValueError(
+                "The velocity command has heading commands active (heading_command=True) but the `ranges.heading`"
+                " parameter is set to None."
+            )
+        if self.cfg.ranges.heading and not self.cfg.heading_command:
+            omni.log.warn(
+                f"The velocity command has the 'ranges.heading' attribute set to '{self.cfg.ranges.heading}'"
+                " but the heading command is not active. Consider setting the flag for the heading command to True."
+            )
 
         # obtain the robot asset
         # -- robot
@@ -68,18 +83,6 @@ class UniformVelocityCommand(CommandTerm):
         # -- metrics
         self.metrics["error_vel_xy"] = torch.zeros(self.num_envs, device=self.device)
         self.metrics["error_vel_yaw"] = torch.zeros(self.num_envs, device=self.device)
-
-        if self.cfg.heading_command and self.cfg.ranges.heading is None:
-            raise ValueError(
-                "The velocity command has heading commands active (heading_command=True) but the `ranges.heading`"
-                " parameter is set to None."
-            )
-        if self.cfg.ranges.heading and not self.cfg.heading_command:
-            omni.log.warn(
-                "The velocity command configuration has the 'ranges.heading' attribute set to"
-                f" {self.cfg.ranges.heading} but the heading command is not active. Consider setting"
-                " the flag for the heading command to True."
-            )
 
     def __str__(self) -> str:
         """Return a string representation of the command generator."""
