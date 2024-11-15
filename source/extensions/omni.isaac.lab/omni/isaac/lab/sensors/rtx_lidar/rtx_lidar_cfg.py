3f2c30ec22ad4999083383cb97a6abd64f44fc00
James Tigue <jtigue@theaiinstitute.com>
2024-11-15 13:51:24 -0500

    more docstrings for config

--- source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/rtx_lidar/rtx_lidar_cfg.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/rtx_lidar/rtx_lidar_cfg.py
@@ -17,6 +17,7 @@ from .rtx_lidar import RtxLidar
 
 @configclass
 class RtxLidarCfg(SensorBaseCfg):
+    """Configuration for the RtxLidar sensor."""
 
     @configclass
     class OffsetCfg:
@@ -28,9 +29,28 @@ class RtxLidarCfg(SensorBaseCfg):
         """Quaternion rotation (w, x, y, z) w.r.t. the parent frame. Defaults to (1.0, 0.0, 0.0, 0.0)."""
 
     class_type: type = RtxLidar
+
     offset: OffsetCfg = OffsetCfg()
+    """The offset pose of the sensor's frame from the sensor's parent frame. Defaults to identity.
+
+    Note:
+        The parent frame is the frame the sensor attaches to. For example, the parent frame of a
+        camera at path ``/World/envs/env_0/Robot/Camera`` is ``/World/envs/env_0/Robot``.
+    """
     optional_data_types: list[
         Literal["azimuth", "beamId", "elevation", "emitterId", "index", "materialId", "normal", "objectId", "velocity"]
     ] = []
+    """The optional output data types to include in RtxLidarData.
+
+    Please refer to the :class:'RtxLidar' and :class:'RtxLidarData' for a list and description of available data types.
+    """
     data_frame: Literal["world", "sensor"] = "world"
+    """The frame to represent the output.data.
+
+    If 'world' the output.data will be in the world frame. If 'sensor' the output.data will be in the sensor frame."""
     spawn: LidarCfg = MISSING
+    """Spawn configuration for the asset.
+
+    If None, then the prim is not spawned by the asset. Instead, it is assumed that the
+    asset is already present in the scene.
+    """
