35fdb1a824157ac4444617d583e2c6221f5d0471
Kelly Guo <kellyg@nvidia.com>
2024-11-22 14:02:37 -0500

    Updates torch to 2.5.1 (#168)
    # Description
    Updates torch to 2.5.1

--- source/extensions/omni.isaac.lab/setup.py
+++ source/extensions/omni.isaac.lab/setup.py
@@ -19,7 +19,7 @@ EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extensio
 INSTALL_REQUIRES = [
     # generic
     "numpy<2",
-    "torch==2.4.0",
+    "torch==2.5.1",
     "onnx==1.16.1",  # 1.16.2 throws access violation on Windows
     "prettytable==3.3.0",
     "toml",
