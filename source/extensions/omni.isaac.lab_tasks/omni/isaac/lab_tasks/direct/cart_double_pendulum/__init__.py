5949de44ad1142e8ba27dc7ff9280f586aa87007
Rishi-V <rveerapa@andrew.cmu.edu>
2025-01-02 20:07:32 -0500

    Revert "Added sb3, rsl_rl and changed skrl from 1600 iterations to 4800. sb3 and rsl_rl currently fail."
    This reverts commit 1228a4e65bfd49c52950ef9092bf1d972ea512b2.

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cart_double_pendulum/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/cart_double_pendulum/__init__.py
@@ -22,12 +22,8 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.cart_double_pendulum_env:CartDoublePendulumEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
-        "rsl_rl_cfg_entry_point": (
-            f"{agents.__name__}.rsl_rl_ppo_cfg:CartDoublePendulumPPORunnerCfg"
-        ),  # FIXME: Raises errors
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_cfg.yaml",
         "skrl_mappo_cfg_entry_point": f"{agents.__name__}:skrl_mappo_cfg.yaml",
-        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",  # FIXME: Raises errors
     },
 )
