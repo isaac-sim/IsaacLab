c80cd685be64fa2cdd553a84eede990689bbb608
peterd-NV <peterd@nvidia.com>
2025-01-16 14:42:14 -0500

    Adds robomimic to IsaacLab install script (#238)
    Add robomimic back to isaaclab.sh install script
    
    Fixes # (issue)
    
    Fixes robomimic not found error when running Mimic tutorial
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->

--- source/isaaclab_mimic/setup.py
+++ source/isaaclab_mimic/setup.py
@@ -5,6 +5,7 @@
 
 """Installation script for the 'isaaclab_mimic' python package."""
 
+import itertools
 import os
 import platform
 import toml
@@ -16,6 +17,11 @@ EXTENSION_PATH = os.path.dirname(os.path.realpath(__file__))
 # Read the extension.toml file
 EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extension.toml"))
 
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = [
+    "tomli",
+]
+
 # Extra dependencies for IL agents
 EXTRAS_REQUIRE = {"robomimic": []}
 
@@ -23,6 +29,10 @@ EXTRAS_REQUIRE = {"robomimic": []}
 if platform.system() == "Linux":
     EXTRAS_REQUIRE["robomimic"].append("robomimic@git+https://github.com/ARISE-Initiative/robomimic.git")
 
+# Cumulation of all extra-requires
+EXTRAS_REQUIRE["all"] = list(itertools.chain.from_iterable(EXTRAS_REQUIRE.values()))
+# Remove duplicates in the all list to avoid double installations
+EXTRAS_REQUIRE["all"] = list(set(EXTRAS_REQUIRE["all"]))
 
 # Installation operation
 setup(
@@ -34,6 +44,7 @@ setup(
     version=EXTENSION_TOML_DATA["package"]["version"],
     description=EXTENSION_TOML_DATA["package"]["description"],
     keywords=EXTENSION_TOML_DATA["package"]["keywords"],
+    install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
     license="MIT",
     include_package_data=True,
