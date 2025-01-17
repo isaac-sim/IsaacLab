8bd9412f7fbb1f97db2a8736974c5d9dd9bcb8c9
hapatel <hapatel@theaiinstitute.com>
2025-01-17 18:31:55 +0000

    cleaned up code and added replaice_physics checker warning

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/events.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/events.py
@@ -195,6 +195,11 @@ class randomize_visual_texture_material(ManagerTermBase):
         texture_paths = cfg.params.get("texture_paths")
         event_name = cfg.params.get("event_name")
         texture_rotation = cfg.params.get("texture_rotation", (0.0, 0.0))
+        replicate_physics = cfg.params.get("replicate_physics")
+
+        # check to make sure replicate_physics is set to False, else raise warning
+        if replicate_physics == True:
+            raise Warning("replicate_physics is set to True, meaning all environments will have same textures applied.")
 
         # convert from radians to degrees
         texture_rotation = tuple(math.degrees(angle) for angle in texture_rotation)
@@ -222,7 +227,6 @@ class randomize_visual_texture_material(ManagerTermBase):
                 )
 
             return prims_group.node
-
         # Register the event to the replicator
         with rep.trigger.on_custom_event(event_name=event_name):
             rep_texture_randomization()
@@ -233,8 +237,9 @@ class randomize_visual_texture_material(ManagerTermBase):
         env_ids: torch.Tensor,
         event_name: str,
         asset_cfg: SceneEntityCfg,
+        replicate_physics: bool,
         texture_paths: list[str],
-        texture_rotation: tuple[float, float] = (0.0, 0.0),
+        texture_rotation: tuple[float, float] = (0.0, 0.0)
     ):
         # import replicator
         import omni.replicator.core as rep
@@ -243,6 +248,7 @@ class randomize_visual_texture_material(ManagerTermBase):
         # note: This triggers the nodes for all the environments.
         #   We need to investigate how to make it happen only for a subset based on env_ids.
         rep.utils.send_og_event(event_name)
+        
 
 
 def randomize_rigid_body_mass(
