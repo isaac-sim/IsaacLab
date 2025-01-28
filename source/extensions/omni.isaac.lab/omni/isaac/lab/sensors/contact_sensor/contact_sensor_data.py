60902d0b2eb48fa81188dab6face0d1c0cae4ae5
lukasfro <lukas.froehlich@swiss-mile.com>
2025-01-28 16:32:58 +0100

    Add force_matrix_w_history attribute to ContactSensor

--- source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/contact_sensor/contact_sensor_data.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/sensors/contact_sensor/contact_sensor_data.py
@@ -65,6 +65,17 @@ class ContactSensorData:
         If the :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty, then this quantity is None.
     """
 
+    force_matrix_w_history: torch.Tensor | None = None
+    """The normal contact forces filtered between the sensor bodies and filtered bodies in world frame.
+
+    Shape is (N, T, B, M, 3), where N is the number of sensors, T is the configured history length,
+    B is number of bodies in each sensor and M is the number of filtered bodies.
+    In the history dimension, the first index is the most recent and the last index is the oldest.
+
+    Note:
+        If the :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty, then this quantity is None.
+    """
+
     last_air_time: torch.Tensor | None = None
     """Time spent (in s) in the air before the last contact.
 
