b20d49f04408a4c02fd47247df26be6bea1f26a2
hapatel <hapatel@theaiinstitute.com>
2025-01-29 15:57:32 +0000

    added formal test for texture randomization

---
+++ source/extensions/omni.isaac.lab/test/envs/test_texture_randomization.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script tests the functionality of texture randomization applied to the cartpole scene.
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+from omni.isaac.lab.app import AppLauncher, run_tests
+
+# Can set this to False to see the GUI for debugging
+HEADLESS = True
+
+# launch omniverse app
+app_launcher = AppLauncher(headless=HEADLESS)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import math
+import torch
+import unittest
+
+import omni.isaac.lab.envs.mdp as mdp
+from omni.isaac.lab.envs import ManagerBasedEnv, ManagerBasedEnvCfg
+from omni.isaac.lab.managers import EventTermCfg as EventTerm
+from omni.isaac.lab.managers import SceneEntityCfg
+from omni.isaac.lab.utils import configclass
+from omni.isaac.lab.utils.assets import NVIDIA_NUCLEUS_DIR
+
+from omni.isaac.lab_tasks.manager_based.classic.cartpole.cartpole_env_cfg import CartpoleSceneCfg
+
+
+@configclass
+class EventCfg:
+    """Configuration for events."""
+
+    # on reset apply a new set of textures
+    cart_texture_randomizer = EventTerm(
+        func=mdp.randomize_visual_texture_material,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=["cart"]),
+            "texture_paths": [
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Bamboo_Planks/Bamboo_Planks_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Cherry/Cherry_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Oak/Oak_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Timber/Timber_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Timber_Cladding/Timber_Cladding_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Walnut_Planks/Walnut_Planks_BaseColor.png",
+            ],
+            "event_name": "cart_texture_randomizer",
+            "texture_rotation": (math.pi / 2, math.pi / 2),
+        },
+    )
+
+    pole_texture_randomizer = EventTerm(
+        func=mdp.randomize_visual_texture_material,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names=["pole"]),
+            "texture_paths": [
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Bamboo_Planks/Bamboo_Planks_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Cherry/Cherry_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Oak/Oak_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Timber/Timber_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Timber_Cladding/Timber_Cladding_BaseColor.png",
+                f"{NVIDIA_NUCLEUS_DIR}/Materials/Base/Wood/Walnut_Planks/Walnut_Planks_BaseColor.png",
+            ],
+            "event_name": "pole_texture_randomizer",
+            "texture_rotation": (math.pi / 2, math.pi / 2),
+        },
+    )
+
+
+@configclass
+class CartpoleEnvCfg(ManagerBasedEnvCfg):
+    """Configuration for the cartpole environment."""
+
+    # Scene settings
+    scene = CartpoleSceneCfg(env_spacing=2.5)
+
+    # Basic settings
+    events = EventCfg()
+
+    def __post_init__(self):
+        """Post initialization."""
+        # viewer settings
+        self.viewer.eye = [4.5, 0.0, 6.0]
+        self.viewer.lookat = [0.0, 0.0, 2.0]
+        # step settings
+        self.decimation = 4  # env step every 4 sim steps: 200Hz / 4 = 50Hz
+        # simulation settings
+        self.sim.dt = 0.005  # sim step every 5ms: 200Hz
+
+
+class TestTextureRandomization(unittest.TestCase):
+    """Test for texture randomization"""
+
+    """
+    Tests
+    """
+
+    def test_texture_randomization(self):
+        # set the arguments
+        env_cfg = CartpoleEnvCfg()
+        env_cfg.scene.num_envs = 16
+        env_cfg.scene.replicate_physics = False
+
+        # setup base environment
+        env = ManagerBasedEnv(cfg=env_cfg)
+
+        # simulate physics
+        count = 0
+        while simulation_app.is_running():
+            with torch.inference_mode():
+                # reset
+                if count % 10 == 0:
+                    env.reset()
+                    print("-" * 80)
+                    print("[INFO]: Resetting environment...")
+                    print("[INFO]: A new set of random textures have been applied.")
+
+                # sample random actions
+                joint_efforts = torch.randn_like(env.action_manager.action)
+                # step the environment
+                env.step(joint_efforts)
+
+                # update counter
+                count += 1
+
+                # After 5 iterations of new textures being applied finish the test and close env
+                if count >= 50:
+                    env.close()
+                    break
+
+
+if __name__ == "__main__":
+    # run the main function
+    run_tests()
