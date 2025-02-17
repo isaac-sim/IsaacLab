b571a19942ce4cd31cf9ac2103f5a6fbb01b0ff1
Mayank Mittal <mittalma@leggedrobotics.com>
2025-02-17 22:20:19 +0100

    runs test over subset

--- tools/run_all_tests.py
+++ tools/run_all_tests.py
@@ -32,14 +32,7 @@ from pathlib import Path
 from prettytable import PrettyTable
 
 # Local imports
-from per_test_timeouts import PER_TEST_TIMEOUTS
-from tests_to_skip import TESTS_TO_SKIP
-
-ISAACLAB_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-"""Path to the root directory of the Isaac Lab repository."""
-
-DEFAULT_TIMEOUT = 120
-"""The default timeout for each test in seconds."""
+from test_settings import DEFAULT_TIMEOUT, ISAACLAB_PATH, PER_TEST_TIMEOUTS, TESTS_TO_SKIP
 
 
 def parse_args() -> argparse.Namespace:
