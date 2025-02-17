18a40dc79d481b9097a5e8f4830263c69383cba3
Mayank Mittal <mittalma@leggedrobotics.com>
2025-02-17 22:20:19 +0100

    runs test over subset

--- tools/run_train_envs.py
+++ tools/run_train_envs.py
@@ -6,12 +6,23 @@
 """
 This scripts run training with rsl-rl library over a subset of the environments.
 
+It calls the script ``scripts/reinforcement_learning/${args.lib_name}/train.py`` with the appropriate arguments.
 
-It calls the script ``scripts/reinforcement_learning/rsl_rl/train.py`` with the appropriate arguments.
+Example usage:
+
+.. code-block:: bash
+    # for rsl-rl
+    python run_train_envs.py --lib-name rsl_rl
+
+    # for skrl
+    python run_train_envs.py --lib-name skrl
+
 """
 
 import argparse
 import subprocess
+
+from test_settings import ISAACLAB_PATH, TEST_RL_ENVS
 
 
 def parse_args() -> argparse.Namespace:
@@ -32,35 +43,29 @@ def main(args: argparse.Namespace):
     # get the git commit hash
     git_commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("utf-8").strip()
 
-    # list of environments to train on
-    name_of_envs = [
-        # classic control
-        "Isaac-Ant-v0",
-        "Isaac-Cartpole-v0",
-        # manipulation
-        "Isaac-Lift-Cube-Franka-v0",
-        "Isaac-Open-Drawer-Franka-v0",
-        # dexterous manipulation
-        "Isaac-Repose-Cube-Allegro-v0",
-        # locomotion
-        "Isaac-Velocity-Flat-Anymal-D-v0",
-        "Isaac-Velocity-Rough-Anymal-D-v0",
-        "Isaac-Velocity-Rough-G1-v0",
-    ]
-
     # train on each environment
-    for env_name in name_of_envs:
+    for env_name in TEST_RL_ENVS:
+        # print a colored output to catch the attention of the user
+        # this should be a multi-line print statement
+        print("\033[91m==============================================\033[0m")
+        print("\033[91m==============================================\033[0m")
+        print(f"\033[91mTraining on {env_name} with {args.lib_name}...\033[0m")
+        print("\033[91m==============================================\033[0m")
+        print("\033[91m==============================================\033[0m")
+
+        # run the training script
         subprocess.run(
             [
-                "python",
-                f"scripts/reinforcement_learning/{args.lib_name}/train.py",
+                f"{ISAACLAB_PATH}/isaaclab.sh",
+                "-p",
+                f"{ISAACLAB_PATH}/scripts/reinforcement_learning/{args.lib_name}/train.py",
                 "--task",
                 env_name,
                 "--headless",
                 "--run_name",
                 git_commit_hash,
             ],
-            check=True,
+            check=False,  # do not raise an error if the script fails
         )
 
 
