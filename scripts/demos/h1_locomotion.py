7bbc48938016b5f5970456e2c0dda85346be95d6
Kelly Guo <kellyguo123@hotmail.com>
2025-03-01 01:05:21 -0800

    Adds H1 rough terrain interactive demo script

---
+++ scripts/demos/h1_locomotion.py
@@ -0,0 +1,190 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates an interactive demo with the H1 rough terrain environment.
+
+.. code-block:: bash
+
+    # Usage
+    ./isaaclab.sh -p scripts/demos/h1_locomotion.py
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# local imports
+import scripts.reinforcement_learning.rsl_rl.cli_args as cli_args  # isort: skip
+
+
+# add argparse arguments
+parser = argparse.ArgumentParser(
+    description="This script demonstrates an interactive demo with the H1 rough terrain environment."
+)
+# append RSL-RL cli arguments
+cli_args.add_rsl_rl_args(parser)
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+import torch
+
+import carb
+import omni
+from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlVecEnvWrapper
+from omni.kit.viewport.utility import get_viewport_from_window_name
+from omni.kit.viewport.utility.camera_state import ViewportCameraState
+from pxr import Gf, Sdf
+from rsl_rl.runners import OnPolicyRunner
+
+from isaaclab.envs import ManagerBasedRLEnv
+from isaaclab.utils.math import quat_apply
+from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
+
+from isaaclab_tasks.manager_based.locomotion.velocity.config.h1.rough_env_cfg import H1RoughEnvCfg_PLAY
+
+TASK = "Isaac-Velocity-Rough-H1-v0"
+RL_LIBRARY = "rsl_rl"
+
+
+class H1RoughDemo:
+    def __init__(self):
+        agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(TASK, args_cli)
+        # load the trained jit policy
+        checkpoint = get_published_pretrained_checkpoint(RL_LIBRARY, TASK)
+        # create envionrment
+        env_cfg = H1RoughEnvCfg_PLAY()
+        env_cfg.scene.num_envs = 25
+        env_cfg.curriculum = None
+        env_cfg.commands.base_velocity.ranges.lin_vel_x = (0.0, 1.0)
+        env_cfg.commands.base_velocity.ranges.heading = (-1.0, 1.0)
+        # wrap around environment for rsl-rl
+        self.env = RslRlVecEnvWrapper(ManagerBasedRLEnv(cfg=env_cfg))
+        self.device = self.env.unwrapped.device
+        # load previously trained model
+        ppo_runner = OnPolicyRunner(self.env, agent_cfg.to_dict(), log_dir=None, device=self.device)
+        ppo_runner.load(checkpoint)
+        # obtain the trained policy for inference
+        self.policy = ppo_runner.get_inference_policy(device=self.device)
+
+        self.create_camera()
+        # self.env.unwrapped.command_manager.reset(torch.arange(env_cfg.scene.num_envs))
+        self.commands = torch.zeros(env_cfg.scene.num_envs, 4, device=self.device)
+        self.commands[:, 0:3] = self.env.unwrapped.command_manager.get_command("base_velocity")
+        # self.commands[:, 3] = self.env.unwrapped.command_manager.get_term("base_velocity").heading_target
+        self.set_up_keyboard()
+        self._prim_selection = omni.usd.get_context().get_selection()
+        self._selected_id = None
+        self._previous_selected_id = None
+        self._camera_local_transform = torch.tensor([-1.8, 0.0, 0.6], device=self.device)
+
+    def create_camera(self):
+        stage = omni.usd.get_context().get_stage()
+        self.viewport = get_viewport_from_window_name("Viewport")
+        # Create camera
+        self.camera_path = "/World/Camera"
+        self.perspective_path = "/OmniverseKit_Persp"
+        camera_prim = stage.DefinePrim(self.camera_path, "Camera")
+        camera_prim.GetAttribute("focalLength").Set(8.5)
+        coi_prop = camera_prim.GetProperty("omni:kit:centerOfInterest")
+        if not coi_prop or not coi_prop.IsValid():
+            camera_prim.CreateAttribute(
+                "omni:kit:centerOfInterest", Sdf.ValueTypeNames.Vector3d, True, Sdf.VariabilityUniform
+            ).Set(Gf.Vec3d(0, 0, -10))
+        self.viewport.set_active_camera(self.perspective_path)
+
+    def set_up_keyboard(self):
+        self._input = carb.input.acquire_input_interface()
+        self._keyboard = omni.appwindow.get_default_app_window().get_keyboard()
+        self._sub_keyboard = self._input.subscribe_to_keyboard_events(self._keyboard, self._on_keyboard_event)
+        T = 1
+        R = 0.5
+        self._key_to_control = {
+            "UP": torch.tensor([T, 0.0, 0.0, 0.0], device=self.device),
+            "DOWN": torch.tensor([0.0, 0.0, 0.0, 0.0], device=self.device),
+            "LEFT": torch.tensor([T, 0.0, 0.0, -R], device=self.device),
+            "RIGHT": torch.tensor([T, 0.0, 0.0, R], device=self.device),
+            "ZEROS": torch.tensor([0.0, 0.0, 0.0, 0.0], device=self.device),
+        }
+
+    def _on_keyboard_event(self, event, *args, **kwargs):
+        if event.type == carb.input.KeyboardEventType.KEY_PRESS:
+            if event.input.name in self._key_to_control:
+                if self._selected_id:
+                    self.commands[self._selected_id] = self._key_to_control[event.input.name]
+            elif event.input.name == "ESCAPE":
+                self._prim_selection.clear_selected_prim_paths()
+            elif event.input.name == "C":
+                if self._selected_id is not None:
+                    if self.viewport.get_active_camera() == self.camera_path:
+                        self.viewport.set_active_camera(self.perspective_path)
+                    else:
+                        self.viewport.set_active_camera(self.camera_path)
+        elif event.type == carb.input.KeyboardEventType.KEY_RELEASE:
+            if self._selected_id:
+                self.commands[self._selected_id] = self._key_to_control["ZEROS"]
+
+    def update_selected_object(self):
+        self._previous_selected_id = self._selected_id
+        selected_prim_paths = self._prim_selection.get_selected_prim_paths()
+        if len(selected_prim_paths) == 0:
+            self._selected_id = None
+            self.viewport.set_active_camera(self.perspective_path)
+        elif len(selected_prim_paths) > 1:
+            print("Multiple prims are selected. Please only select one!")
+        else:
+            prim_splitted_path = selected_prim_paths[0].split("/")
+            if len(prim_splitted_path) >= 4 and prim_splitted_path[3][0:4] == "env_":
+                self._selected_id = int(prim_splitted_path[3][4:])
+                if self._previous_selected_id != self._selected_id:
+                    self.viewport.set_active_camera(self.camera_path)
+                self._update_camera()
+            else:
+                print("The selected prim was not a H1 robot")
+
+        if self._previous_selected_id is not None and self._previous_selected_id != self._selected_id:
+            self.env.unwrapped.command_manager.reset([self._previous_selected_id])
+            self.commands[:, 0:3] = self.env.unwrapped.command_manager.get_command("base_velocity")
+            # self.commands[:, 3] = self.env.unwrapped.command_manager.get_term("base_velocity").heading_target
+
+    def _update_camera(self):
+        base_pos = self.env.unwrapped.scene["robot"].data.root_pos_w[self._selected_id, :]  # - env.scene.env_origins
+        base_quat = self.env.unwrapped.scene["robot"].data.root_quat_w[self._selected_id, :]
+
+        camera_pos = quat_apply(base_quat, self._camera_local_transform) + base_pos
+
+        camera_state = ViewportCameraState(self.camera_path, self.viewport)
+        eye = Gf.Vec3d(camera_pos[0].item(), camera_pos[1].item(), camera_pos[2].item())
+        target = Gf.Vec3d(base_pos[0].item(), base_pos[1].item(), base_pos[2].item() + 0.6)
+        camera_state.set_position_world(eye, True)
+        camera_state.set_target_world(target, True)
+
+
+def main():
+    """Main function."""
+    demo_h1 = H1RoughDemo()
+    obs, _ = demo_h1.env.reset()
+    while simulation_app.is_running():
+        demo_h1.update_selected_object()
+        with torch.inference_mode():
+            action = demo_h1.policy(obs)
+            obs, _, _, _ = demo_h1.env.step(action)
+            # overwrite command based on keyboard input
+            obs[:, 9:13] = demo_h1.commands
+
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
