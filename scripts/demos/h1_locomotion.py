738c5a56ef31b4a087727fa82469e17835b0f274
Kelly Guo <kellyguo123@hotmail.com>
2025-03-01 08:54:07 -0800

    Adds comments

--- scripts/demos/h1_locomotion.py
+++ scripts/demos/h1_locomotion.py
@@ -60,7 +60,23 @@ RL_LIBRARY = "rsl_rl"
 
 
 class H1RoughDemo:
+    """This class provides an interactive demo for the H1 rough terrain environment.
+    It loads a pre-trained checkpoint for the Isaac-Velocity-Rough-H1-v0 task, trained with RSL RL
+    and defines a set of keyboard commands for directing motion of selected robots.
+
+    A robot can be selected from the scene through a mouse click. Once selected, the following
+    keyboard controls can be used to control the robot:
+
+    * UP: go forward
+    * LEFT: turn left
+    * RIGHT: turn right
+    * DOWN: stop
+    * C: switch between third-person and perspective views
+    * ESC: exit current third-person view"""
+
     def __init__(self):
+        """Initializes environment config designed for the interactive model and sets up the environment,
+        loads pre-trained checkpoints, and registers keyboard events."""
         agent_cfg: RslRlOnPolicyRunnerCfg = cli_args.parse_rsl_rl_cfg(TASK, args_cli)
         # load the trained jit policy
         checkpoint = get_published_pretrained_checkpoint(RL_LIBRARY, TASK)
@@ -91,6 +107,7 @@ class H1RoughDemo:
         self._camera_local_transform = torch.tensor([-2.5, 0.0, 0.8], device=self.device)
 
     def create_camera(self):
+        """Creates a camera to be used for third-person view."""
         stage = omni.usd.get_context().get_stage()
         self.viewport = get_viewport_from_window_name("Viewport")
         # Create camera
@@ -106,6 +123,7 @@ class H1RoughDemo:
         self.viewport.set_active_camera(self.perspective_path)
 
     def set_up_keyboard(self):
+        """Sets up interface for keyboard input and registers the desired keys for control."""
         self._input = carb.input.acquire_input_interface()
         self._keyboard = omni.appwindow.get_default_app_window().get_keyboard()
         self._sub_keyboard = self._input.subscribe_to_keyboard_events(self._keyboard, self._on_keyboard_event)
@@ -120,23 +138,33 @@ class H1RoughDemo:
         }
 
     def _on_keyboard_event(self, event, *args, **kwargs):
+        """Checks for a keyboard event and assign the corresponding command control depending on key pressed."""
         if event.type == carb.input.KeyboardEventType.KEY_PRESS:
+            # Arrow keys map to pre-defined command vectors to control navigation of robot
             if event.input.name in self._key_to_control:
                 if self._selected_id:
                     self.commands[self._selected_id] = self._key_to_control[event.input.name]
+            # Escape key exits out of the current selected robot view
             elif event.input.name == "ESCAPE":
                 self._prim_selection.clear_selected_prim_paths()
+            # C key swaps between third-person and perspective views
             elif event.input.name == "C":
                 if self._selected_id is not None:
                     if self.viewport.get_active_camera() == self.camera_path:
                         self.viewport.set_active_camera(self.perspective_path)
                     else:
                         self.viewport.set_active_camera(self.camera_path)
+        # On key release, the robot stops moving
         elif event.type == carb.input.KeyboardEventType.KEY_RELEASE:
             if self._selected_id:
                 self.commands[self._selected_id] = self._key_to_control["ZEROS"]
 
     def update_selected_object(self):
+        """Determines which robot is currently selected and whether it is a valid H1 robot.
+        For valid robots, we enter the third-person view for that robot.
+        When a new robot is selected, we reset the command of the previously selected
+        to continue random commands."""
+
         self._previous_selected_id = self._selected_id
         selected_prim_paths = self._prim_selection.get_selected_prim_paths()
         if len(selected_prim_paths) == 0:
@@ -146,6 +174,7 @@ class H1RoughDemo:
             print("Multiple prims are selected. Please only select one!")
         else:
             prim_splitted_path = selected_prim_paths[0].split("/")
+            # a valid robot was selected, update the camera to go into third-person view
             if len(prim_splitted_path) >= 4 and prim_splitted_path[3][0:4] == "env_":
                 self._selected_id = int(prim_splitted_path[3][4:])
                 if self._previous_selected_id != self._selected_id:
@@ -154,12 +183,15 @@ class H1RoughDemo:
             else:
                 print("The selected prim was not a H1 robot")
 
+        # Reset commands for previously selected robot if a new one is selected
         if self._previous_selected_id is not None and self._previous_selected_id != self._selected_id:
             self.env.unwrapped.command_manager.reset([self._previous_selected_id])
             self.commands[:, 0:3] = self.env.unwrapped.command_manager.get_command("base_velocity")
-            # self.commands[:, 3] = self.env.unwrapped.command_manager.get_term("base_velocity").heading_target
 
     def _update_camera(self):
+        """Updates the per-frame transform of the third-person view camera to follow
+        the selected robot's torso transform."""
+
         base_pos = self.env.unwrapped.scene["robot"].data.root_pos_w[self._selected_id, :]  # - env.scene.env_origins
         base_quat = self.env.unwrapped.scene["robot"].data.root_quat_w[self._selected_id, :]
 
@@ -177,6 +209,7 @@ def main():
     demo_h1 = H1RoughDemo()
     obs, _ = demo_h1.env.reset()
     while simulation_app.is_running():
+        # check for selected robots
         demo_h1.update_selected_object()
         with torch.inference_mode():
             action = demo_h1.policy(obs)
