ba5c11cc667adead1ffcbf33cb781ba6f7f7b4c4
Jean-Francois Lafleche <jlafleche@nvidia.com>
2025-03-04 20:30:13 -0500

    Remove notebook widgets

--- source/isaaclab/isaaclab/utils/__init__.py
+++ source/isaaclab/isaaclab/utils/__init__.py
@@ -11,7 +11,6 @@ from .configclass import configclass
 from .dict import *
 from .interpolation import *
 from .modifiers import *
-from .notebook_widgets import *
 from .string import *
 from .timer import Timer
 from .types import *
