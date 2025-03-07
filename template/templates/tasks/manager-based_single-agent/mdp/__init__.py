adfd4d2965d70837c5e99ef67351b4182686c02e
Antonio Serrano Munoz <aserranomuno@nvidia.com>
2025-03-07 11:23:01 -0500

    Apply pre-commit hooks

--- template/templates/tasks/manager-based_single-agent/mdp/__init__.py
+++ template/templates/tasks/manager-based_single-agent/mdp/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 """This sub-module contains the functions that are specific to the environment."""
 
 from isaaclab.envs.mdp import *  # noqa: F401, F403
