5c3c39c3a7db90bb7cae30c80e8aa1f2df49e38c
Kelly Guo <kellyg@nvidia.com>
2025-03-12 19:10:09 -0700

    Update tools/template/templates/extension/setup.py
    Signed-off-by: Kelly Guo <kellyguo123@hotmail.com>

--- tools/template/templates/extension/setup.py
+++ tools/template/templates/extension/setup.py
@@ -39,7 +39,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.10",
         "Isaac Sim :: 2023.1.1",
-        "Isaac Sim :: 4.0.0",
+        "Isaac Sim :: 4.5.0",
     ],
     zip_safe=False,
 )
