6104e0662e507014b933ce1c6d2d4be1a0fd61e7
louis <le.lay.louis@gmail.com>
2025-03-16 03:15:42 -0400

    removes rl_games agent

--- source/isaaclab_tasks/isaaclab_tasks/direct/franka_reach/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/franka_reach/__init__.py
@@ -20,7 +20,6 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.franka_reach_env:FrankaReachEnvCfg",
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:FrankaReachPPORunnerCfg",
     },
 )
