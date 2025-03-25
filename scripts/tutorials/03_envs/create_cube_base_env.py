7aa895058526877cbded99a0e789e2bf8faf02f1
Mayank Mittal <mittalma@leggedrobotics.com>
2025-03-25 19:19:13 +0100

    Fixes device settings in env tutorials

--- scripts/tutorials/03_envs/create_cube_base_env.py
+++ scripts/tutorials/03_envs/create_cube_base_env.py
@@ -290,6 +290,7 @@ class CubeEnvCfg(ManagerBasedEnvCfg):
         self.sim.dt = 0.01
         self.sim.physics_material = self.scene.terrain.physics_material
         self.sim.render_interval = 2  # render interval should be a multiple of decimation
+        self.sim.device = args_cli.device
 
 
 def main():
