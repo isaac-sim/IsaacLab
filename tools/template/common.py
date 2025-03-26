376846940f7f914e1407d1c2f4d775890c8d549c
Toni-SM <aserranomuno@nvidia.com>
2025-03-26 00:47:33 -0400

    Fixes template generator (#2161)
    # Description
    
    Fix template generator:
    - Fix `rsl_rl` agent configuration
    - Don't list `skrl`'s multi-agent algorithms for single-agent tasks
    - Don't list `rsl_rl` and `sb3` for multi-agent tasks
    - Update docs to include usage steps for the generated internal task
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [x] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->

--- tools/template/common.py
+++ tools/template/common.py
@@ -5,6 +5,11 @@
 
 import os
 
+# paths
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TASKS_DIR = os.path.join(ROOT_DIR, "source", "isaaclab_tasks", "isaaclab_tasks")
 TEMPLATE_DIR = os.path.join(ROOT_DIR, "tools", "template", "templates")
+
+# RL algorithms
+SINGLE_AGENT_ALGORITHMS = ["AMP", "PPO"]
+MULTI_AGENT_ALGORITHMS = ["IPPO", "MAPPO"]
