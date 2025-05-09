4a310fd0825fc8b42706a5f04c026cdd0ac3431d
alessandro.assirelli <alessandro.assirelli@agile-robots.com>
2025-05-09 16:37:26 +0200

    add clip for rsl_rl distillation training

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/distillation_cfg.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/distillation_cfg.py
@@ -78,3 +78,6 @@ class RslRlDistillationAlgorithmCfg:
 
     gradient_length: int = MISSING
     """The number of environment steps the gradient flows back."""
+
+    max_grad_norm: float = 1.0
+    """The maximum gradient norm used for distillation"""
