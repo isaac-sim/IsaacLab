0112c85c886252ab25a78c408f03b28e647bde6b
Alessandro Assirelli <alessandroassirelli@Alessandros-MacBook-Pro.local>
2025-05-15 18:37:47 +0200

    added suggestions

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/distillation_cfg.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/distillation_cfg.py
@@ -80,4 +80,4 @@ class RslRlDistillationAlgorithmCfg:
     """The number of environment steps the gradient flows back."""
 
     max_grad_norm: None | float = None
-    """The maximum gradient norm used for distillation"""
+     """The maximum norm the gradient is clipped to."""
