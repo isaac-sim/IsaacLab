34100b8ba4eab8092bf1ff0a5635482e7575ba7c
Kelly Guo <kellyg@nvidia.com>
2025-05-14 20:45:07 -0700

    Increases collision stack size for factory environments (#425)
    # Description
    
    Increases collision stack size for factory environments as they were
    exceeding the previous set values in some cases.
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->

--- source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env_cfg.py
@@ -107,6 +107,7 @@ class FactoryEnvCfg(DirectRLEnvCfg):
             friction_correlation_distance=0.00625,
             gpu_max_rigid_contact_count=2**23,
             gpu_max_rigid_patch_count=2**23,
+            gpu_collision_stack_size=2**28,
             gpu_max_num_partitions=1,  # Important for stable simulation.
         ),
         physics_material=RigidBodyMaterialCfg(
