a821cc7357ec28a9c31895492d935278edce694e
Mayank Mittal <12863862+Mayankm96@users.noreply.github.com>
2025-05-23 12:49:40 +0200

    Apply suggestions from code review
    Signed-off-by: Mayank Mittal <12863862+Mayankm96@users.noreply.github.com>

--- source/isaaclab/isaaclab/envs/mdp/events.py
+++ source/isaaclab/isaaclab/envs/mdp/events.py
@@ -393,6 +393,7 @@ def randomize_rigid_body_com(
     # Set the new coms
     asset.root_physx_view.set_coms(coms, env_ids)
 
+
 def randomize_rigid_body_collider_offsets(
     env: ManagerBasedEnv,
     env_ids: torch.Tensor | None,
