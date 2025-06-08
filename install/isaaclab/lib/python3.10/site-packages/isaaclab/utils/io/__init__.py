a92711b2f9eda551ace7d15fb04c1a635ea94fa9
Oblivious20 <leandro.scaglioni@gmx.ch>
2025-06-08 13:58:54 +0200

    Add nao.usd

---
+++ install/isaaclab/lib/python3.10/site-packages/isaaclab/utils/io/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Submodules for files IO operations.
+"""
+
+from .pkl import dump_pickle, load_pickle
+from .yaml import dump_yaml, load_yaml
---
+++ install/isaaclab_tasks/lib/python3.10/site-packages/isaaclab_tasks/manager_based/classic/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Classic environments for control.
+
+These environments are based on the MuJoCo environments provided by OpenAI.
+
+Reference:
+    https://github.com/openai/gym/tree/master/gym/envs/mujoco
+"""
