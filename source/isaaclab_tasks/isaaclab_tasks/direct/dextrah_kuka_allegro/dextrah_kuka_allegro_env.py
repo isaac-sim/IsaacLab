7be818c896ec9668b12c603c7e3e2b076014b4bc
Octi Zhang <zhengyuz@nvidia.com>
2025-06-08 14:20:11 -0700

    fix typo lefted -> lifted

--- source/isaaclab_tasks/isaaclab_tasks/direct/dextrah_kuka_allegro/dextrah_kuka_allegro_env.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/dextrah_kuka_allegro/dextrah_kuka_allegro_env.py
@@ -246,11 +246,11 @@ class DextrahKukaAllegroEnv(DirectRLEnv):
         finger_curl_reg_weight = self.dextrah_adr.get_custom_param_value("reward_weights", "finger_curl_reg")
         lift_weight = self.dextrah_adr.get_custom_param_value("reward_weights", "lift_weight")
 
-        lefted = object_pos[:, 2] > 0.33
+        lifted = object_pos[:, 2] > 0.33
         hand_to_object_rew = torch.exp(-self.cfg.hand_to_object_sharpness * hand_to_object_pos_error)
-        object_to_goal_rew = torch.where(lefted, torch.exp(-object_to_goal_std * object_to_goal_pos_error), 0)
+        object_to_goal_rew = torch.where(lifted, torch.exp(-object_to_goal_std * object_to_goal_pos_error), 0)
         finger_curl_reg = torch.sum(torch.square(joint_pos[:, 7:] - self.curled_q), dim=-1)
-        lift_rew = torch.where(lefted, torch.exp(-self.cfg.lift_sharpness * object_vertical_error), 0)
+        lift_rew = torch.where(lifted, torch.exp(-self.cfg.lift_sharpness * object_vertical_error), 0)
 
         # Add reward signals to tensorboard
         self.extras["num_adr_increases"] = self.dextrah_adr.num_increments()
