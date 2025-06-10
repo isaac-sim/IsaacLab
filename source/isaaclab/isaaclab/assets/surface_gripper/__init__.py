14a3a7afc835754da7a275209a95ea21b40c0d7a
Antoine RICHARD <antoiner@nvidia.com>
2025-06-10 15:15:12 +0200

    Adds support and example for SurfaceGrippers (#464)
    # Description
    
    Adds support for SurfaceGrippers inside IsaacLab
    
    
    ## Type of change
    
    - New feature (non-breaking change which adds functionality)
    - This change requires a documentation update
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there

---
+++ source/isaaclab/isaaclab/assets/surface_gripper/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Sub-module for surface_gripper assets."""
+
+from .surface_gripper import SurfaceGripper
+from .surface_gripper_cfg import SurfaceGripperCfg
