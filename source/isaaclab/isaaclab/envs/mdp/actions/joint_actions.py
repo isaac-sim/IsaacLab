12838874132646cd80ae320e43bcca90143d5880
Antoine Richard <antoiner@nvidia.com>
2025-07-10 10:01:57 +0200

    Updated based on feedback

--- source/isaaclab/isaaclab/envs/mdp/actions/joint_actions.py
+++ source/isaaclab/isaaclab/envs/mdp/actions/joint_actions.py
@@ -138,8 +138,8 @@ class JointAction(ActionTerm):
         else:
             self._IO_descriptor.offset = self._offset
         #FIXME: This is not correct. Add list support.
-        if self.cfg.clip is not None: 
-            self._IO_descriptor.clip = self._clip
+        if isinstance(self._clip, torch.Tensor):
+            self._IO_descriptor.clip = self._clip[0].detach().cpu().numpy().tolist()
         else:
             self._IO_descriptor.clip = None
         return self._IO_descriptor
