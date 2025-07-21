be84a71775a361e3f5d5987298734f2c9eea0199
Octi Zhang <zhengyuz@nvidia.com>
2025-07-21 15:07:03 -0700

    run formating

--- scripts/reinforcement_learning/rl_games/play.py
+++ scripts/reinforcement_learning/rl_games/play.py
@@ -89,7 +89,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     # override configurations with non-hydra CLI arguments
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
     env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
-    
+
     # randomly sample a seed if seed = -1
     if args_cli.seed == -1:
         args_cli.seed = random.randint(0, 10000)
