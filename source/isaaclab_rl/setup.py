219d001ecfb6700c26eb6c389a34ea6822a3f184
Kelly Guo <kellyg@nvidia.com>
2025-07-22 23:54:38 -0400

    Updates torch to 2.7.0 with cuda 12.8 blackwell support (#2998)
    # Description
    
    As required by https://github.com/isaac-sim/IsaacLab/pull/2962 and
    vulnerabilities in torch 2.5.1, this change updates torch to 2.7.0
    during the installation of Isaac Lab.
    
    Although inconsistent with Isaac Sim 4.5 (still on torch 2.5.1), Isaac
    Lab should work fine with 2.7.0+cu128. This update will also allow us to
    have better support for blackwell GPUs.
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - This change requires a documentation update
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->

--- source/isaaclab_rl/setup.py
+++ source/isaaclab_rl/setup.py
@@ -20,7 +20,7 @@ EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extensio
 INSTALL_REQUIRES = [
     # generic
     "numpy<2",
-    "torch==2.5.1",
+    "torch>=2.5.1",
     "torchvision>=0.14.1",  # ensure compatibility with torch 1.13.1
     "protobuf>=3.20.2,!=5.26.0",
     # configuration management
@@ -35,7 +35,7 @@ INSTALL_REQUIRES = [
     "pillow==11.2.1",
 ]
 
-PYTORCH_INDEX_URL = ["https://download.pytorch.org/whl/cu118"]
+PYTORCH_INDEX_URL = ["https://download.pytorch.org/whl/cu128"]
 
 # Extra dependencies for RL agents
 EXTRAS_REQUIRE = {
