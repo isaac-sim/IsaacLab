e51356725d5ea3b75de0485890cd589d8ab1afcb
Kelly Guo <kellyg@nvidia.com>
2025-07-31 21:54:35 -0700

    Fixes timeouts for environment tests (#591)
    # Description
    
    Restructures the environment test to include new Forge environments as
    part of the Factory tests. Also fixes some timeouts for tests to make
    sure they can complete.
    
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->

--- source/isaaclab_tasks/test/env_test_utils.py
+++ source/isaaclab_tasks/test/env_test_utils.py
@@ -59,8 +59,8 @@ def setup_environment(
         # TODO: factory environments cause tests to fail if run together with other envs,
         # so we collect these environments separately to run in a separate unit test.
         # apply factory filter
-        if (factory_envs is True and "Factory" not in task_spec.id) or (
-            factory_envs is False and "Factory" in task_spec.id
+        if (factory_envs is True and ("Factory" not in task_spec.id and "Forge" not in task_spec.id)) or (
+            factory_envs is False and ("Factory" in task_spec.id or "Forge" in task_spec.id)
         ):
             continue
         # if None: no filter
--- tools/test_settings.py
+++ tools/test_settings.py
@@ -25,10 +25,10 @@ PER_TEST_TIMEOUTS = {
         2000
     ),  # Like the above, with stage in memory and with and without fabric cloning
     "test_environment_determinism.py": 500,  # This test runs through many the environments for 100 steps each
-    "test_factory_environments.py": 500,  # This test runs through Factory environments for 100 steps each
-    "test_multi_agent_environments": 1000,  # This test runs through multi-agent environments for 100 steps each
+    "test_factory_environments.py": 1000,  # This test runs through Factory environments for 100 steps each
+    "test_multi_agent_environments.py": 500,  # This test runs through multi-agent environments for 100 steps each
     "test_env_rendering_logic.py": 300,
-    "test_multi_tiled_camera": 300,
+    "test_multi_tiled_camera.py": 300,
     "test_generate_dataset.py": 500,  # This test runs annotation for 10 demos and generation until one succeeds
     "test_operational_space.py": 300,
     "test_environments_training.py": 5000,
