bd26dda2abf309064282217e59c5e6f2fe63804a
Johann Bonneau <johann.bonneau2000@gmail.com>
2025-08-01 12:14:34 +0200

    Added new environment

---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/two_robot_pick_cube/__init__.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Isaac-Two-Robot-Pick-Cube-Direct-v0",
+    entry_point=f"{__name__}.two_robot_pick_cube_env:TwoRobotPickCubeEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.two_robot_pick_cube_env_cfg:TwoRobotPickCubeCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-Two-Robot-Pick-Cube-Camera-Direct-v0",
+    entry_point=f"{__name__}.two_robot_pick_cube_env:TwoRobotPickCubeEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.two_robot_pick_cube_env_cfg:TwoRobotPickCubeCameraCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "skrl_amp_cfg_entry_point": f"{agents.__name__}:skrl_amp_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+    },
+)
---
+++ source/isaaclab_tasks/isaaclab_tasks/direct/two_robot_pick_cube/two_robot_pick_cube_env_cfg.py
@@ -0,0 +1,260 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import torch
+
+from isaaclab.envs import DirectRLEnvCfg
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sim import SimulationCfg, PhysxCfg, PinholeCameraCfg
+from isaaclab.sim.spawners.materials.physics_materials_cfg import RigidBodyMaterialCfg
+from isaaclab.utils import configclass
+from isaaclab.sensors import CameraCfg
+from isaaclab.utils.math import quat_from_euler_xyz
+import isaaclab.sim as sim_utils
+from isaaclab.assets import RigidObjectCfg
+from isaaclab.sensors import (
+    FrameTransformer,
+    FrameTransformerCfg,
+    OffsetCfg,
+    ContactSensorCfg,
+    ContactSensor,
+)
+from isaaclab.sim.spawners.from_files.from_files_cfg import UsdFileCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.markers.config import FRAME_MARKER_CFG, CUBOID_MARKER_CFG
+from isaaclab.sim.schemas.schemas_cfg import RigidBodyPropertiesCfg
+
+
+from isaaclab_assets.robots.franka import FRANKA_PANDA_CFG  # isort: skip
+
+
+@configclass
+class TwoRobotPickCubeCfg(DirectRLEnvCfg):
+    # env
+    decimation = 2
+    episode_length_s = 4  # 4 / (decimation * dt) = 200 steps
+    # - spaces definition
+    action_space = 18
+    observation_space = 30
+    state_space = 0
+
+    obs_mode: str = "state"
+
+    # simulation
+    sim: SimulationCfg = SimulationCfg(
+        device="cuda:0",
+        dt=1 / 100,
+        gravity=(0.0, 0.0, -9.81),
+        physx=PhysxCfg(
+            solver_type=1,
+            max_position_iteration_count=192,
+            max_velocity_iteration_count=1,
+            bounce_threshold_velocity=0.2,
+            friction_offset_threshold=0.01,
+            friction_correlation_distance=0.00625,
+            gpu_max_rigid_contact_count=2**23,
+            gpu_max_rigid_patch_count=2**23,
+            gpu_max_num_partitions=1,
+        ),
+        physics_material=RigidBodyMaterialCfg(
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+    )
+    # scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(
+        num_envs=2048,
+        env_spacing=2.5,
+        replicate_physics=True,  # TODO set to True for optimization
+    )
+
+    robot_left_cfg = FRANKA_PANDA_CFG.replace(prim_path="/World/envs/env_.*/Robot_left")
+    robot_left_cfg.spawn.activate_contact_sensors = True
+    robot_left_cfg.init_state.pos = (0, 0.75, 0)
+    robot_left_cfg.init_state.rot = tuple(
+        quat_from_euler_xyz(
+            torch.tensor(0.0),
+            torch.tensor(0.0),
+            torch.tensor(-torch.pi / 2.0),
+        ).tolist()
+    )  # identity quaternion
+    robot_right_cfg = FRANKA_PANDA_CFG.replace(
+        prim_path="/World/envs/env_.*/Robot_right"
+    )
+    robot_right_cfg.spawn.activate_contact_sensors = True
+    robot_right_cfg.init_state.pos = (0, -0.75, 0)
+    robot_right_cfg.init_state.rot = tuple(
+        quat_from_euler_xyz(
+            torch.tensor(0.0),
+            torch.tensor(0.0),
+            torch.tensor(torch.pi / 2.0),
+        ).tolist()
+    )
+
+    # TODO adapt position
+    sensors = (
+        CameraCfg(
+            prim_path="/World/envs/env_.*/Camera",  # Fixed camera
+            # prim_path="/World/envs/env_.*/Robot/panda_hand/Camera",  # attach to the wrist link
+            update_period=0.0,
+            height=128,
+            width=128,
+            data_types=["rgb"],
+            spawn=PinholeCameraCfg(
+                focal_length=24.0,
+                focus_distance=1.0,
+                horizontal_aperture=36.0,  # gives ~90° HFOV with f=24
+                clipping_range=(0.01, 100.0),
+            ),
+            offset=CameraCfg.OffsetCfg(
+                pos=(0.85, 0.0, 0.4),  # offset from center view
+                rot=(
+                    tuple(
+                        quat_from_euler_xyz(
+                            torch.tensor(0.6),
+                            torch.tensor(torch.pi),
+                            torch.tensor(-torch.pi / 2.0),
+                        ).tolist()
+                    )
+                ),  # identity quaternion
+                convention="ros",  # right-handed, X-forward, Z-up
+            ),
+        ),
+    )
+
+    tcp_marker_cfg = FRAME_MARKER_CFG.copy()
+    tcp_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+    tcp_marker_cfg.prim_path = "/Visuals/FrameTransformer"
+
+    target_marker_cfg = CUBOID_MARKER_CFG.copy()
+    target_marker_cfg.markers["cuboid"].size = (0.05, 0.05, 0.05)
+    target_marker_cfg.prim_path = "/Visuals/TargetMarker"
+
+    tcp_left_cfg = FrameTransformerCfg(
+        prim_path="/World/envs/env_.*/Robot_left/panda_link7",
+        debug_vis=True,
+        visualizer_cfg=tcp_marker_cfg,
+        target_frames=[
+            FrameTransformerCfg.FrameCfg(
+                prim_path="/World/envs/env_.*/Robot_left/panda_hand",
+                name="end_effector",
+                offset=OffsetCfg(
+                    pos=(0.0, 0.0, 0.1),
+                ),
+            ),
+        ],
+    )
+
+    tcp_right_cfg = FrameTransformerCfg(
+        prim_path="/World/envs/env_.*/Robot_right/panda_link7",
+        debug_vis=True,
+        visualizer_cfg=tcp_marker_cfg,
+        target_frames=[
+            FrameTransformerCfg.FrameCfg(
+                prim_path="/World/envs/env_.*/Robot_right/panda_hand",
+                name="end_effector",
+                offset=OffsetCfg(
+                    pos=(0.0, 0.0, 0.1),
+                ),
+            ),
+        ],
+    )
+
+    left_finger_cfg = FrameTransformerCfg(
+        prim_path="/World/envs/env_.*/Robot_left/panda_link7",
+        debug_vis=True,
+        visualizer_cfg=tcp_marker_cfg,
+        target_frames=[
+            FrameTransformerCfg.FrameCfg(
+                prim_path="/World/envs/env_.*/Robot_left/panda_leftfinger",
+                name="end_effector",
+                offset=OffsetCfg(
+                    pos=(0.0, 0.0, 0.045),
+                ),
+            ),
+        ],
+    )
+
+    right_finger_cfg = FrameTransformerCfg(
+        prim_path="/World/envs/env_.*/Robot_left/panda_link7",
+        debug_vis=True,
+        visualizer_cfg=tcp_marker_cfg,
+        target_frames=[
+            FrameTransformerCfg.FrameCfg(
+                prim_path="/World/envs/env_.*/Robot_left/panda_rightfinger",
+                name="end_effector",
+                offset=OffsetCfg(
+                    pos=(0.0, 0.0, 0.045),
+                ),
+            ),
+        ],
+    )
+
+    # Set Cube as object
+    cube_cfg = RigidObjectCfg(
+        prim_path="/World/envs/env_.*/Cube",
+        init_state=RigidObjectCfg.InitialStateCfg(
+            pos=(0.0, 0.3, 0.0), rot=(1, 0, 0, 0)
+        ),
+        spawn=UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/DexCube/dex_cube_instanceable.usd",
+            scale=(0.8, 0.8, 0.8),
+            rigid_props=RigidBodyPropertiesCfg(
+                solver_position_iteration_count=16,
+                solver_velocity_iteration_count=1,
+                max_angular_velocity=1000.0,
+                max_linear_velocity=1000.0,
+                max_depenetration_velocity=5.0,
+                disable_gravity=False,
+            ),
+        ),
+    )
+
+    success_distance_threshold = 0.025
+
+    cube_sample_range = [
+        [
+            -0.2,
+            0.2,
+            0.0,
+            0.0,
+            0.0,
+            -torch.pi,
+        ],
+        [
+            0.2,
+            0.3,
+            0.0,
+            0.0,
+            0.0,
+            torch.pi,
+        ],
+    ]
+
+    target_sample_range = [
+        [
+            -0.2,
+            -0.2,
+            0.1,
+            -torch.pi,
+            -torch.pi,
+            -torch.pi,
+        ],
+        [
+            0.2,
+            -0.3,
+            0.3,
+            torch.pi,
+            torch.pi,
+            torch.pi,
+        ],
+    ]
+
+
+@configclass
+class TwoRobotPickCubeCameraCfg(TwoRobotPickCubeCfg):
+
+    obs_mode: str = "camera"
