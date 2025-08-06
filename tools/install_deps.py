1d0c6cd396554230850b46e51512707e29bf9c3d
hhansen-bdai <hhansen@theaiinstitute.com>
2025-08-06 12:13:39 -0400

    Import order

--- tools/install_deps.py
+++ tools/install_deps.py
@@ -31,7 +31,7 @@ import argparse
 import os
 import shutil
 import toml
-from subprocess import PIPE, Popen, STDOUT
+from subprocess import PIPE, STDOUT, Popen
 
 # add argparse arguments
 parser = argparse.ArgumentParser(description="A utility to install dependencies based on extension.toml files.")
