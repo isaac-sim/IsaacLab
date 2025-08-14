6ca59ae676524e91e3498e1ce290090aec016e82
Michael Lin <michalin@nvidia.com>
2025-08-14 14:14:02 -0700

    Adding mimic envs for Waist Enabled Environment

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/pick_place/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/pick_place/__init__.py
@@ -49,6 +49,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": pickplace_gr1t2_waist_enabled_env_cfg.PickPlaceGR1T2WaistEnabledEnvCfg,
+        "robomimic_bc_cfg_entry_point": os.path.join(agents.__path__[0], "robomimic/bc_rnn_low_dim.json"),
     },
     disable_env_checker=True,
 )
