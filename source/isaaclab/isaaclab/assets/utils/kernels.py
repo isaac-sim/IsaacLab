1ef90d71963a1cc946f533a778b0eddfec921875
Antoine Richard <antoiner@nvidia.com>
2025-08-28 13:59:04 +0200

    pre-commits

--- source/isaaclab/isaaclab/assets/utils/kernels.py
+++ source/isaaclab/isaaclab/assets/utils/kernels.py
@@ -10,12 +10,14 @@ import warp as wp
 def skew_symetric_matrix(v: wp.vec3f) -> wp.mat33f:
     return wp.mat33f(0.0, -v[2], v[1], v[2], 0.0, -v[0], -v[1], v[0], 0.0)
 
+
 @wp.func
 def cast_to_com_frame(position: wp.vec3f, com_position: wp.vec3f, is_global: bool) -> wp.vec3f:
     if is_global:
         return position - com_position
     else:
         return position
+
 
 @wp.kernel
 def add_forces_and_torques_at_position(
@@ -34,7 +36,10 @@ def add_forces_and_torques_at_position(
         composed_forces_b[env_ids[tid_env], body_ids[tid_body]] += forces[tid_env, tid_body]
     if (positions.shape[0] > 0) and (forces.shape[0] > 0):
         composed_torques_b[env_ids[tid_env], body_ids[tid_body]] += (
-            skew_symetric_matrix(cast_to_com_frame(positions[tid_env, tid_body], com_positions[tid_env, tid_body], is_global)) @ forces[tid_env, tid_body]
+            skew_symetric_matrix(
+                cast_to_com_frame(positions[tid_env, tid_body], com_positions[tid_env, tid_body], is_global)
+            )
+            @ forces[tid_env, tid_body]
         )
     elif (com_positions.shape[0] > 0) and (forces.shape[0] > 0):
         composed_torques_b[env_ids[tid_env], body_ids[tid_body]] += (
