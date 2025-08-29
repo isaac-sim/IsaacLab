c46679b6ec49f2f891557668e7c5401990dbd3da
Antoine Richard <antoiner@nvidia.com>
2025-08-29 15:53:37 +0200

    adding current settings for a1 and spot

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/spot/mdp/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+"""This sub-module contains the functions that are specific to the Spot locomotion task."""
+
+from .events import *  # noqa: F401, F403
+from .rewards import *  # noqa: F401, F403
