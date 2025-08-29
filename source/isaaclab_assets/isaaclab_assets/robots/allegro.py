c42a3a34a40553945e041a908ce940e0d70127ee
Antoine Richard <antoiner@nvidia.com>
2025-08-29 17:50:00 +0200

    Initial support for Allegro. Currently there are collision issues

---
+++ source/isaaclab_assets/isaaclab_assets/robots/allegro.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Allegro Hand robots from Wonik Robotics.
+
+The following configurations are available:
+
+* :obj:`ALLEGRO_HAND_CFG`: Allegro Hand with implicit actuator model.
+
+Reference:
+
+* https://www.wonikrobotics.com/robot-hand
+
+"""
+
+
+import math
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators.actuator_cfg import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+ALLEGRO_HAND_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"{ISAAC_NUCLEUS_DIR}/Robots/WonikRobotics/AllegroHand/allegro_hand_instanceable.usd",
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.5),
+        rot=(0.257551, 0.283045, 0.683330, -0.621782),
+        joint_pos={"^(?!thumb_joint_0).*": 0.0, "thumb_joint_0": 0.28},
+    ),
+    actuators={
+        "fingers": ImplicitActuatorCfg(
+            control_mode="position",
+            joint_names_expr=[".*"],
+            effort_limit_sim=0.5,
+            stiffness=1.0,
+            damping=0.3,
+            friction=0.01,
+            armature=1e-5,
+        ),
+    },
+    soft_joint_pos_limit_factor=1.0,
+)
+"""Configuration of Allegro Hand robot."""
