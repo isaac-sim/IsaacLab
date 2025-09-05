332b8be0cf658f777a93e7b9b3ca8725ddbcd71a
René zurbrügg <zrene@ethz.ch>
2025-09-05 20:32:58 +0200

    improve docstrings

--- source/isaaclab/isaaclab/sensors/ray_caster/multi_mesh_ray_caster_cfg.py
+++ source/isaaclab/isaaclab/sensors/ray_caster/multi_mesh_ray_caster_cfg.py
@@ -34,17 +34,18 @@ class MultiMeshRayCasterCfg(RayCasterCfg):
         """Whether the target prim is a global object or exists for each environment instance. Defaults to False."""
 
         is_shared: bool = False
-        """Whether the target prim is shared across all environments. Defaults to False.
-        If True, the target prim is assumed to be the same mesh in all environments. In this case, the target prim is only read once
-        and the same warp mesh is used for all environments. This provides a performance boost when the target prim
-        is shared across all environments.
+        """Whether the target prim is assumed to be the same mesh across all environments. Defaults to False.
+
+        If True, only the first mesh is read and then reused for all environments, rather than re-parsed.
+        This provides a startup performance boost when there are many environments that all use the same asset. Note,
+        if reference_meshes is False, this flag has no effect.
         """
 
         merge_prim_meshes: bool = True
         """Whether to merge the meshes under each entry of :attr:`mesh_prim_paths`."""
 
         track_mesh_transforms: bool = False
-        """Whether the meshes transformations should be tracked. Defaults to False.
+        """Whether the mesh transformations should be tracked. Defaults to False.
 
         Note:
             Not tracking the mesh transformations is recommended when the meshes are static to increase performance.
@@ -53,12 +54,16 @@ class MultiMeshRayCasterCfg(RayCasterCfg):
     class_type: type = MultiMeshRayCaster
 
     mesh_prim_paths: list[str | RaycastTargetCfg] = MISSING
-    """The list of mesh primitive paths to ray cast against. If an entry is a string, it is treated as 
+    """The list of mesh primitive paths to ray cast against. If an entry is a string, it is treated as
     a global :attr:`target_prim_expr` of a :class:`RaycastTargetCfg` with default values for the other fields for backwards compatibility."""
-
 
     update_mesh_ids: bool = False
     """Whether to update the mesh ids of the ray hits in the :attr:`data` container."""
 
     reference_meshes: bool = True
-    """Whether to reference duplicated meshes instead of loading all of them to into memory. If set to false, each mesh in each environment will be loaded into memory separately."""
+    """Whether to reference duplicated meshes instead of loading each one separately into memory.
+    Defaults to True.
+
+    When enabled, the raycaster parses all meshes in all environments, but reuses references
+    for duplicates instead of storing multiple copies. This reduces memory footprint.
+    """
