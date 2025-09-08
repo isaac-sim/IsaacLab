9eae2548d12d60dd7afbabd2e5395228c572a0c0
Octi Zhang <zhengyuz@nvidia.com>
2025-09-08 00:34:43 -0700

    apply nit changes

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/__init__.py
@@ -3,9 +3,25 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+"""Dexsuite environments.
 
-"""Dexsuite environments."""
+Implementation Reference:
+
+Reorient:
+@article{petrenko2023dexpbt,
+  title={Dexpbt: Scaling up dexterous manipulation for hand-arm systems with population based training},
+  author={Petrenko, Aleksei and Allshire, Arthur and State, Gavriel and Handa, Ankur and Makoviychuk, Viktor},
+  journal={arXiv preprint arXiv:2305.12127},
+  year={2023}
+}
+
+Lift:
+@article{singh2024dextrah,
+  title={Dextrah-rgb: Visuomotor policies to grasp anything with dexterous hands},
+  author={Singh, Ritvik and Allshire, Arthur and Handa, Ankur and Ratliff, Nathan and Van Wyk, Karl},
+  journal={arXiv preprint arXiv:2412.01791},
+  year={2024}
+}
+
+"""
+
--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/mdp/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/mdp/__init__.py
@@ -7,6 +7,6 @@ from isaaclab.envs.mdp import *  # noqa: F401, F403
 
 from .curriculums import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
-from .pose_commands_cfg import *  # noqa: F401, F403
+from .commands import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
 from .terminations import *  # noqa: F401, F403
