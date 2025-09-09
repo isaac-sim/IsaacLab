9d194dc5c98de4e7051159a3dbca52bfd3768535
rebeccazhang0707 <168459200+rebeccazhang0707@users.noreply.github.com>
2025-09-09 09:29:32 +0800

    Adds two new robots with grippers (#3229)
    # Description
    
    <!--
    Thank you for your interest in sending a pull request. Please make sure
    to check the contribution guidelines.
    
    Link:
    https://isaac-sim.github.io/IsaacLab/main/source/refs/contributing.html
    -->
    
    Adds two new robots with grippers:
    - franka + robotiq_2f_85 gripper, with a new usd asset
    - ur10e + robotiq_2f_140 gripper, with IsaacSim UR10E asset with
    variants
    
    Test the two new arms with gripper with this script:
    [test_new_arms.py](https://github.com/user-attachments/files/22200295/test_new_arms.py)
    
    `./isaaclab.sh -p test_new_arms.py`
    
    <!-- As a practice, it is recommended to open an issue to have
    discussions on the proposed pull request.
    This makes it easier for the community to keep track of what is being
    developed or added, and if a given feature
    is demanded by more than one party. -->
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - New feature (non-breaking change which adds functionality)
    
    ## Screenshots
    
    ![ur10e_franka_robotiq_grippers](https://github.com/user-attachments/assets/8f904de2-90ad-4534-a274-d0d9a220ccf4)
    
    <!--
    Example:
    
    | Before | After |
    | ------ | ----- |
    | _gif/png before_ | _gif/png after_ |
    
    To upload images to a PR -- simply drag and drop an image while in edit
    mode and it should upload the image directly. You can then paste that
    source into the above before/after sections.
    -->
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->
    
    ---------
    
    Signed-off-by: rebeccazhang0707 <168459200+rebeccazhang0707@users.noreply.github.com>
    Signed-off-by: Kelly Guo <kellyg@nvidia.com>
    Co-authored-by: Kelly Guo <kellyg@nvidia.com>

--- source/isaaclab_assets/isaaclab_assets/robots/franka.py
+++ source/isaaclab_assets/isaaclab_assets/robots/franka.py
@@ -9,14 +9,16 @@ The following configurations are available:
 
 * :obj:`FRANKA_PANDA_CFG`: Franka Emika Panda robot with Panda hand
 * :obj:`FRANKA_PANDA_HIGH_PD_CFG`: Franka Emika Panda robot with Panda hand with stiffer PD control
+* :obj:`FRANKA_ROBOTIQ_GRIPPER_CFG`: Franka robot with Robotiq_2f_85 gripper
 
 Reference: https://github.com/frankaemika/franka_ros
 """
 
+
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
-from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR, ISAACLAB_NUCLEUS_DIR
 
 ##
 # Configuration
@@ -82,3 +84,65 @@ FRANKA_PANDA_HIGH_PD_CFG.actuators["panda_forearm"].damping = 80.0
 
 This configuration is useful for task-space control using differential IK.
 """
+
+
+FRANKA_ROBOTIQ_GRIPPER_CFG = FRANKA_PANDA_CFG.copy()
+FRANKA_ROBOTIQ_GRIPPER_CFG.spawn.usd_path = f"{ISAAC_NUCLEUS_DIR}/Robots/FrankaRobotics/FrankaPanda/franka.usd"
+FRANKA_ROBOTIQ_GRIPPER_CFG.spawn.variants = {"Gripper": "Robotiq_2F_85"}
+FRANKA_ROBOTIQ_GRIPPER_CFG.spawn.rigid_props.disable_gravity = True
+FRANKA_ROBOTIQ_GRIPPER_CFG.init_state.joint_pos = {
+    "panda_joint1": 0.0,
+    "panda_joint2": -0.569,
+    "panda_joint3": 0.0,
+    "panda_joint4": -2.810,
+    "panda_joint5": 0.0,
+    "panda_joint6": 3.037,
+    "panda_joint7": 0.741,
+    "finger_joint": 0.0,
+    ".*_inner_finger_joint": 0.0,
+    ".*_inner_finger_knuckle_joint": 0.0,
+    ".*_outer_.*_joint": 0.0,
+}
+FRANKA_ROBOTIQ_GRIPPER_CFG.init_state.pos = (-0.85, 0, 0.76)
+FRANKA_ROBOTIQ_GRIPPER_CFG.actuators = {
+    "panda_shoulder": ImplicitActuatorCfg(
+        joint_names_expr=["panda_joint[1-4]"],
+        effort_limit_sim=5200.0,
+        velocity_limit_sim=2.175,
+        stiffness=1100.0,
+        damping=80.0,
+    ),
+    "panda_forearm": ImplicitActuatorCfg(
+        joint_names_expr=["panda_joint[5-7]"],
+        effort_limit_sim=720.0,
+        velocity_limit_sim=2.61,
+        stiffness=1000.0,
+        damping=80.0,
+    ),
+    "gripper_drive": ImplicitActuatorCfg(
+        joint_names_expr=["finger_joint"],  # "right_outer_knuckle_joint" is its mimic joint
+        effort_limit_sim=1650,
+        velocity_limit_sim=10.0,
+        stiffness=17,
+        damping=0.02,
+    ),
+    # enable the gripper to grasp in a parallel manner
+    "gripper_finger": ImplicitActuatorCfg(
+        joint_names_expr=[".*_inner_finger_joint"],
+        effort_limit_sim=50,
+        velocity_limit_sim=10.0,
+        stiffness=0.2,
+        damping=0.001,
+    ),
+    # set PD to zero for passive joints in close-loop gripper
+    "gripper_passive": ImplicitActuatorCfg(
+        joint_names_expr=[".*_inner_finger_knuckle_joint", "right_outer_knuckle_joint"],
+        effort_limit_sim=1.0,
+        velocity_limit_sim=10.0,
+        stiffness=0.0,
+        damping=0.0,
+    ),
+}
+
+
+"""Configuration of Franka Emika Panda robot with Robotiq_2f_85 gripper."""
