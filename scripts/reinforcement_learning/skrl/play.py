9ba8467febd6dbd37df330aa715ff530e80e0c5a
Kelly Guo <kellyg@nvidia.com>
2025-09-10 17:46:43 -0700

    Adds logdir configuration parameter to environments (#3391)
    # Description
    
    Adds a logdir parameter for all environment configs to allow passing of
    the log dir for each experiment from the training/inferencing scripts to
    the environment object.
    
    This allows environments to access the logdir for the run and store log
    files in there, such as from the feature extractor.
    
    
    ## Type of change
    
    <!-- As you go through the list, delete the ones that are not
    applicable. -->
    
    - Bug fix (non-breaking change which fixes an issue)
    
    
    ## Checklist
    
    - [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [x] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    <!--
    As you go through the checklist above, you can mark something as done by
    putting an x character in it
    
    For example,
    - [x] I have done this task
    - [ ] I have not done this task
    -->
    
    ---------
    
    Signed-off-by: Kelly Guo <kellyg@nvidia.com>
    Signed-off-by: Kelly Guo <kellyguo123@hotmail.com>
    Co-authored-by: Mayank Mittal <12863862+Mayankm96@users.noreply.github.com>

--- scripts/reinforcement_learning/skrl/play.py
+++ scripts/reinforcement_learning/skrl/play.py
@@ -165,6 +165,9 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, expe
         )
     log_dir = os.path.dirname(os.path.dirname(resume_path))
 
+    # set the log directory for the environment (works for all environment types)
+    env_cfg.log_dir = log_dir
+
     # create isaac environment
     env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
 
