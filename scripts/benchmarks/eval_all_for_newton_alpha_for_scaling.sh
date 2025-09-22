89edffea3ccac9cf4e08e4632bef0f314bb4c4e9
Kelly Guo <kellyg@nvidia.com>
2025-09-21 17:18:15 -0700

    update

--- scripts/benchmarks/eval_all_for_newton_alpha_for_scaling.sh
+++ scripts/benchmarks/eval_all_for_newton_alpha_for_scaling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ISAAC_EXEC_SCRIPT=/home/kellyg/Documents/isaac/IsaacLab/isaaclab.sh
+ISAAC_EXEC_SCRIPT=/home/antoiner/Documents/IsaacLab-Internal/isaaclab.sh
 RSL_SCRIPT=scripts/benchmarks/benchmark_non_rl.py
 TASKS="Isaac-Repose-Cube-Allegro-Direct-v0 Isaac-Ant-Direct-v0 Isaac-Cartpole-Direct-v0 Isaac-Cartpole-RGB-Camera-Direct-v0 Isaac-Cartpole-Depth-Camera-Direct-v0 Isaac-Humanoid-Direct-v0 Isaac-Ant-v0 Isaac-Cartpole-v0 Isaac-Humanoid-v0 Isaac-Velocity-Flat-Unitree-A1-v0 Isaac-Velocity-Flat-Anymal-B-v0 Isaac-Velocity-Flat-Anymal-C-v0 Isaac-Velocity-Flat-Anymal-D-v0 Isaac-Velocity-Flat-Cassie-v0 Isaac-Velocity-Flat-G1-v0 Isaac-Velocity-Flat-G1-v1 Isaac-Velocity-Flat-Unitree-Go1-v0 Isaac-Velocity-Flat-Unitree-Go2-v0 Isaac-Velocity-Flat-H1-v0 Isaac-Velocity-Flat-Spot-v0 Isaac-Reach-Franka-v0 Isaac-Reach-UR10-v0"
 NUM_FRAMES=100
