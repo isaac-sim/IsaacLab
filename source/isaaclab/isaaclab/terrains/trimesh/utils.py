c82d66bb7ee3693edc1653635430253b51a3ba91
grzemal <grzegorz.malczyk@ntnu.no>
2025-10-17 14:30:41 +0200

    clean up

--- source/isaaclab/isaaclab/terrains/trimesh/utils.py
+++ source/isaaclab/isaaclab/terrains/trimesh/utils.py
@@ -102,7 +102,7 @@ def make_box(
     width: float,
     height: float,
     center: tuple[float, float, float],
-    max_yxz_angle: float = 0,
+    max_yx_angle: float = 0,
     degrees: bool = True,
 ) -> trimesh.Trimesh:
     """Generate a box mesh with a random orientation.
@@ -112,21 +112,21 @@ def make_box(
         width: The width (along y) of the box (in m).
         height: The height of the cylinder (in m).
         center: The center of the cylinder (in m).
-        max_yxz_angle: The maximum angle along the y,x and z axis. Defaults to 0.
+        max_yx_angle: The maximum angle along the y and x axis. Defaults to 0.
         degrees: Whether the angle is in degrees. Defaults to True.
 
     Returns:
-        A trimesh.Trimesh object for the box.
+        A trimesh.Trimesh object for the cylinder.
     """
-    # create a pose for the box
+    # create a pose for the cylinder
     transform = np.eye(4)
     transform[0:3, -1] = np.asarray(center)
     # -- create a random rotation
     euler_zyx = tf.Rotation.random().as_euler("zyx")  # returns rotation of shape (3,)
-    # -- cap the rotation along the x,y and z axis
+    # -- cap the rotation along the y and x axis
     if degrees:
-        max_yxz_angle = max_yxz_angle / 180.0
-    euler_zyx *= max_yxz_angle
+        max_yx_angle = max_yx_angle / 180.0
+    euler_zyx[1:] *= max_yx_angle
     # -- apply the rotation
     transform[0:3, 0:3] = tf.Rotation.from_euler("zyx", euler_zyx).as_matrix()
     # create the box
