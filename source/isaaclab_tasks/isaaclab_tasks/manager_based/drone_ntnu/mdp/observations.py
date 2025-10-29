0fb3f1831285af64891e7c6accdee8bf7364d945
Zwoelf12 <rehberg.welf@gmail.com>
2025-10-29 11:06:14 +0100

    added docstrings and removed obsolete terrain

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/drone_ntnu/mdp/observations.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/drone_ntnu/mdp/observations.py
@@ -11,13 +11,14 @@ the observation introduced by the function.
 
 from __future__ import annotations
 
-import torch, torch.jit
+import torch
+import torch.jit
 from typing import TYPE_CHECKING
 
 import isaaclab.utils.math as math_utils
 from isaaclab.assets import Articulation, RigidObject
 from isaaclab.managers import SceneEntityCfg
-from isaaclab.sensors import Camera,RayCasterCamera, TiledCamera, MultiMeshRayCasterCamera
+from isaaclab.sensors import Camera, RayCasterCamera, TiledCamera, MultiMeshRayCasterCamera
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedEnv, ManagerBasedRLEnv
