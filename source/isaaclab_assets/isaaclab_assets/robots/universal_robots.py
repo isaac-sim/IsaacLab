4c8aca6dcbed43d763747e5507820782e035a996
Ashwin Varghese Kuruttukulam <ashwinvk@nvidia.com>
2025-10-30 07:54:14 -0700

    stiffness=8.0, damping=0.04,

--- source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
+++ source/isaaclab_assets/isaaclab_assets/robots/universal_robots.py
@@ -162,8 +162,8 @@ UR10e_ROBOTIQ_GRIPPER_CFG.actuators["gripper_finger"] = ImplicitActuatorCfg(
     joint_names_expr=[".*_inner_finger_joint"],
     effort_limit_sim=1.0,
     velocity_limit_sim=1.0,
-    stiffness=4.0,
-    damping=0.02,
+    stiffness=8.0,
+    damping=0.04,
     friction=0.0,
     armature=0.0,
 )
