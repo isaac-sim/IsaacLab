357c6a0a7466ed9d72911b5225a9ab89f34396a0
Zwoelf12 <rehberg.welf@gmail.com>
2025-10-31 14:34:46 +0100

    remove obsolete curriculum

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/drone_arl/mdp/curriculums.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/drone_arl/mdp/curriculums.py
@@ -23,40 +23,6 @@ if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
 
 
-def terrain_levels_vel(
-    env: ManagerBasedRLEnv,
-    env_ids: Sequence[int],
-    asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
-    command_name: str = "target_pose",
-) -> torch.Tensor:
-    """Curriculum based on the distance the drone is from the target position at the end of the episode.
-
-    .. note::
-        It is only possible to use this term with the terrain type ``generator``. For further information
-        on different terrain types, check the :class:`isaaclab.terrains.TerrainImporter` class.
-
-    Returns:
-        The mean terrain level for the given environment ids.
-    """
-    # extract the used quantities (to enable type-hinting)
-    asset: Articulation = env.scene[asset_cfg.name]
-    terrain: TerrainImporter = env.scene.terrain
-    command = env.command_manager.get_command(command_name)
-
-    target_position_w = command[:, :3].clone()
-
-    current_position = asset.data.root_pos_w - env.scene.env_origins
-    position_error = torch.norm(target_position_w[env_ids] - current_position[env_ids], dim=1)
-
-    # robots that are within 1m range should progress to harder terrains
-    move_up = position_error < 1.5
-    move_down = env.termination_manager.terminated[env_ids]
-    # update terrain levels
-    terrain.update_drone_env_origins(env_ids, move_up, move_down)
-    # return the mean terrain level
-    return torch.mean(terrain.terrain_levels.float())
-
-
 def get_obstacle_curriculum_state(
     env: ManagerBasedRLEnv,
     min_difficulty: int = 2,
