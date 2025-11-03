b139fc1913f013ccbd2c86452e16e01f532bffad
mgussert <mgussert@nvidia.com>
2025-11-03 15:30:50 -0800

    swapping to other work

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/ur10_SG/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/lift/config/ur10_SG/agents/rsl_rl_ppo_cfg.py
@@ -13,7 +13,7 @@ class UR10LiftCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 1500
     save_interval = 50
-    experiment_name = "UR10SG_lift"
+    experiment_name = "franka_lift"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
         actor_obs_normalization=False,
