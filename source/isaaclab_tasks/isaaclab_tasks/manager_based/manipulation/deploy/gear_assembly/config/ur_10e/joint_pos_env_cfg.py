2187d209d22c3b9410e39585483433e5bfd69a5d
Ashwin Varghese Kuruttukulam <ashwinvk@nvidia.com>
2025-11-04 15:40:06 -0800

    "static_friction_range": (8.0

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/config/ur_10e/joint_pos_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/config/ur_10e/joint_pos_env_cfg.py
@@ -114,8 +114,8 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*finger"),
-            "static_friction_range": (4.0, 4.0),
-            "dynamic_friction_range": (4.0, 4.0),
+            "static_friction_range": (8.0, 8.0),
+            "dynamic_friction_range": (8.0, 8.0),
             "restitution_range": (0.0, 0.0),
             "num_buckets": 16,
         },
