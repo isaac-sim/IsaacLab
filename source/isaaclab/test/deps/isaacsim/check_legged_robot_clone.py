c32db68e909bcb311caf5d014a5dd23cde84bfdf
Pascal Roth <57946385+pascal-roth@users.noreply.github.com>
2025-11-13 05:25:37 +0100

    Replaces `nucleus_utils` with reimplementation as part of `IsaacLab` (#3921)
    # Description
    
    Replaces import of 
    
    ```
    try:
        import isaacsim.storage.native as nucleus_utils
    except ModuleNotFoundError:
        import isaacsim.core.utils.nucleus as nucleus_utils
    ```
    
    with own utils implemented inside the sim  utils folder.
    
    ```
    import isaaclab.sim.utils.nucleus as nucleus_utils
    ```
    
    
    ## Type of change
    
    - Dependency removal
    
    ## Checklist
    
    - [ ] I have read and understood the [contribution
    guidelines](https://isaac-sim.github.io/IsaacLab/main/source/refs/contributing.html)
    - [ ] I have run the [`pre-commit` checks](https://pre-commit.com/) with
    `./isaaclab.sh --format`
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have updated the changelog and the corresponding version in the
    extension's `config/extension.toml` file
    - [ ] I have added my name to the `CONTRIBUTORS.md` or my name already
    exists there
    
    ---------
    
    Co-authored-by: Octi Zhang <zhengyuz@nvidia.com>

--- source/isaaclab/test/deps/isaacsim/check_legged_robot_clone.py
+++ source/isaaclab/test/deps/isaacsim/check_legged_robot_clone.py
@@ -44,11 +44,6 @@ import logging
 import os
 import torch
 
-try:
-    import isaacsim.storage.native as nucleus_utils
-except ModuleNotFoundError:
-    import isaacsim.core.utils.nucleus as nucleus_utils
-
 import isaacsim.core.utils.prims as prim_utils
 from isaacsim.core.api.world import World
 from isaacsim.core.cloner import GridCloner
@@ -57,6 +52,7 @@ from isaacsim.core.utils.viewports import set_camera_view
 
 # import logger
 logger = logging.getLogger(__name__)
+import isaaclab.sim.utils.nucleus as nucleus_utils
 
 # check nucleus connection
 if nucleus_utils.get_assets_root_path() is None:
