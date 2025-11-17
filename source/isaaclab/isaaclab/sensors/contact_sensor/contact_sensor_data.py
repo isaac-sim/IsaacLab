b3e9640a90ad625fc3017d9dab030a0b1bec2d55
Greg Attra <gattra@rai-inst.com>
2025-11-17 11:12:00 -0500

    formatting bullet list

--- source/isaaclab/isaaclab/sensors/contact_sensor/contact_sensor_data.py
+++ source/isaaclab/isaaclab/sensors/contact_sensor/contact_sensor_data.py
@@ -37,8 +37,8 @@ class ContactSensorData:
         * If the :attr:`ContactSensorCfg.track_contact_points` is False, then this quantity is None.
         * If the :attr:`ContactSensorCfg.track_contact_points` is True, a ValueError will be raised if:
 
-            * If the :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty.
-            * If the :attr:`ContactSensorCfg.max_contact_data_per_prim` is not specified or less than 1.
+          * If the :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty.
+          * If the :attr:`ContactSensorCfg.max_contact_data_per_prim` is not specified or less than 1.
             will not be calculated.
 
     """
@@ -55,8 +55,8 @@ class ContactSensorData:
         * If the :attr:`ContactSensorCfg.track_friction_forces` is False, then this quantity is None.
         * If the :attr:`ContactSensorCfg.track_friction_forces` is True, a ValueError will be raised if:
 
-            * The :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty.
-            * The :attr:`ContactSensorCfg.max_contact_data_per_prim` is not specified or less than 1.
+          * The :attr:`ContactSensorCfg.filter_prim_paths_expr` is empty.
+          * The :attr:`ContactSensorCfg.max_contact_data_per_prim` is not specified or less than 1.
 
     """
 
