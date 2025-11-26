0d51e76484d421ccce0dc4711021848d493020b2
JinnnK <wls8074@gmail.com>
2025-11-26 12:50:43 +0900

    updated license

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/reach_openarm_uni_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/reach/reach_openarm_uni_env_cfg.py
@@ -1,16 +1,7 @@
-# Copyright 2025 Enactic, Inc.
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: BSD-3-Clause
 
 from dataclasses import MISSING
 
@@ -32,7 +23,7 @@ from isaaclab.utils import configclass
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
 
-from . import mdp
+import isaaclab_tasks.manager_based.manipulation.reach.mdp as mdp
 
 import math
 
