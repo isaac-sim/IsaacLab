4028970ec9841290aeecae5496fb0860574de373
Octi Zhang <zhengyuz@nvidia.com>
2025-12-02 17:29:29 -0800

    add botocore to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -142,6 +142,7 @@ exclude_patterns = ["_build", "_redirect", "_templates", "Thumbs.db", ".DS_Store
 # Mock out modules that are not available on RTD
 autodoc_mock_imports = [
     "boto3",
+    "botocore",
     "torch",
     "torchvision",
     "numpy",
