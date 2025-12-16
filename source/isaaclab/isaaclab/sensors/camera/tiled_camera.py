fb6b7e3181c80c7a3e293f2c6f6be362dbeab055
Ada Yuan <yuanxiaodidl@163.com>
2025-12-15 22:02:50 -0800

    Fix Bug: incorrect normals in TiledCamera

--- source/isaaclab/isaaclab/sensors/camera/tiled_camera.py
+++ source/isaaclab/isaaclab/sensors/camera/tiled_camera.py
@@ -201,6 +201,13 @@ class TiledCamera(Camera):
                     "distance_to_image_plane", device=self.device, do_array_copy=False
                 )
                 self._annotators[annotator_type] = annotator
+            elif annotator_type == "normals":
+                annotator = rep.AnnotatorRegistry.get_annotator(
+                    "normals", device=self.device, do_array_copy=False
+                )
+                # annotator returns 4-channel data, store as normals_padded
+                # we will extract first 3 channels to "normals"
+                self._annotators["normals_padded"] = annotator
             # note: we are verbose here to make it easier to understand the code.
             #   if colorize is true, the data is mapped to colors and a uint8 4 channel image is returned.
             #   if colorize is false, the data is returned as a uint32 image with ids as values.
@@ -286,6 +293,14 @@ class TiledCamera(Camera):
             # alias rgb as first 3 channels of rgba
             if data_type == "rgba" and "rgb" in self.cfg.data_types:
                 self._data.output["rgb"] = self._data.output["rgba"][..., :3]
+                
+            if data_type == "normals_padded":
+                if "normals" not in self.cfg.data_types:
+                    raise ValueError(
+                        f"TiledCamera: 'normals_padded' annotator was created but 'normals' is not in cfg.data_types: {self.cfg.data_types}. "
+                        "This indicates an internal inconsistency in the sensor configuration."
+                    )
+                self._data.output["normals"] = self._data.output["normals_padded"][..., :3]
 
             # NOTE: The `distance_to_camera` annotator returns the distance to the camera optical center. However,
             #       the replicator depth clipping is applied w.r.t. to the image plane which may result in values
@@ -360,9 +375,10 @@ class TiledCamera(Camera):
                 (self._view.count, self.cfg.height, self.cfg.width, 1), device=self.device, dtype=torch.float32
             ).contiguous()
         if "normals" in self.cfg.data_types:
-            data_dict["normals"] = torch.zeros(
-                (self._view.count, self.cfg.height, self.cfg.width, 3), device=self.device, dtype=torch.float32
+            data_dict["normals_padded"] = torch.zeros(
+                (self._view.count, self.cfg.height, self.cfg.width, 4), device=self.device, dtype=torch.float32
             ).contiguous()
+            data_dict["normals"] = data_dict["normals_padded"][..., :3]
         if "motion_vectors" in self.cfg.data_types:
             data_dict["motion_vectors"] = torch.zeros(
                 (self._view.count, self.cfg.height, self.cfg.width, 2), device=self.device, dtype=torch.float32
