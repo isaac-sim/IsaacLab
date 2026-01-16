88a1f07272fb58b664c37ae4f8507fb13c6684ab
Kelly Guo <kellyg@nvidia.com>
2026-01-15 20:42:08 -0800

    Update source/isaaclab_mimic/isaaclab_mimic/datagen/datagen_info.py
    Signed-off-by: Kelly Guo <kellyg@nvidia.com>

--- source/isaaclab_mimic/isaaclab_mimic/datagen/datagen_info.py
+++ source/isaaclab_mimic/isaaclab_mimic/datagen/datagen_info.py
@@ -38,6 +38,7 @@ class DatagenInfo:
         gripper_action=None,
     ):
         """Initialize the DatagenInfo object.
+
         Args:
             eef_pose (torch.Tensor or None): robot end effector poses of shape [..., 4, 4]
             object_poses (dict or None): dictionary mapping object name to object poses
