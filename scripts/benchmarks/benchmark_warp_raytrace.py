a43048b48e12e900b82f151838e7e29348e4ad13
Daniela Hasenbring <dhasenbring@nvidia.com>
2026-01-23 17:39:12 -0800

    Working on WarpRender conversion

---
+++ scripts/benchmarks/benchmark_warp_raytrace.py
@@ -0,0 +1,140 @@
+import contextlib
+import argparse
+import time
+import os
+
+from isaaclab.app import AppLauncher
+
+@contextlib.contextmanager
+def measure_time(title: str, iterations: int = 1):
+    start_time = time.perf_counter_ns()
+    yield
+    end_time = time.perf_counter_ns()
+    print(f"[BENCHMARK] {title}: {(end_time - start_time) / iterations / 1e6:.2f} ms")
+
+
+@contextlib.contextmanager
+def launch_app(args):
+    with measure_time("App start time"):
+        app_launcher = AppLauncher(args)
+    yield app_launcher.app
+    if app_launcher.app:
+        app_launcher.app.close()
+
+
+parser = argparse.ArgumentParser(description="Benchmark Warp Raytrace")
+parser.add_argument("--num_envs", type=int, default=32, help="Number of robots to simulate")
+parser.add_argument("--save_images", action="store_true", help="Save Sensor Images")
+parser.add_argument("--steps", type=int, default=2000, help="Number of steps to simulate")
+
+AppLauncher.add_app_launcher_args(parser)
+args, _ = parser.parse_known_args()
+
+app_launcher = AppLauncher(args)
+
+with measure_time("Imports time"):
+    import torch
+
+    import isaaclab.sim as isaaclab_sim
+    from isaaclab.sensors import TiledCamera, TiledCameraCfg
+    from isaaclab.assets import Articulation, ArticulationCfg, AssetBaseCfg
+    from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
+    from isaaclab.utils import configclass
+    from isaaclab_assets import ANYMAL_D_CFG
+    from warp_convert import WarpRTX_Renderer
+
+
+@configclass
+class SceneCfg(InteractiveSceneCfg):
+    ground = AssetBaseCfg(prim_path="/World/defaultGroundPlane", spawn=isaaclab_sim.GroundPlaneCfg())
+    dome_light = AssetBaseCfg(prim_path="/World/Light", spawn=isaaclab_sim.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75)))
+    robot: ArticulationCfg = ANYMAL_D_CFG
+    robot.prim_path = "{ENV_REGEX_NS}/Robot"
+
+
+def save_image(sensor, frame_no: int):
+    tiled_camera: TiledCamera = sensor
+    data = tiled_camera.data.output["rgb"].cpu().numpy()
+    shape = data.shape
+    for tile in range(shape[0]):
+        tile_data = data[tile,:,:]
+        
+        from PIL import Image
+        os.makedirs("benchmark_tiled_camera", exist_ok=True)
+        Image.fromarray(tile_data).save(f"benchmark_tiled_camera/rgb.{tile:02d}.{frame_no:04d}.png")
+
+
+def run_simulator(sim: isaaclab_sim.SimulationContext, scene: InteractiveScene, num_steps: int, save_images: bool):
+    robot: Articulation = scene["robot"]
+    for step in range(num_steps):
+        if step == 0:
+            if step % 500 == 0:
+                # reset the scene entities root state we offset the root state by the origin since the states are 
+                # written in simulation world frame if this is not done, then the robots will be spawned at 
+                # the (0, 0, 0) of the simulation world
+
+                root_state = robot.data.default_root_state.clone()
+                root_state[:, :3] += scene.env_origins
+                robot.write_root_pose_to_sim(root_state[:, :7])
+                robot.write_root_velocity_to_sim(root_state[:, 7:])
+                # set joint positions with some noise
+                joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
+                joint_pos += torch.rand_like(joint_pos) * 0.1
+                robot.write_joint_state_to_sim(joint_pos, joint_vel)
+                # clear internal buffers
+                scene.reset()
+
+            # Apply random action
+            efforts = torch.randn_like(robot.data.joint_pos) * 5.0
+            robot.set_joint_effort_target(efforts)
+
+            scene.write_data_to_sim()
+            sim.step()
+        sim.render()
+        if step == 0:
+            scene.update(sim.get_physics_dt())
+
+        if save_images:
+            save_image(scene.sensors["tiled_camera"], step)
+
+        # Have to access the sensor data to update buffers internally ..
+        # scene.sensors["tiled_camera"].data
+
+
+def main():
+    with measure_time("Simulation Context creation time"):
+        sim_cfg = isaaclab_sim.SimulationCfg(device="cuda:0")
+        sim = isaaclab_sim.SimulationContext(sim_cfg)
+
+    sim.set_camera_view((2.5, 0.0, 4.0), (0.0, 0.0, 2.0))
+
+    with measure_time("Scene creation time"):
+        scene_cfg = SceneCfg(num_envs=args.num_envs, env_spacing=2.0)
+        scene = InteractiveScene(scene_cfg)
+
+        tiled_camera_cfg: TiledCameraCfg = TiledCameraCfg(
+            prim_path="/World/envs/env_.*/Camera",
+            offset=TiledCameraCfg.OffsetCfg(pos=(-3.0, 0.0, 1.0), rot=(0.0, 0.0, 0.0, 1.0), convention="world"),
+            data_types=["rgb"],
+            spawn=isaaclab_sim.PinholeCameraCfg(focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 20.0)),
+            width=400,
+            height=400,
+        )
+        scene.sensors["tiled_camera"] = TiledCamera(tiled_camera_cfg)
+
+        renderer = WarpRTX_Renderer(scene, 500, 500)
+        renderer.render()
+
+        # stage = isaaclab_sim.get_current_stage()
+        # stage.Export("/home/dhasenbring/development/isaac/IsaacLab/stage.usda")
+
+    with measure_time("Sim start time"):
+        sim.reset()
+
+    with measure_time("Average sim step time", iterations=args.steps):
+        run_simulator(sim, scene, args.steps, args.save_images)
+
+
+if __name__ == "__main__":
+    main()
+    app_launcher.app.close()
---
+++ scripts/benchmarks/warp_convert.py
@@ -0,0 +1,300 @@
+from isaaclab.utils.math import convert_camera_frame_orientation_convention
+from isaaclab.scene import InteractiveScene
+import isaaclab.sim as isaaclab_sim
+
+from dataclasses import dataclass, field
+from warp_raytrace import RenderContext, RenderShapeType
+from pxr import Usd, UsdGeom, Gf
+
+import numpy as np
+import warp as wp
+import re
+import os
+
+
+@wp.kernel(enable_backward=False)
+def compute_triangle_count(num_face_counts: wp.int32, face_counts: wp.array(dtype=wp.int32), out_face_offsets: wp.array(dtype=wp.int32)):
+    offset = wp.int32(0)
+    for i in range(num_face_counts):
+        out_face_offsets[i] = offset
+        offset += face_counts[i]
+    out_face_offsets[num_face_counts] = offset - num_face_counts * 2
+
+
+@wp.kernel(enable_backward=False)
+def triangulate_faces(num_face_counts: wp.int32, face_counts: wp.array(dtype=wp.int32), face_indices: wp.array(dtype=wp.int32), face_offsets: wp.array(dtype=wp.int32), out_triangles: wp.array(dtype=wp.int32)):
+    offset = face_offsets[wp.tid()]
+    num_triangles = face_counts[wp.tid()] - 2
+    tri = wp.atomic_add(face_offsets, num_face_counts + 1, num_triangles * 3)
+
+    for i in range(num_triangles):
+        out_triangles[tri + i * 3 + 0] = face_indices[offset]
+        out_triangles[tri + i * 3 + 1] = face_indices[offset + i + 1]
+        out_triangles[tri + i * 3 + 2] = face_indices[offset + i + 2]
+
+
+class WarpRTX_Renderer:
+    @dataclass
+    class PrimData:
+        is_shared: bool = False
+        prim_type: RenderShapeType = RenderShapeType.NONE
+        master_prim: Usd.Prim | None = None
+        prims: list[tuple[int, Usd.Prim]] = field(default_factory=lambda: [])
+
+    @dataclass
+    class CameraData:
+        is_shared: bool = False
+        master_prim: Usd.Prim | None = None
+        prims: list[tuple[int, Usd.Prim]] = field(default_factory=lambda: [])
+
+    def __init__(self, scene: InteractiveScene, width: int, height: int):
+        self.num_worlds = 0
+        self.num_cameras = 1
+        self.camera_data: dict[str, WarpRTX_Renderer.CameraData] = {}
+        self.prim_data: dict[str, WarpRTX_Renderer.PrimData] = {}
+        self.__collect_prims(isaaclab_sim.get_current_stage())
+
+        shape_transforms = []
+        shape_sizes = []
+        shape_types = []
+        shape_mesh_indices = []
+        shape_world_indices = []
+
+        self.__warp_meshes = []
+
+        for prim_path, prim_data in self.prim_data.items():
+            if prim_data.is_shared:
+                if prim_data.prim_type == RenderShapeType.MESH:
+                    mesh_index = len(self.__warp_meshes)
+                    self.__warp_meshes.append(self.__build_mesh(prim_data.master_prim))
+
+                    for world_id, prim in prim_data.prims:
+                        shape_transforms.append(self.__resolve_transform(prim))
+                        shape_sizes.append(self.__resolve_scale(prim))
+                        shape_types.append(RenderShapeType.MESH)
+                        shape_mesh_indices.append(mesh_index)
+                        shape_world_indices.append(world_id)
+
+        self.render_context = RenderContext(width, height, self.num_worlds, self.num_cameras)
+        self.render_context.num_shapes_total = len(shape_transforms)
+        self.render_context.num_shapes_enabled = self.render_context.num_shapes_total
+        self.render_context.mesh_ids = wp.array([mesh.id for mesh in self.__warp_meshes], dtype=wp.uint64)
+        self.render_context.shape_mesh_indices = wp.array(shape_mesh_indices, dtype=wp.int32)
+        self.render_context.shape_types = wp.array(shape_types, dtype=wp.int32)
+        self.render_context.mesh_bounds = wp.empty((self.render_context.num_shapes_total, 2), dtype=wp.vec3f, ndim=2)
+        self.render_context.shape_enabled = wp.array(np.arange(self.render_context.num_shapes_total), dtype=wp.uint32)
+        self.render_context.shape_sizes = wp.array(shape_sizes, dtype=wp.vec3f)
+        self.render_context.shape_transforms = wp.array(shape_transforms, dtype=wp.transformf)
+        self.render_context.shape_materials = wp.array(np.full(self.render_context.num_shapes_total, fill_value=-1, dtype=np.int32), dtype=wp.int32)
+        self.render_context.shape_world_index = wp.array(shape_world_indices, dtype=wp.int32)
+        self.render_context.utils.compute_mesh_bounds()
+
+        self.render_context.utils.create_default_light(False)
+        self.render_context.utils.assign_random_colors_per_shape()
+
+        self.camera_fovs = wp.array([20.0] * self.render_context.num_cameras, dtype=wp.float32)
+        self.camera_rays = self.render_context.utils.compute_pinhole_camera_rays(self.camera_fovs)
+        self.color_image = self.render_context.create_color_image_output()
+
+    def update(self):
+        # ... update from stage!
+        pass
+
+    def render(self):
+        self.render_context.render(self.__get_camera_transforms(), self.camera_rays, self.color_image)
+
+        color_data = self.render_context.utils.flatten_color_image_to_rgba(self.color_image)
+        
+        from PIL import Image
+        os.makedirs("benchmark_warp_raytrace", exist_ok=True)
+        Image.fromarray(color_data.numpy()).save(f"benchmark_warp_raytrace/meow.png")
+
+    def __collect_prims(self, stage: Usd.Stage, env_regex: str = r"(?P<root>/World/envs/env_)(?P<id>\d+)(?P<path>/.*)"):
+        env_pattern = re.compile(env_regex)
+
+        num_worlds = -1
+        stage_prims: list[Usd.Prim] = [stage.GetPseudoRoot()]
+        while stage_prims:
+            prim = stage_prims.pop(0)
+            prim_path = prim.GetPath().pathString
+
+            world_id = -1
+            if match := env_pattern.match(prim_path):
+                world_id = int(match.group("id"))
+                prim_path = match.group("root") + "%d" + match.group("path")
+
+            if world_id > -1:
+                if world_id > num_worlds:
+                    num_worlds = world_id
+
+            imageable = UsdGeom.Imageable(prim)
+            if imageable and imageable.ComputeVisibility() == UsdGeom.Tokens.invisible:
+                continue
+
+            # Capsule, Cube, Cylinder, Mesh, Plane, Sphere
+
+            if prim.IsA(UsdGeom.Mesh):
+                if not prim_path in self.prim_data:
+                    self.prim_data[prim_path] = WarpRTX_Renderer.PrimData(world_id > -1, RenderShapeType.MESH, prim)
+                self.prim_data[prim_path].prims.append((world_id, prim))
+
+            if prim.IsA(UsdGeom.Camera):
+                if not prim_path in self.camera_data:
+                    self.camera_data[prim_path] = WarpRTX_Renderer.CameraData(world_id > -1, prim)
+                self.camera_data[prim_path].prims.append((world_id, prim))
+
+            if child_prims := prim.GetFilteredChildren(Usd.TraverseInstanceProxies()):
+                stage_prims.extend(child_prims)
+
+        self.num_cameras = 0
+        for prim_path, camera_data in self.camera_data.items():
+            if camera_data.is_shared:
+                self.num_cameras += 1
+        self.num_worlds = num_worlds + 1
+
+    def __build_mesh(self, prim: Usd.Prim):
+        mesh = UsdGeom.Mesh(prim)
+        points = wp.array(mesh.GetPointsAttr().Get(), dtype=wp.vec3f)
+        face_vertex_counts = wp.array(mesh.GetFaceVertexCountsAttr().Get(), dtype=wp.int32)
+        face_vertex_indices = wp.array(mesh.GetFaceVertexIndicesAttr().Get(), dtype=wp.int32)
+
+        num_face_counts = face_vertex_counts.size
+        wp_face_offsets = wp.zeros(num_face_counts + 2, dtype=wp.int32)
+
+        wp.launch(kernel=compute_triangle_count, dim=1, inputs=[num_face_counts, face_vertex_counts, wp_face_offsets])
+
+        num_triangles = wp_face_offsets.numpy()[num_face_counts].item()
+        wp_triangle_indices = wp.empty(num_triangles * 3, dtype=wp.int32)
+
+        wp.launch(kernel=triangulate_faces, dim=num_face_counts, inputs=[num_face_counts, face_vertex_counts, face_vertex_indices, wp_face_offsets, wp_triangle_indices])
+
+        return wp.Mesh(points=points, velocities=None, indices=wp_triangle_indices)
+
+    def __resolve_transform(self, prim: Usd.Prim) -> wp.transformf:
+        position, orientation = isaaclab_sim.resolve_prim_pose(prim)
+        return wp.transformf(position, wp.quatf(orientation[1], orientation[2], orientation[3], orientation[0]))
+
+    def __resolve_scale(self, prim: Usd.Prim) -> wp.vec3f:
+        scale = isaaclab_sim.resolve_prim_scale(prim)
+        return wp.vec3f(scale)
+
+    def __resolve_camera_transform(self, prim: Usd.Prim) -> wp.transformf:
+        # position, orientation = isaaclab_sim.resolve_prim_pose(prim)
+        # t = torch.tensor(orientation, dtype=torch.float32, device="cpu").unsqueeze(0)
+        # t = convert_camera_frame_orientation_convention(t)
+        # orientation = t.squeeze(0).cpu().numpy()
+        # return wp.transformf(position, wp.quatf(orientation))
+
+        position, orientation = isaaclab_sim.resolve_prim_pose(prim)
+        return wp.transformf(position, wp.quatf(orientation[1], -orientation[2], -orientation[3], orientation[0]))
+
+
+    def __get_camera_transforms(self) -> wp.array(dtype=wp.transformf):
+        camera_transforms = [[] for i in range(self.num_worlds)]
+        for prim_path, camera_data in self.camera_data.items():
+            if camera_data.is_shared:
+                for world_id, camera_prim in camera_data.prims:
+                    camera_transforms[world_id].append(self.__resolve_camera_transform(camera_prim))
+
+        return wp.array(camera_transforms, dtype=wp.transformf)
+
+
+# def extract_scene_geometry_per_frame(
+#     scene: InteractiveScene,
+#     sim: isaaclab_sim.SimulationContext,
+#     root_path: str = "/World",
+# ) -> List[GeometryData]:
+#     """Extract geometry with current frame transforms.
+    
+#     Call this function after scene.update() to get per-frame transforms.
+#     This version handles multi-env scenarios better by extracting transforms
+#     for all environments.
+    
+#     Args:
+#         scene: The interactive scene
+#         sim: The simulation context  
+#         root_path: Root path to search from
+        
+#     Returns:
+#         List of GeometryData objects with current frame transforms
+#     """
+#     # Update scene to get latest transforms
+#     scene.update(sim.get_physics_dt())
+    
+#     stage = isaaclab_sim.get_current_stage()
+#     geometry_list = []
+    
+#     def is_geometry_prim(prim: Usd.Prim) -> bool:
+#         prim_type = prim.GetTypeName()
+#         return prim_type == "Mesh" or prim_type in PRIMITIVE_MESH_TYPES
+    
+#     all_geometry_prims = isaaclab_sim.get_all_matching_child_prims(
+#         root_path,
+#         predicate=is_geometry_prim,
+#         traverse_instance_prims=True,
+#     )
+    
+#     for geom_prim in all_geometry_prims:
+#         prim_path = geom_prim.GetPath().pathString
+#         prim_type = geom_prim.GetTypeName()
+        
+#         if not geom_prim.IsValid():
+#             continue
+        
+#         # Extract mesh geometry (static, doesn't change per frame)
+#         try:
+#             if prim_type == "Mesh":
+#                 trimesh_obj = create_trimesh_from_geom_mesh(geom_prim)
+#             else:
+#                 trimesh_obj = create_trimesh_from_geom_shape(geom_prim)
+            
+#             vertices = trimesh_obj.vertices
+#             faces = trimesh_obj.faces
+#         except Exception as e:
+#             continue
+        
+#         # Get current frame transform
+#         try:
+#             pos_w, quat_w = isaaclab_sim.resolve_prim_pose(geom_prim)
+#             pos_w = np.array(pos_w)
+#             quat_w = np.array(quat_w)
+#         except Exception as e:
+#             continue
+        
+#         # Build transform matrix
+#         w, x, y, z = quat_w
+#         rotation_matrix = np.array([
+#             [1 - 2*(y**2 + z**2), 2*(x*y - z*w), 2*(x*z + y*w)],
+#             [2*(x*y + z*w), 1 - 2*(x**2 + z**2), 2*(y*z - x*w)],
+#             [2*(x*z - y*w), 2*(y*z + x*w), 1 - 2*(x**2 + y**2)]
+#         ])
+        
+#         transform_matrix = np.eye(4)
+#         transform_matrix[:3, :3] = rotation_matrix
+#         transform_matrix[:3, 3] = pos_w
+        
+#         # Check if physics object
+#         is_physics = (
+#             geom_prim.HasAPI(UsdPhysics.RigidBodyAPI) or
+#             geom_prim.HasAPI(UsdPhysics.ArticulationRootAPI) or
+#             any(geom_prim.GetPath().IsDescendantPath(Usd.Stage.GetPrimAtPath(stage, asset.cfg.prim_path).GetPath())
+#                 for asset_list in [scene.articulations.values(), scene.rigid_objects.values()]
+#                 for asset in asset_list)
+#         )
+        
+#         geom_data = GeometryData(
+#             prim_path=prim_path,
+#             vertices=vertices,
+#             faces=faces,
+#             position=pos_w,
+#             quaternion=quat_w,
+#             transform_matrix=transform_matrix,
+#             prim_type=prim_type,
+#             is_physics=is_physics,
+#         )
+        
+#         geometry_list.append(geom_data)
+    
+#     return geometry_list
+
+
