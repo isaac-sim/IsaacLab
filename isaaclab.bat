0a24c6aafd48c9673c64a64e3601195d16e7aed1
Mikhail Yurasov <myurasov@nvidia.com>
2026-02-09 11:49:36 -0800

    Improve comments to explain what's going on; fix license headers.

--- isaaclab.bat
+++ isaaclab.bat
@@ -1,16 +1,17 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-rem Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+rem Copyright (c) 2022-2025, The Isaac Lab Project Developers
+rem (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
 rem All rights reserved.
 rem
 rem SPDX-License-Identifier: BSD-3-Clause
 
 set "ISAACLAB_PATH=%~dp0"
-rem remove trailing backslash
+rem Remove trailing backslash.
 if "%ISAACLAB_PATH:~-1%"=="\" set "ISAACLAB_PATH=%ISAACLAB_PATH:~0,-1%"
 
-rem Check python
+rem Find python to run CLI.
 if defined VIRTUAL_ENV (
     set "python_exe=%VIRTUAL_ENV%\Scripts\python.exe"
 ) else if defined CONDA_PREFIX (
@@ -18,15 +19,14 @@ if defined VIRTUAL_ENV (
 ) else if exist "%ISAACLAB_PATH%\_isaac_sim\python.bat" (
     set "python_exe=%ISAACLAB_PATH%\_isaac_sim\python.bat"
 ) else (
-    rem Fallback
+    rem Fallback.
     set "python_exe=python"
 )
 
+rem Add source/isaaclab to PYTHONPATH so we can import isaaclab.cli.
 set "PYTHONPATH=%ISAACLAB_PATH%\source\isaaclab;%PYTHONPATH%"
 
-echo [INFO] Using Python: "%python_exe%"
-
-rem Execute CLI
+rem Execute CLI.
 "%python_exe%" -c "from isaaclab.cli import cli; cli()" %*
 
 if errorlevel 1 exit /b 1
--- isaaclab.sh
+++ isaaclab.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Exits if error occurs
+# Exit on error.
 set -e
 
-# get source directory
+# Get repo directory.
 export ISAACLAB_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-# Check if using conda or uv or venv
+# Find python to run CLI.
 if [ -n "$VIRTUAL_ENV" ]; then
     python_exe="$VIRTUAL_ENV/bin/python"
 elif [ -n "$CONDA_PREFIX" ]; then
@@ -23,10 +23,8 @@ else
     python_exe="python3"
 fi
 
-# Add source/isaaclab to PYTHONPATH so we can import isaaclab.cli
+# Add source/isaaclab to PYTHONPATH so we can import isaaclab.cli.
 export PYTHONPATH="$ISAACLAB_PATH/source/isaaclab:$PYTHONPATH"
 
-echo "[INFO] Using Python: $python_exe"
-
-# Execute CLI
+# Execute CLI.
 exec "$python_exe" -c "from isaaclab.cli import cli; cli()" "$@"
