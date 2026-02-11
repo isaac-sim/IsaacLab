f6507496f020f438454aa16cea9202f6012300f6
Kelly Guo <kellyg@nvidia.com>
2026-02-10 20:51:50 -0800

    Update source/isaaclab/test/markers/check_markers_visibility.py
    Signed-off-by: Kelly Guo <kellyg@nvidia.com>

--- source/isaaclab/test/markers/check_markers_visibility.py
+++ source/isaaclab/test/markers/check_markers_visibility.py
@@ -38,7 +38,6 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
-
 import isaaclab.sim as sim_utils
 from isaaclab.assets import ArticulationCfg, AssetBaseCfg
 from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
