143578436abe4c0196a9fa9a666d302c97fdc422
sameerchavan <sameerchavan@nvidia.com>
2026-03-05 21:59:06 -0800

    Check active visualizers

--- source/isaaclab/isaaclab/envs/manager_based_env.py
+++ source/isaaclab/isaaclab/envs/manager_based_env.py
@@ -151,6 +151,7 @@ class ManagerBasedEnv:
         # Initialize when GUI is available OR when visualizers are active (headless rendering)
         # Visualizers support camera updates via sim.set_camera_view() which forwards to all active visualizers
         has_visualizers = bool(self.sim.get_setting("/isaaclab/visualizer"))
+        if self.sim.has_gui or has_visualizers:
             self.viewport_camera_controller = ViewportCameraController(self, self.cfg.viewer)
         else:
             self.viewport_camera_controller = None
