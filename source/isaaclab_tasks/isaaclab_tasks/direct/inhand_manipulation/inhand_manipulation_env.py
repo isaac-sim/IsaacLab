Fixes in hand env goal computation

source/isaaclab_tasks/isaaclab_tasks/direct/inhand_manipulation/inhand_manipulation_env.py
@@ -394,8 +394,8 @@ def rotation_distance(object_rot, target_rot):
     # Orientation alignment for the cube in hand and goal cube
     quat_diff = quat_mul(object_rot, quat_conjugate(target_rot))
     return 2.0 * torch.asin(
-        torch.clamp(torch.linalg.norm(quat_diff[:, 1:4], ord=2, dim=-1), max=1.0)
-    )  # changed quat convention
+        torch.clamp(torch.linalg.norm(quat_diff[:, 0:3], ord=2, dim=-1), max=1.0)
+    )
 
 
 @torch.jit.script