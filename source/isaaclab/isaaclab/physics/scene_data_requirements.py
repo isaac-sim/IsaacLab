2015595ded9eda75600d8ad25314ced73779f2eb
Matthew Trepte <mtrepte@nvidia.com>
2026-03-09 22:41:39 +0000

    add copyright header

--- source/isaaclab/isaaclab/physics/scene_data_requirements.py
+++ source/isaaclab/isaaclab/physics/scene_data_requirements.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 """Central requirement resolution for scene-data consumers.
 
 This module is intentionally type-based (not config-import based) so requirement
