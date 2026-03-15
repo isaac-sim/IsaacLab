c1b1b9ec6659d630768a31f1e797d4a397e24360
Octi Zhang <zhengyuz@nvidia.com>
2026-03-15 00:25:18 -0700

    resolve unusally high memory consumption in dexsuite

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/dexsuite_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/dexsuite/dexsuite_env_cfg.py
@@ -469,8 +469,6 @@ class DexsuiteReorientEnvCfg(ManagerBasedEnvCfg):
             bounce_threshold_velocity=0.01,
             gpu_max_rigid_patch_count=4 * 5 * 2**15,
             gpu_found_lost_pairs_capacity=2**26,
-            gpu_found_lost_aggregate_pairs_capacity=2**29,
-            gpu_total_aggregate_pairs_capacity=2**25,
         )
 
 
