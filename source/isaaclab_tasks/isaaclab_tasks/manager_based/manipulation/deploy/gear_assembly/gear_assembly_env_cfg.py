34207efbf5b48ca82a35037a529cf62c2c32c919
shauryadNv <shauryad@nvidia.com>
2026-03-16 17:28:30 -0700

    Disabled scaling of ee gear wt.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
@@ -269,8 +269,8 @@ class RewardsCfg:
         params={
             "robot_asset_cfg": SceneEntityCfg("robot"),
             "keypoint_scale": 0.15,
-            "ee_gear_threshold": 0.02,
-            "weight_ramp_start": 0.0,  # Set to 0.0 to enable ramp-up
+            "ee_gear_threshold": 0.05,
+            "weight_ramp_start": 1.0,  # Set to 0.0 to enable ramp-up
             "weight_ramp_steps": 250_000,
         },
     )
@@ -284,7 +284,7 @@ class RewardsCfg:
             "kp_use_sum_of_exps": False,
             "keypoint_scale": 0.15,
             "ee_gear_threshold": 0.02,
-            "weight_ramp_start": 0.0,  # Set to 0.0 to enable ramp-up
+            "weight_ramp_start": 1.0,  # Set to 0.0 to enable ramp-up
             "weight_ramp_steps": 250_000,
         },
     )
