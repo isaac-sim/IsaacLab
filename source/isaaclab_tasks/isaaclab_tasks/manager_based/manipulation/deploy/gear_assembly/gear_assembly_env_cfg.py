b3b035fcd202ba3313b2cc9306edccea34928b1d
shauryadNv <shauryad@nvidia.com>
2026-03-19 09:46:44 -0700

    Removed action reward.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
@@ -291,7 +291,7 @@ class RewardsCfg:
 
 
     action_rate = RewTerm(func=mdp.action_rate_l2, weight=-5.0e-06)
-    action = RewTerm(func=mdp.action_l2, weight=-5.0e-06)
+    # action = RewTerm(func=mdp.action_l2, weight=-5.0e-06)
 
 
 @configclass
