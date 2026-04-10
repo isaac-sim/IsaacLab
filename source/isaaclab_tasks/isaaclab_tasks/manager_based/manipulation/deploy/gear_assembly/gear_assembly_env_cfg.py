a106373fad04f3c759e748c7844b0234bd96e33f
shauryadNv <shauryad@nvidia.com>
2026-04-10 09:40:23 -0700

    1 degree rotation obs noise.

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/deploy/gear_assembly/gear_assembly_env_cfg.py
@@ -200,9 +200,9 @@ class ObservationsCfg:
         gear_shaft_quat = ObsTerm(
             func=mdp.gear_shaft_quat_w,
             noise=ResetSampledQuaternionNoiseModelCfg(
-                roll_range=(-0.03491, 0.03491),
-                pitch_range=(-0.03491, 0.03491),
-                yaw_range=(-0.03491, 0.03491),
+                roll_range=(-0.01745, 0.01745),
+                pitch_range=(-0.01745, 0.01745),
+                yaw_range=(-0.01745, 0.01745),
             ),
         )
 
