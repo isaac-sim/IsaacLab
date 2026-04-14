5eca07aedf24b24325536e69f510ff92bcf2acde
Matthew Trepte <mtrepte@nvidia.com>
2026-04-14 00:26:12 +0000

    rm file

--- source/isaaclab_visualizers/isaaclab_visualizers/kit/Untitled-1.sh
+++
@@ -1,6 +0,0 @@
-./isaaclab.sh -p scripts/imitation_learning/isaaclab_mimic/generate_dataset.py \
---enable_cameras \
---num_envs 1 \
---generation_num_trials 1 \
---input_file ./datasets/annotated_dataset_visuomotor.hdf5 \
---output_file ./datasets/generated_dataset_visuomotor.hdf5
