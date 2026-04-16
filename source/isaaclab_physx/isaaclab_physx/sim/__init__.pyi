4e90ad9c743a8552fcbb5ae8136df757acbc9679
Kelly Guo <kellyg@nvidia.com>
2026-04-16 12:07:36 -0700

    Apply suggestion from @kellyguo11
    Signed-off-by: Kelly Guo <kellyg@nvidia.com>

--- source/isaaclab_physx/isaaclab_physx/sim/__init__.pyi
+++ source/isaaclab_physx/isaaclab_physx/sim/__init__.pyi
@@ -27,4 +27,26 @@ from .spawners import (
     spawn_deformable_body_material,
     DeformableBodyMaterialCfg,
     SurfaceDeformableBodyMaterialCfg,
+__all__ = [
+    "define_deformable_body_properties",
+    "modify_deformable_body_properties",
+    "DeformableBodyPropertiesCfg",
+    "DeformableObjectSpawnerCfg",
+    "spawn_deformable_body_material",
+    "DeformableBodyMaterialCfg",
+    "SurfaceDeformableBodyMaterialCfg",
+    "views",
+]
+
+from .schemas import (
+    define_deformable_body_properties,
+    modify_deformable_body_properties,
+    DeformableBodyPropertiesCfg
 )
+from .spawners import (
+    DeformableObjectSpawnerCfg,
+    spawn_deformable_body_material,
+    DeformableBodyMaterialCfg,
+    SurfaceDeformableBodyMaterialCfg,
+)
+from . import views
