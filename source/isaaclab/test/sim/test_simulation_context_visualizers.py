3b9f74aa1486fcae436da513fca81defc9fd494f
Matthew Trepte <mtrepte@nvidia.com>
2026-04-26 01:19:52 +0000

    fix unit test

--- source/isaaclab/test/sim/test_simulation_context_visualizers.py
+++ source/isaaclab/test/sim/test_simulation_context_visualizers.py
@@ -183,6 +183,9 @@ class _DummyViserSceneDataProvider:
         self.state_calls.append(None)
         return {"state_call": len(self.state_calls)}
 
+    def get_camera_transforms(self):
+        return {}
+
 
 class _DummyViserViewer:
     def __init__(self):
