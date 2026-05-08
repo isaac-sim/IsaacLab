f1c738f81c8dbf775f7ad2c4919386618708a3fe
HuiDong Chen <huidongc@nvidia.com>
2026-05-08 21:00:48 +0800

    format

--- tools/test_settings.py
+++ tools/test_settings.py
@@ -62,9 +62,11 @@ PER_TEST_TIMEOUTS = {
     "test_shadow_hand_vision_presets.py": 5000,
     "test_environments_newton.py": 5000,
     "test_surface_gripper.py": 3000,
-    "test_rendering_cartpole_kitless.py": 2000,       # For some reason CI takes much longer than local machine for
-    "test_rendering_dexsuite_kuka_kitless.py": 2000,  # kitless rendering tests. I will double confirm when we pin
-    "test_rendering_shadow_hand_kitless.py": 2000,    # OVRTX to 0.3 and file bug if still repro-ed.
+    # For some reason kitless rendering tests take much longer on CI than local machines.
+    # After we pin OVRTX to 0.3 we need to test whether it is still reproducible.
+    "test_rendering_cartpole_kitless.py": 2000,
+    "test_rendering_dexsuite_kuka_kitless.py": 2000,
+    "test_rendering_shadow_hand_kitless.py": 2000,
 }
 """A dictionary of tests and their timeouts in seconds.
 
