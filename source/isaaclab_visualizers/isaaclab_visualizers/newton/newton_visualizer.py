22886f895bc255e9b5953617d8eb716d45340041
Matthew Trepte <mtrepte@nvidia.com>
2026-05-19 03:07:35 +0000

    extremely small perf gain

--- source/isaaclab_visualizers/isaaclab_visualizers/newton/newton_visualizer.py
+++ source/isaaclab_visualizers/isaaclab_visualizers/newton/newton_visualizer.py
@@ -383,11 +383,12 @@ class NewtonVisualizer(BaseVisualizer):
             self._update_camera_from_usd_path()
 
         self._state = NewtonManager.get_state(self._scene_data_provider)
-        num_envs = NewtonManager.get_num_envs()
 
         update_frequency = self._viewer._update_frequency if self._viewer else self._update_frequency
         if self._step_counter % update_frequency != 0:
             return
+
+        num_envs = NewtonManager.get_num_envs()
 
         try:
             if not self._viewer.is_paused():
