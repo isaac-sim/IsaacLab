305204ea7612e7e0bcdc7efe299e87fc932dc5ac
Matthew Trepte <mtrepte@nvidia.com>
2026-06-05 00:34:45 +0000

    consolidate tests

--- source/isaaclab/test/app/test_kwarg_launch.py
+++ source/isaaclab/test/app/test_kwarg_launch.py
@@ -9,6 +9,7 @@ import pytest
 
 import isaaclab.app.app_launcher as app_launcher_module
 from isaaclab.app import AppLauncher
+from isaaclab.app.sim_launcher import _ensure_livestream_kit_visualizer
 
 
 @pytest.mark.usefixtures("mocker")
@@ -22,6 +23,21 @@ def test_livestream_launch_with_kwargs(mocker):
 
     # close the app on exit
     app.close()
+
+
+def test_livestream_injects_kit_visualizer_when_missing():
+    args = argparse.Namespace(livestream=2, visualizer=None, visualizer_explicit=False)
+
+    _ensure_livestream_kit_visualizer(args)
+
+    assert args.visualizer == ["kit"]
+
+
+def test_livestream_rejects_disabled_visualizers():
+    args = argparse.Namespace(livestream=2, visualizer=None, visualizer_explicit=True)
+
+    with pytest.raises(ValueError, match="Livestreaming requires the Kit visualizer"):
+        _ensure_livestream_kit_visualizer(args)
 
 
 class _DummySettings:
