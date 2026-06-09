3122501af1862be8f66aaeaced54aac92f8e990c
Matthew Trepte <mtrepte@nvidia.com>
2026-06-09 06:10:08 +0000

    fix test

--- source/isaaclab/isaaclab/scene_data/scene_data_provider.py
+++ source/isaaclab/isaaclab/scene_data/scene_data_provider.py
@@ -60,10 +60,8 @@ class SceneDataProvider:
         """Return Isaac Lab contact sensors keyed by scene sensor name."""
         if self._interactive_scene is None:
             return {}
-        try:
-            from isaaclab.sensors.contact_sensor import BaseContactSensor
-        except ImportError:
-            return {}
+        from isaaclab.sensors.contact_sensor import BaseContactSensor
+
         return {
             name: sensor
             for name, sensor in getattr(self._interactive_scene, "sensors", {}).items()
