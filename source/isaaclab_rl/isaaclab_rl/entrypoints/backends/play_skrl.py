07956338de386eea6d4092f739d243e1750ddc4f
jichuanh <jichuanh@nvidia.com>
2026-07-23 16:03:21 -0700

    Route the shared RL CLIs through the warp frontend factory
    Add --frontend {torch,warp} to the shared reinforcement-learning training
    and play entrypoints and construct environments through the frontend
    factory.

--- source/isaaclab_rl/isaaclab_rl/entrypoints/backends/play_skrl.py
+++ source/isaaclab_rl/isaaclab_rl/entrypoints/backends/play_skrl.py
@@ -27,7 +27,13 @@ from isaaclab.envs import DirectMARLEnvCfg
 from isaaclab.utils.dict import print_dict
 from isaaclab.utils.seed import configure_seed
 
-from isaaclab_rl.entrypoints.common import CHECKPOINT_SELECTORS, resolve_checkpoint_selector, resolve_play_task_name
+from isaaclab_rl.entrypoints.common import (
+    CHECKPOINT_SELECTORS,
+    add_frontend_args,
+    create_isaaclab_env,
+    resolve_checkpoint_selector,
+    resolve_play_task_name,
+)
 from isaaclab_rl.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
 
 import isaaclab_tasks  # noqa: F401
@@ -52,6 +58,7 @@ parser.add_argument(
 )
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
+add_frontend_args(parser)
 parser.add_argument(
     "--agent",
     type=str,
@@ -175,12 +182,12 @@ def main():
 
         env_cfg.log_dir = log_dir
 
-        env = gym.make(args_cli.task, cfg=env_cfg, render_mode="rgb_array" if args_cli.video else None)
-
-        if isinstance(env.unwrapped.cfg, DirectMARLEnvCfg) and algorithm in ["ppo"]:
-            from isaaclab.envs import multi_agent_to_single_agent
-
-            env = multi_agent_to_single_agent(env)
+        env = create_isaaclab_env(
+            args_cli.task,
+            env_cfg,
+            args_cli,
+            convert_marl_to_single_agent=isinstance(env_cfg, DirectMARLEnvCfg) and algorithm in ["ppo"],
+        )
 
         try:
             dt = env.step_dt
