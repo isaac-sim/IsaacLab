288fdf84a6845e4f45a30a8e9ea198f7da0250ff
Mustafa Haiderbhai <mhaiderbhai@nvidia.com>
2026-08-01 17:37:39 -0700

    Fix joint id type

--- source/isaaclab/isaaclab/utils/leapp/export_annotator.py
+++ source/isaaclab/isaaclab/utils/leapp/export_annotator.py
@@ -712,7 +712,7 @@ class ExportPatcher:
                     static_values.append(
                         TensorSemantics(
                             name=f"{term_name}_kp_gains",
-                            ref=kp_gains[:, joint_ids] if joint_ids else kp_gains,
+                            ref=kp_gains if joint_ids is None else kp_gains[:, joint_ids],
                             kind="kp",
                             element_names=select_element_names(joint_names, joint_ids),
                             extra=build_write_connection(scene_key, "write_joint_stiffness_to_sim_index"),
@@ -722,7 +722,7 @@ class ExportPatcher:
                     static_values.append(
                         TensorSemantics(
                             name=f"{term_name}_kd_gains",
-                            ref=kd_gains[:, joint_ids] if joint_ids else kd_gains,
+                            ref=kd_gains if joint_ids is None else kd_gains[:, joint_ids],
                             kind="kd",
                             element_names=select_element_names(joint_names, joint_ids),
                             extra=build_write_connection(scene_key, "write_joint_damping_to_sim_index"),
