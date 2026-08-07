acdfee21b442beccc2f927d6aecb73200e927aba
nvsekkin <esekkin@nvidia.com>
2026-08-07 15:36:30 -0700

    test: Link partition xfail to NVBug

--- source/isaaclab_physx/test/renderers/test_isaac_rtx_renderer_scene_partitioning.py
+++ source/isaaclab_physx/test/renderers/test_isaac_rtx_renderer_scene_partitioning.py
@@ -50,7 +50,9 @@ from isaaclab.utils.configclass import configclass
 from isaaclab_assets.robots.kuka_allegro import KUKA_ALLEGRO_CFG
 
 _ENV_VAR = "ISAAC_LAB_ENABLE_ISAAC_RTX_PER_ENV_SCENE_PARTITION"
-_PARTITION_XFAIL = pytest.mark.xfail(reason="Kit c0b875cd scene partitioning regression", strict=False, run=False)
+_PARTITION_XFAIL = pytest.mark.xfail(
+    reason="NVBug 6264822: Kit c0b875cd scene partitioning regression", strict=False, run=False
+)
 
 
 @pytest.fixture()
