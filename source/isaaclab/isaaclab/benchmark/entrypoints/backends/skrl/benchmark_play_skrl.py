e4317a553ac9f62d17212890fee6a34048e3cb83
Antoine Richard <antoiner@nvidia.com>
2026-08-12 13:12:25 +0200

    fix(preset_cli): propagate agent_library="skrl" to benchmark_play_skrl

--- source/isaaclab/isaaclab/benchmark/entrypoints/backends/skrl/benchmark_play_skrl.py
+++ source/isaaclab/isaaclab/benchmark/entrypoints/backends/skrl/benchmark_play_skrl.py
@@ -102,7 +102,7 @@ def _parse_args(argv: list[str]):
     )
     add_launcher_args(parser)
 
-    args_cli, remaining_args = setup_preset_cli(parser, argv)
+    args_cli, remaining_args = setup_preset_cli(parser, argv, agent_library="skrl")
     sys.argv = [sys.argv[0]] + remaining_args
 
     return args_cli, remaining_args
