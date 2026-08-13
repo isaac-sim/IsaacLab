543ee34b647c35e7c3e7c00a0938c43debcf4838
Mustafa Haiderbhai <mhaiderbhai@nvidia.com>
2026-08-13 01:09:16 -0700

    Format environment test updates

--- source/isaaclab_tasks/test/core/test_environments.py
+++ source/isaaclab_tasks/test/core/test_environments.py
@@ -21,8 +21,8 @@ import isaaclab_tasks  # noqa: F401
 # Local imports should be imported last
 from env_test_utils import _run_environments, setup_environment  # isort: skip
 
-@pytest.mark.parametrize("physics_preset_name", ["newton_mjwarp", "physx", "isaacsim_physx"])
 
+@pytest.mark.parametrize("physics_preset_name", ["newton_mjwarp", "physx", "isaacsim_physx"])
 @pytest.mark.parametrize("num_envs, device", [(2, "cuda"), (1, "cuda")])
 @pytest.mark.parametrize(
     "task_name",
