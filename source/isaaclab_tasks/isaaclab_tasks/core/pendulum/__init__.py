d7c82112af4cd6f6ec008494a15a49f953c4d91b
Mustafa Haiderbhai <mhaiderbhai@nvidia.com>
2026-08-12 21:02:32 -0700

    Default core tasks to RSL-RL

--- source/isaaclab_tasks/isaaclab_tasks/core/pendulum/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/core/pendulum/__init__.py
@@ -21,6 +21,8 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": f"{__name__}.pendulum_env_cfg:PendulumEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PendulumPPORunnerCfg",
+        "default_agent": "rsl_rl",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_cfg.yaml",
