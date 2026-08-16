aa447a7bce88384fe572a5ccb6368161addaa834
Mustafa Haiderbhai <mhaiderbhai@nvidia.com>
2026-08-16 13:33:01 -0700

    Cleanup

--- source/isaaclab_tasks/isaaclab_tasks/utils/presets.py
+++ source/isaaclab_tasks/isaaclab_tasks/utils/presets.py
@@ -22,11 +22,11 @@ class _AutoRtxRendererCfg(RendererCfg):
 
 @configclass
 class MultiBackendRendererCfg(PresetCfg):
-    default: NewtonWarpRendererCfg = NewtonWarpRendererCfg()
     rtx: _AutoRtxRendererCfg = _AutoRtxRendererCfg()
-    newton_renderer: NewtonWarpRendererCfg = NewtonWarpRendererCfg()
     ovrtx: OVRTXRendererCfg = OVRTXRendererCfg()
     isaacsim_rtx: IsaacRtxRendererCfg = IsaacRtxRendererCfg()
+    newton_renderer: NewtonWarpRendererCfg = NewtonWarpRendererCfg()
+    default: NewtonWarpRendererCfg = NewtonWarpRendererCfg()
 
 
 def set_isaac_rtx_global_settings(renderer_cfg: Any, **settings: Any) -> None:
