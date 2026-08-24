7c545266bbfe7018e1e1d5edc57fe5288bc3447e
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-24 21:23:28 +0100

    Remove duplicated dotted callable tests

--- source/isaaclab/test/utils/test_dotted_callable_resolution.py
+++ source/isaaclab/test/utils/test_dotted_callable_resolution.py
@@ -8,7 +8,7 @@ from collections import Counter
 import pytest
 
 from isaaclab.utils.configclass import configclass
-from isaaclab.utils.string import ResolvableString, callable_to_string, string_to_callable
+from isaaclab.utils.string import ResolvableString
 
 pytestmark = pytest.mark.unit
 
@@ -19,48 +19,10 @@ class _NestedCallableCfg:
 
 
 def test_configclass_wraps_nested_callable_reference():
+    """Configclass should wrap dotted callable references as lazy resolvable strings."""
     cfg = _NestedCallableCfg()
 
     assert isinstance(cfg.updater, ResolvableString)
     counter = Counter()
     cfg.updater(counter, {"value": 2})
     assert counter["value"] == 2
-
-
-def test_nested_callable_reference_round_trips():
-    reference = callable_to_string(Counter.update)
-
-    assert reference == "collections:Counter.update"
-    assert string_to_callable(reference) is Counter.update
-
-    counter = Counter()
-    ResolvableString(reference)(counter, {"value": 2})
-    assert counter["value"] == 2
-
-
-def test_missing_nested_callable_attribute_raises_value_error():
-    with pytest.raises(ValueError, match="Could not resolve"):
-        string_to_callable("collections:Counter.not_a_method")
-
-
-def _make_exported_local_callable():
-    def exported_local_callable():
-        return "ok"
-
-    return exported_local_callable
-
-
-exported_local_callable = _make_exported_local_callable()
-
-
-def test_local_callable_serialization_falls_back_to_resolvable_name():
-    reference = callable_to_string(exported_local_callable)
-
-    assert reference == f"{__name__}:exported_local_callable"
-    assert string_to_callable(reference) is exported_local_callable
-
-
-def test_instance_bound_method_preserves_legacy_simple_name():
-    counter = Counter()
-
-    assert callable_to_string(counter.update) == "collections:update"
