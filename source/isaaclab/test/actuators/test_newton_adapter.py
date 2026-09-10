695c6351a48168cf5b6033095c3534cd5bbc9265
Kelly Guo <kellyg@nvidia.com>
2026-09-09 17:41:53 -0700

    Use Newton actuators by default

--- source/isaaclab/test/actuators/test_newton_adapter.py
+++ source/isaaclab/test/actuators/test_newton_adapter.py
@@ -13,6 +13,7 @@ from pxr import Usd, UsdGeom, UsdPhysics
 
 from isaaclab.actuators import ActuatorBaseCfg, DCMotor, DCMotorCfg, DelayedPDActuatorCfg, RemotizedPDActuatorCfg
 from isaaclab.actuators.newton import NewtonActuatorAdapter
+from isaaclab.sim import SimulationCfg
 from isaaclab.sim.schemas.schemas_actuators import _author_actuator_prims
 from isaaclab.utils.configclass import configclass
 
@@ -94,6 +95,12 @@ def _make_actuator_stage() -> Usd.Stage:
         },
     )
     return stage
+
+
+def test_newton_actuator_path_is_enabled_by_default():
+    """Use the Newton actuator path unless the legacy path is explicitly requested."""
+    assert SimulationCfg().use_newton_actuators
+    assert not SimulationCfg(use_newton_actuators=False).use_newton_actuators
 
 
 def test_from_usd_groups_by_structure_and_preserves_per_dof_values():
